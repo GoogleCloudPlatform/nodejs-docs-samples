2fb06b2add990f563a92935f99be2badb73414b2
Jerjou Cheng <jerjou@google.com>
2015-08-14 14:58:15 -0700

    Add sample for using cloud storage api.

---
+++ cloud-storage/authSample.js
@@ -0,0 +1,54 @@
+/**
+ * @fileoverview Sample code that grabs default credentials from the
+ * environment, then uses it to make an api call.
+ */
+
+var google = require('googleapis');
+
+/**
+ * Callback for remote requests.
+ *
+ * @callback requestCallback
+ * @param {Object} error - if there's an error with the request, this is pass
+ * through to the callback.
+ * @param {Object} response - the response for the request.
+ */
+
+/**
+ * Fetches a list of the buckets under the given project id.
+ *
+ * @param {String} projectId - the project id that owns the buckets.
+ * @param {requestCallback} callback - a function to be called when the server
+ *     responds with the list of buckets.
+ */
+// [START list_buckets]
+function listBuckets(projectId, callback) {
+  google.auth.getApplicationDefault(function(error, authClient) {
+    if (error) {
+      return callback(error);
+    }
+
+    // Depending on the environment that provides the default credentials
+    // (eg Compute Engine, App Engine), the credentials may require us to
+    // specify the scopes we need explicitly.
+    // Check for this case, and inject the Cloud Storage scope if required.
+    if (authClient.createScopedRequired &&
+        authClient.createScopedRequired()) {
+      authClient = authClient.createScoped(
+          ['https://www.googleapis.com/auth/devstorage.read_write']);
+    }
+
+    // Create the service object.
+    var storage = google.storage('v1');
+    // Make the api call to list the buckets.
+    storage.buckets.list({
+      auth: authClient,
+      project: projectId
+    }, callback);
+  });
+}
+// [END list_buckets]
+
+module.exports = {
+  listBuckets: listBuckets
+};
---
+++ test/cloud-storage/testAuthSample.js
@@ -0,0 +1,27 @@
+/**
+ * @fileoverview Tests for the list-buckets module.
+ */
+
+var assert = require("assert");
+var _ = require('lodash');
+
+var authSample = require("../../cloud-storage/authSample");
+
+describe('listBuckets', function() {
+  it('returns a list of buckets', function (done) {
+    authSample.listBuckets(
+        process.env.TEST_PROJECT_ID,
+        function(error, success) {
+          if (error) {
+            done('Should not have returned an error: ' + error);
+          } else {
+            assert(success.items.length > 0);
+            assert(_.find(success.items, function(item) {
+              return item.name == process.env.TEST_BUCKET_NAME;
+            }));
+            done();
+          }
+        });
+  });
+});
+

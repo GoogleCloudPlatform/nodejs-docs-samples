8f180d6e53a073ee403a80d235e9cd97fab476db
Jason Dobry <jmdobry@users.noreply.github.com>
2015-11-23 15:30:28 -0800

    Add github include tag.

--- appengine/mongo/server.js
+++ appengine/mongo/server.js
@@ -31,6 +31,7 @@ var uri = 'mongodb://' +
   nconf.get('mongoPort') + '/' +
   nconf.get('mongoDatabase');
 
+// [START client]
 mongodb.MongoClient.connect(uri, function(err, db) {
   
   if (err) {
@@ -72,3 +73,4 @@ mongodb.MongoClient.connect(uri, function(err, db) {
     console.log('started web process');  
   });
 });
+// [END client]

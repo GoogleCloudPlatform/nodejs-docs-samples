806a9879cea0d26a4fce295d1c5680a6ae8a4be8
Phil Targun <phil.targun@icloud.com>
2016-03-07 23:07:19 -0600

    Update brunch-server.js

--- appengine/brunch/brunch-server.js
+++ appengine/brunch/brunch-server.js
@@ -7,12 +7,9 @@ var Path = require('path');
 module.exports = function startServer(port, path, callback) {
   var app = express();
   var server = http.createServer(app);
-
   app.use(express.static(Path.join(__dirname, '/public')));
-
   app.set('views', Path.join(__dirname, 'app/assets/views'));
   app.set('view engine', 'jade');
-
   app.get('/', function(req, res) {
     res.render('index');
   });

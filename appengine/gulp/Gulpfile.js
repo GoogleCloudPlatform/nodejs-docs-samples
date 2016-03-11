d4cf9aebf8ba35d19065b1227dd570421c25faf6
Phil Targun <phil.targun@icloud.com>
2016-03-11 13:34:20 -0600

    removed spaces and dev dependencies

--- appengine/gulp/Gulpfile.js
+++ appengine/gulp/Gulpfile.js
@@ -4,22 +4,22 @@ var cssnano = require('gulp-cssnano');
 
 // Lint Task
 gulp.task('lint', function() {
-    return gulp.src(['Gulpfile.js', 'src/**/*.js'])
-        .pipe(jshint())
-        .pipe(jshint.reporter('default'));
+  return gulp.src(['Gulpfile.js', 'src/**/*.js'])
+    .pipe(jshint())
+    .pipe(jshint.reporter('default'));
 });
 
 // Minify CSS
 gulp.task('styles', function() {
-    return gulp.src('src/public/stylesheets/*.css')
-        .pipe(cssnano())
-        .pipe(gulp.dest('src/public/stylesheets'));
+  return gulp.src('src/public/stylesheets/*.css')
+    .pipe(cssnano())
+    .pipe(gulp.dest('src/public/stylesheets'));
 });
 
 // Watch Files For Changes
 gulp.task('watch', function() {
-    gulp.watch('src/**/*.js', ['lint', 'scripts']);
-    gulp.watch('src/public/stylesheets/*.css', ['styles']);
+  gulp.watch('src/**/*.js', ['lint', 'scripts']);
+  gulp.watch('src/public/stylesheets/*.css', ['styles']);
 });
 
 

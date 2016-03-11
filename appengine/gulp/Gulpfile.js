713c0eb0a9d0e206055b6553a17186adaca08e15
Phil Targun <phil.targun@icloud.com>
2016-03-11 13:03:28 -0600

    Added basic gulp + express sample

---
+++ appengine/gulp/Gulpfile.js
@@ -0,0 +1,27 @@
+var gulp = require('gulp');
+var jshint = require('gulp-jshint');
+var cssnano = require('gulp-cssnano');
+
+// Lint Task
+gulp.task('lint', function() {
+    return gulp.src(['Gulpfile.js', 'src/**/*.js'])
+        .pipe(jshint())
+        .pipe(jshint.reporter('default'));
+});
+
+// Minify CSS
+gulp.task('styles', function() {
+    return gulp.src('src/public/stylesheets/*.css')
+        .pipe(cssnano())
+        .pipe(gulp.dest('src/public/stylesheets'));
+});
+
+// Watch Files For Changes
+gulp.task('watch', function() {
+    gulp.watch('src/**/*.js', ['lint', 'scripts']);
+    gulp.watch('src/public/stylesheets/*.css', ['styles']);
+});
+
+
+gulp.task('build', ['lint', 'styles']);
+gulp.task('default', ['lint', 'styles', 'watch']);
---
+++ appengine/gulp/src/app.js
@@ -0,0 +1,72 @@
+// Copyright 2015-2016, Google, Inc.
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+'use strict';
+
+var express = require('express');
+var path = require('path');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+var routes = require('./routes/index');
+var users = require('./routes/users');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', routes);
+app.use('/users', users);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
+});
+
+
+module.exports = app;

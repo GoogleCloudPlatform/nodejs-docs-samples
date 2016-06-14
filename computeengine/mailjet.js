5482463416261a2694fbd0362f150daf49f1d951
CRoed <chris.roed@anserinnovation.com>
2016-06-14 11:41:58 -0500

    Update mailjet.js

--- computeengine/mailjet.js
+++ computeengine/mailjet.js
@@ -18,7 +18,7 @@ var mailer = require('nodemailer');
 var smtp = require('nodemailer-smtp-transport');
 
 var transport = mailer.createTransport(smtp({
-  host: "in.mailjet.com",
+  host: 'in.mailjet.com',
   port: 2525,
   auth: {
     user: process.env.MAILJET_API_KEY || '<your-mailjet-api-key',

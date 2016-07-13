82b53978d0c7c60b7daed6a7f6b0ef778a40eceb
Jason Dobry <jason.dobry@gmail.com>
2016-07-13 16:31:58 -0700

    Add Cloud Functions Slack Slash Command sample.

---
+++ functions/slack/README.md
@@ -0,0 +1,57 @@
+<img src="https://avatars2.githubusercontent.com/u/2810941?v=3&s=96" alt="Google Cloud Platform logo" title="Google Cloud Platform" align="right" height="96" width="96"/>
+
+# Google Cloud Functions Slack Slash Command sample
+
+This recipe shows you how to implement a Slack Slash Command with Cloud
+Functions.
+
+View the [source code][code].
+
+[code]: index.js
+
+## Deploy and Test
+
+1. Follow the [Cloud Functions quickstart guide][quickstart] to setup Cloud
+Functions for your project.
+
+1. Clone this repository:
+
+        git clone https://github.com/GoogleCloudPlatform/nodejs-docs-samples.git
+        cd nodejs-docs-samples/functions/sendgrid
+
+1. Create or join a Slack community.
+
+    See [Creating a Slack community](TODO).
+
+1. Create a Slash Command:
+
+    See [Creating a Slash Command](TODO).
+
+1. Create a Cloud Storage bucket to stage your Cloud Functions files, where
+`[YOUR_STAGING_BUCKET_NAME]` is a globally-unique bucket name:
+
+        gsutil mb gs://[YOUR_STAGING_BUCKET_NAME]
+
+1. Deploy the `kgSearch` function with an HTTP trigger, where
+`[YOUR_STAGING_BUCKET_NAME]` is the name of your staging bucket:
+
+        gcloud alpha functions deploy kgSearch --bucket [YOUR_STAGING_BUCKET_NAME] --trigger-http
+
+1. Call the `kgSearch` function by making an HTTP request:
+
+        curl -X POST "https://[YOUR_REGION].[YOUR_PROJECT_ID].cloudfunctions.net/kgSearch" --data '{"token":"[YOUR_SLACK_TOKEN]","text":"giraffe"}'
+
+    * Replace `[YOUR_REGION]` with the region where you function is deployed. This is visible in your terminal when your function finishes deploying.
+    * Replace `[YOUR_PROJECT_ID]` with your Cloud project ID. This is visible in your terminal when your function finishes deploying.
+    * `[YOUR_SLACK_TOKEN]` is the token provided by Slack in the configured Slash Command.
+
+1. Check the logs for the `subscribe` function:
+
+        gcloud alpha functions get-logs kgSearch
+
+    You should see something like this in your console:
+
+        D      ... User function triggered, starting execution
+        D      ... Execution took 1 ms, user function completed successfully
+
+[quickstart]: https://cloud.google.com/functions/quickstart
---
+++ functions/slack/index.js
@@ -0,0 +1,148 @@
+// Copyright 2016, Google, Inc.
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+'use strict';
+
+// [START setup]
+var config = require('./config.json');
+var googleapis = require('googleapis');
+
+// Get a reference to the Knowledge Graph Search component
+var kgsearch = googleapis.kgsearch('v1');
+// [END setup]
+
+// [START formatSlackMessage]
+/**
+ * TODO.
+ *
+ * @param {string} query The user's search query.
+ * @param {Object} response The response from the Knowledge Graph API.
+ * @returns {Object} The formatted message.
+ */
+function formatSlackMessage (query, response) {
+  var entity;
+
+  // Extract the first entity from the result list, if any
+  if (response && response.itemListElement &&
+    response.itemListElement.length) {
+    entity = response.itemListElement[0].result;
+  }
+
+  // Prepare a rich Slack message
+  // See https://api.slack.com/docs/message-formatting
+  var response = {
+    response_type: 'in_channel',
+    text: 'Query: ' + query,
+    attachments: []
+  };
+
+  if (entity) {
+    response.attachments.push({
+      color: '#3367d6',
+      title: entity.name + ': ' + entity.description,
+      title_link: entity.detailedDescription.url,
+      text: entity.detailedDescription.articleBody,
+      image_url: entity.image.contentUrl
+    });
+  } else {
+    response.attachments.push({
+      text: 'No results match your query...'
+    });
+  }
+
+  return response;
+}
+// [END formatSlackMessage]
+
+// [START verifyWebhook]
+/**
+ * Verify that the webhook request came from Slack.
+ *
+ * @param {Object} body The body of the request.
+ * @param {string} body.token The Slack token to be verified.
+ */
+function verifyWebhook (body) {
+  if (!body || body.token !== config.SLACK_TOKEN) {
+    var error = new Error('Invalid credentials');
+    error.code = 401;
+    throw error;
+  }
+}
+// [END verifyWebhook]
+
+// [START makeSearchRequest]
+/**
+ * Send the user's search query to the Knowledge Graph API.
+ *
+ * @param {string} query The user's search query.
+ * @param {Function} callback Callback function.
+ */
+function makeSearchRequest (query, callback) {
+  kgsearch.entities.search({
+    auth: config.KG_API_KEY,
+    query: query,
+    limit: 1
+  }, function (err, response) {
+    if (err) {
+      return callback(err);
+    }
+
+    // Return a formatted message
+    return callback(null, formatSlackMessage(query, response));
+  });
+}
+// [END makeSearchRequest]
+
+// [START kgSearch]
+/**
+ * Receive a Slash Command request from Slack.
+ *
+ * Trigger this function by making a POST request with a payload to:
+ * https://[YOUR_REGION].[YOUR_PROJECT_ID].cloudfunctions.net/kgsearch
+ *
+ * @example
+ * curl -X POST "https://us-central1.your-project-id.cloudfunctions.net/kgSearch" --data '{"token":"[YOUR_SLACK_TOKEN]","text":"giraffe"}'
+ *
+ * @param {Object} req Cloud Function request object.
+ * @param {Object} req.body The request payload.
+ * @param {string} req.body.token Slack's verification token.
+ * @param {string} req.body.text The user's search query.
+ * @param {Object} res Cloud Function response object.
+ */
+exports.kgSearch = function kgSearch (req, res) {
+  try {
+    if (req.method !== 'POST') {
+      var error = new Error('Only POST requests are accepted');
+      error.code = 405;
+      throw error;
+    }
+
+    // Verify that this request came from Slack
+    verifyWebhook(req.body);
+
+    // Make the request to the Knowledge Graph Search API
+    makeSearchRequest(req.body.text, function (err, response) {
+      if (err) {
+        console.error(err);
+        return res.status(500);
+      }
+
+      // Send the formatted message back to Slack
+      return res.json(response);
+    });
+  } catch (err) {
+    console.error(err);
+    return res.status(err.code || 500).send(err.message);
+  }
+};
+// [END kgSearch]

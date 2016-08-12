94aa8b4801427b3b66285acaf7f6e25c0619c570
Jason Dobry <jason.dobry@gmail.com>
2016-08-12 16:20:25 -0700

    StorageTransfer API samples.

---
+++ storage/system-test/transfer.test.js
@@ -0,0 +1,125 @@
+// Copyright 2015-2016, Google, Inc.
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+'use strict';
+
+var uuid = require('node-uuid');
+var program = require('../transfer');
+var Storage = require('@google-cloud/storage');
+
+var storage = Storage();
+var bucketName = 'nodejs-docs-samples-test-' + uuid.v4();
+var bucketName2 = 'nodejs-docs-samples-test-' + uuid.v4();
+
+describe.only('storage:transfer', function () {
+  var jobName;
+  var date = '2222/08/11';
+  var time = '15:30';
+  var description = 'this is a test';
+  var status = 'DISABLED';
+
+  before(function (done) {
+    storage.createBucket(bucketName, function (err) {
+      if (err) {
+        return done(err);
+      }
+      storage.createBucket(bucketName2, done);
+    });
+  });
+
+  after(function (done) {
+    storage.bucket(bucketName).deleteFiles({ force: true }, function (err) {
+      if (err) {
+        return done(err);
+      }
+      storage.bucket(bucketName).delete(function (err) {
+        if (err) {
+          return done(err);
+        }
+        storage.bucket(bucketName2).deleteFiles({ force: true }, function (err) {
+          if (err) {
+            return done(err);
+          }
+          storage.bucket(bucketName2).delete(done);
+        });
+      });
+    });
+  });
+
+  describe('createTransferJob', function () {
+    it('should create a storage transfer job', function (done) {
+      program.createTransferJob(bucketName, bucketName2, date, time, description, function (err, transferJob) {
+        assert.ifError(err);
+        jobName = transferJob.name;
+        assert.equal(transferJob.name.indexOf('transferJobs/'), 0);
+        assert.equal(transferJob.description, description);
+        assert.equal(transferJob.status, 'ENABLED');
+        assert(console.log.calledWith('Created transfer job: %s', transferJob.name));
+        setTimeout(done, 2000);
+      });
+    });
+  });
+
+  describe('getTransferJob', function () {
+    it('should get a transferJob', function (done) {
+      program.getTransferJob(jobName, function (err, transferJob) {
+        assert.ifError(err);
+        assert.equal(transferJob.name, jobName);
+        assert.equal(transferJob.description, description);
+        assert.equal(transferJob.status, 'ENABLED');
+        assert(console.log.calledWith('Found transfer job: %s', transferJob.name));
+        setTimeout(done, 2000);
+      });
+    });
+  });
+
+  describe('updateTransferJob', function () {
+    it('should update a transferJob', function (done) {
+      program.updateTransferJob(jobName, 'status', status, function (err, transferJob) {
+        assert.ifError(err);
+        assert.equal(transferJob.name, jobName);
+        assert.equal(transferJob.description, description);
+        assert.equal(transferJob.status, status);
+        assert(console.log.calledWith('Updated transfer job: %s', transferJob.name));
+        setTimeout(done, 2000);
+      });
+    });
+  });
+
+  describe('listTransferJobs', function () {
+    it('should list transferJobs', function (done) {
+      program.listTransferJobs(function (err, transferJobs) {
+        assert.ifError(err);
+        var matchingTransferJobs = transferJobs.filter(function (transferJob) {
+          return transferJob.name === jobName;
+        });
+        assert.equal(matchingTransferJobs.length, 1);
+        assert.equal(matchingTransferJobs[0].name, jobName);
+        assert.equal(matchingTransferJobs[0].description, description);
+        assert.equal(matchingTransferJobs[0].status, status);
+        assert(console.log.calledWith('Found %d jobs!', transferJobs.length));
+        setTimeout(done, 2000);
+      });
+    });
+  });
+
+  describe('listTransferOperations', function () {
+    it('should list transferJobs', function (done) {
+      program.listTransferOperations(jobName, function (err, operations) {
+        assert.ifError(err);
+        assert(Array.isArray(operations));
+        done();
+      });
+    });
+  });
+});

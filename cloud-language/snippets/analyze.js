0900a58fc1fe3e21519912a96dd4238bb828b2a8
Jason Dobry <jason.dobry@gmail.com>
2016-08-27 17:35:51 -0700

    Address comments.

--- cloud-language/snippets/analyze.js
+++ cloud-language/snippets/analyze.js
@@ -29,17 +29,17 @@ var language = Language();
 var storage = Storage();
 // [END setup]
 
-// [START analyze_sentiment]
+// [START analyze_sentiment_from_string]
 /**
  * Detect the sentiment of a block of text.
  *
  * @param {string} text The text to analyze.
  * @param {object} [options] Configuration options.
- * @param {string} [options.type] "text" or "html".
+ * @param {string} [options.type] The type of the text, either "text" or "html".
  * @param {string} [options.language] The language of the text, e.g. "en".
  * @param {function} callback The callback function.
  */
-function analyzeSentiment (text, options, callback) {
+function analyzeSentimentFromString (text, options, callback) {
   var document = language.document({
     content: text,
     type: options.type,
@@ -61,7 +61,7 @@ function analyzeSentiment (text, options, callback) {
     return callback(null, sentiment);
   });
 }
-// [END analyze_sentiment]
+// [END analyze_sentiment_from_string]
 
 // [START analyze_sentiment_from_file]
 /**
@@ -71,7 +71,7 @@ function analyzeSentiment (text, options, callback) {
  * @param {string} filename The name of the file to be analyzed.
  * @param {object} [options] Optional configuration.
  * @param {string} [options.language] The language of the text, e.g. "en".
- * @param {string} [options.type] "text" or "html".
+ * @param {string} [options.type] The type of the text, either "text" or "html".
  * @param {function} callback The callback function.
  */
 function analyzeSentimentFromFile (bucket, filename, options, callback) {
@@ -98,17 +98,17 @@ function analyzeSentimentFromFile (bucket, filename, options, callback) {
 }
 // [END analyze_sentiment_from_file]
 
-// [START analyze_entities]
+// [START analyze_entities_from_string]
 /**
  * Detect the entities from a block of text.
  *
  * @param {string} text The text to analyze.
  * @param {object} [options] Optional configuration.
  * @param {string} [options.language] The language of the text, e.g. "en".
- * @param {string} [options.type] "text" or "html".
+ * @param {string} [options.type] The type of the text, either "text" or "html".
  * @param {function} callback The callback function.
  */
-function analyzeEntities (text, options, callback) {
+function analyzeEntitiesFromString (text, options, callback) {
   var document = language.document({
     content: text,
     type: options.type,
@@ -130,7 +130,7 @@ function analyzeEntities (text, options, callback) {
     return callback(null, entities);
   });
 }
-// [END analyze_entities]
+// [END analyze_entities_from_string]
 
 // [START analyze_entities_from_file]
 /**
@@ -140,7 +140,7 @@ function analyzeEntities (text, options, callback) {
  * @param {string} filename The name of the file to be analyzed.
  * @param {object} [options] Optional configuration.
  * @param {string} [options.language] The language of the text, e.g. "en".
- * @param {string} [options.type] "text" or "html".
+ * @param {string} [options.type] The type of the text, either "text" or "html".
  * @param {function} callback The callback function.
  */
 function analyzeEntitiesFromFile (bucket, filename, options, callback) {
@@ -167,17 +167,17 @@ function analyzeEntitiesFromFile (bucket, filename, options, callback) {
 }
 // [END analyze_entities_from_file]
 
-// [START analyze_syntax]
+// [START analyze_syntax_from_string]
 /**
  * Detect the syntax in a block of text.
  *
  * @param {string} text The text to analyze.
  * @param {object} [options] Optional configuration.
  * @param {string} [options.language] The language of the text, e.g. "en".
- * @param {string} [options.type] "text" or "html".
+ * @param {string} [options.type] The type of the text, either "text" or "html".
  * @param {function} callback The callback function.
  */
-function analyzeSyntax (text, options, callback) {
+function analyzeSyntaxFromString (text, options, callback) {
   var document = language.document({
     content: text,
     type: options.type,
@@ -198,7 +198,7 @@ function analyzeSyntax (text, options, callback) {
     return callback(null, apiResponse);
   });
 }
-// [END analyze_syntax]
+// [END analyze_syntax_from_string]
 
 // [START analyze_syntax_from_file]
 /**
@@ -208,7 +208,7 @@ function analyzeSyntax (text, options, callback) {
  * @param {string} filename The name of the file to be analyzed.
  * @param {object} [options] Optional configuration.
  * @param {string} [options.language] The language of the text, e.g. "en".
- * @param {string} [options.type] "text" or "html".
+ * @param {string} [options.type] The type of the text, either "text" or "html".
  * @param {function} callback The callback function.
  */
 function analyzeSyntaxFromFile (bucket, filename, options, callback) {
@@ -240,11 +240,11 @@ var cli = require('yargs');
 var utils = require('../utils');
 
 var program = module.exports = {
-  analyzeSentiment: analyzeSentiment,
+  analyzeSentimentFromString: analyzeSentimentFromString,
   analyzeSentimentFromFile: analyzeSentimentFromFile,
-  analyzeEntities: analyzeEntities,
+  analyzeEntitiesFromString: analyzeEntitiesFromString,
   analyzeEntitiesFromFile: analyzeEntitiesFromFile,
-  analyzeSyntax: analyzeSyntax,
+  analyzeSyntaxFromString: analyzeSyntaxFromString,
   analyzeSyntaxFromFile: analyzeSyntaxFromFile,
   main: function (args) {
     // Run the command-line program
@@ -254,22 +254,22 @@ var program = module.exports = {
 
 cli
   .demand(1)
-  .command('sentiment <text>', 'Detect the sentiment of a block of text.', {}, function (options) {
-    program.analyzeSentiment(options.text, utils.pick(options, ['language', 'type']), utils.makeHandler());
+  .command('sentimentFromString <text>', 'Detect the sentiment of a block of text.', {}, function (options) {
+    program.analyzeSentimentFromString(options.text, utils.pick(options, ['language', 'type']), utils.makeHandler());
   })
   .command('sentimentFromFile <bucket> <filename>', 'Detect the sentiment of text in a GCS file.', {}, function (options) {
     program.analyzeSentimentFromFile(options.bucket, options.filename, utils.pick(options, ['language', 'type']), utils.makeHandler());
   })
-  .command('entities <text>', 'Detect the entities of a block of text.', {}, function (options) {
-    program.analyzeEntities(options.text, utils.pick(options, ['language', 'type']), utils.makeHandler());
+  .command('entitiesFromString <text>', 'Detect the entities of a block of text.', {}, function (options) {
+    program.analyzeEntitiesFromString(options.text, utils.pick(options, ['language', 'type']), utils.makeHandler());
   })
   .command('entitiesFromFile <bucket> <filename>', 'Detect the entities of text in a GCS file.', {}, function (options) {
     program.analyzeEntitiesFromFile(options.bucket, options.filename, utils.pick(options, ['language', 'type']), utils.makeHandler());
   })
-  .command('syntax <text>', 'Detect the syntax of a block of text.', {}, function (options) {
-    program.analyzeSyntax(options.text, utils.pick(options, ['language', 'type']), utils.makeHandler());
+  .command('syntaxFromString <text>', 'Detect the syntax of a block of text.', {}, function (options) {
+    program.analyzeSyntaxFromString(options.text, utils.pick(options, ['language', 'type']), utils.makeHandler());
   })
-  .command('syntaxFromFile <bucket> <filename>', 'Detect the syntax of a block of text.', {}, function (options) {
+  .command('syntaxFromFile <bucket> <filename>', 'Detect the syntax of text in a GCS file.', {}, function (options) {
     program.analyzeSyntaxFromFile(options.bucket, options.filename, utils.pick(options, ['language', 'type']), utils.makeHandler());
   })
   .options({
@@ -290,13 +290,13 @@ cli
       global: true
     }
   })
-  .example('node $0 sentiment "President Obama is speaking at the White House."', '')
+  .example('node $0 sentimentFromString "President Obama is speaking at the White House."', '')
   .example('node $0 sentimentFromFile my-bucket file.txt', '')
-  .example('node $0 entities "<p>President Obama is speaking at the White House.</p> -t html"', '')
+  .example('node $0 entitiesFromString "<p>President Obama is speaking at the White House.</p>" -t html', '')
   .example('node $0 entitiesFromFile my-bucket file.txt', '')
-  .example('node $0 syntax "President Obama is speaking at the White House."', '')
+  .example('node $0 syntaxFromString "President Obama is speaking at the White House."', '')
   .example('node $0 syntaxFromFile my-bucket es_file.txt -l es', '')
-  .wrap(200)
+  .wrap(100)
   .recommendCommands()
   .epilogue('For more information, see https://cloud.google.com/natural-language/docs');
 

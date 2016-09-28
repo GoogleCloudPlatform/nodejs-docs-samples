1217387cb5e0b5c38fb34d76d7e5045b864fc005
Jason Dobry <jmdobry@users.noreply.github.com>
2016-09-27 22:02:05 -0700

    Adds 6 quickstart examples (#218)
    * Add some simple "quickstart" samples.
    
    * Add quickstart tests (#215)
    
    * First draft of new tests
    
    * Fix failing tests
    
    * Fix comments
    
    * Add comments.
    
    * Update comments.
    
    * Add another comment.
    
    * Cleanup.
    
    * Fix region tags.
    
    * Fix comments.

---
+++ bigquery/test/quickstart.test.js
@@ -0,0 +1,38 @@
+// Copyright 2016, Google, Inc.
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+'use strict';
+
+const proxyquire = require(`proxyquire`).noCallThru();
+
+describe(`bigquery:quickstart`, () => {
+  let bigqueryMock, BigqueryMock;
+
+  before(() => {
+    bigqueryMock = {
+      createDataset: sinon.stub().yields(null, {}, {})
+    };
+    BigqueryMock = sinon.stub().returns(bigqueryMock);
+  });
+
+  it(`should create a dataset`, () => {
+    proxyquire(`../quickstart`, {
+      '@google-cloud/bigquery': BigqueryMock
+    });
+
+    assert.equal(BigqueryMock.calledOnce, true);
+    assert.deepEqual(BigqueryMock.firstCall.args, [{ projectId: 'YOUR_PROJECT_ID' }]);
+    assert.equal(bigqueryMock.createDataset.calledOnce, true);
+    assert.deepEqual(bigqueryMock.createDataset.firstCall.args.slice(0, -1), ['my_new_dataset']);
+  });
+});
---
+++ storage/system-test/quickstart.test.js
@@ -0,0 +1,54 @@
+// Copyright 2015-2016, Google, Inc.
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+'use strict';
+
+const proxyquire = require(`proxyquire`).noPreserveCache();
+const storage = proxyquire(`@google-cloud/storage`, {})();
+const uuid = require(`node-uuid`);
+
+const bucketName = `nodejs-docs-samples-test-${uuid.v4()}`;
+
+describe(`storage:quickstart`, () => {
+  let storageMock, StorageMock;
+
+  after((done) => {
+    storage.bucket(bucketName).delete(() => {
+      // Ignore any error, the topic might not have been created
+      done();
+    });
+  });
+
+  it(`should create a topic`, (done) => {
+    const expectedBucketName = `my-new-bucket`;
+
+    storageMock = {
+      createBucket: (_bucketName) => {
+        assert.equal(_bucketName, expectedBucketName);
+
+        storage.createBucket(bucketName, (err, bucket, apiResponse) => {
+          assert.ifError(err);
+          assert.notEqual(bucket, undefined);
+          assert.equal(bucket.name, bucketName);
+          assert.notEqual(apiResponse, undefined);
+          done();
+        });
+      }
+    };
+    StorageMock = sinon.stub().returns(storageMock);
+
+    proxyquire(`../quickstart`, {
+      '@google-cloud/storage': StorageMock
+    });
+  });
+});

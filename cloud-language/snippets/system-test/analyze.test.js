eeca1481d5fb0defd3230952a695b6354feebbfe
Jason Dobry <jmdobry@users.noreply.github.com>
2016-11-15 11:17:26 -0800

    Update Language samples. (#254)
    * Update Language samples.
    
    * Address comments.

--- cloud-language/snippets/system-test/analyze.test.js
+++ cloud-language/snippets/system-test/analyze.test.js
@@ -13,103 +13,64 @@
 
 'use strict';
 
-var uuid = require('node-uuid');
-var path = require('path');
-var storage = require('@google-cloud/storage')();
-var program = require('../analyze');
+const uuid = require(`node-uuid`);
+const path = require(`path`);
+const storage = require(`@google-cloud/storage`)();
+const run = require(`../../utils`).run;
 
-var bucketName = 'nodejs-docs-samples-test-' + uuid.v4();
-var fileName = 'text.txt';
-var localFilePath = path.join(__dirname, '../resources/text.txt');
-var text = 'President Obama is speaking at the White House.';
-var options = {
-  type: 'text'
-};
+const cmd = `node analyze.js`;
+const cwd = path.join(__dirname, `..`);
+const bucketName = `nodejs-docs-samples-test-${uuid.v4()}`;
+const fileName = `text.txt`;
+const localFilePath = path.join(__dirname, `../resources/text.txt`);
+const text = `President Obama is speaking at the White House.`;
 
-describe('language:analyze', function () {
-  before(function (done) {
-    storage.createBucket(bucketName, function (err, bucket) {
-      assert.equal(err, null);
-      bucket.upload(localFilePath, done);
-    });
+describe(`language:analyze`, () => {
+  before(() => {
+    return storage.createBucket(bucketName)
+      .then((results) => results[0].upload(localFilePath));
   });
 
-  after(function (done) {
-    storage.bucket(bucketName).deleteFiles({ force: true }, function (err) {
-      assert.equal(err, null);
-      storage.bucket(bucketName).delete(done);
-    });
+  after(() => {
+    return storage.bucket(bucketName).deleteFiles({ force: true })
+      .then(() => storage.bucket(bucketName).delete());
   });
 
-  describe('analyzeSentimentFromString', function () {
-    it('should analyze sentiment in text', function (done) {
-      program.analyzeSentimentFromString(text, options, function (err, sentiment) {
-        assert.equal(err, null);
-        assert.equal(typeof sentiment, 'object');
-        assert.equal(typeof sentiment.polarity, 'number');
-        assert.equal(typeof sentiment.magnitude, 'number');
-        done();
-      });
-    });
+  it(`should analyze sentiment in text`, () => {
+    assert.equal(run(`${cmd} sentimentOfText "${text}"`, cwd), `Sentiment: positive.`);
   });
 
-  describe('analyzeSentimentFromFile', function () {
-    it('should analyze sentiment in a file', function (done) {
-      program.analyzeSentimentFromFile(bucketName, fileName, options, function (err, sentiment) {
-        assert.equal(err, null);
-        assert.equal(typeof sentiment, 'object');
-        assert.equal(typeof sentiment.polarity, 'number');
-        assert.equal(typeof sentiment.magnitude, 'number');
-        done();
-      });
-    });
+  it(`should analyze sentiment in a file`, () => {
+    assert.equal(run(`${cmd} sentimentInFile ${bucketName} ${fileName}`, cwd), `Sentiment: positive.`);
   });
 
-  describe('analyzeEntitiesFromString', function () {
-    it('should analyze entities in text', function (done) {
-      program.analyzeEntitiesFromString(text, options, function (err, entities) {
-        assert.equal(err, null);
-        assert.equal(typeof entities, 'object');
-        assert.equal(Array.isArray(entities.people), true);
-        assert.equal(Array.isArray(entities.places), true);
-        done();
-      });
-    });
+  it(`should analyze entities in text`, () => {
+    const output = run(`${cmd} entitiesOfText "${text}"`, cwd);
+    assert.equal(output.includes(`Entities:`), true);
+    assert.equal(output.includes(`people:`), true);
+    assert.equal(output.includes(`places:`), true);
   });
 
-  describe('analyzeEntitiesFromFile', function () {
-    it('should analyze entities in a file', function (done) {
-      program.analyzeEntitiesFromFile(bucketName, fileName, options, function (err, entities) {
-        assert.equal(err, null);
-        assert.equal(typeof entities, 'object');
-        assert.equal(Array.isArray(entities.people), true);
-        assert.equal(Array.isArray(entities.places), true);
-        done();
-      });
-    });
+  it('should analyze entities in a file', () => {
+    const output = run(`${cmd} entitiesInFile ${bucketName} ${fileName}`, cwd);
+    assert.equal(output.includes(`Entities:`), true);
+    assert.equal(output.includes(`people:`), true);
+    assert.equal(output.includes(`places:`), true);
   });
 
-  describe('analyzeSyntaxFromString', function () {
-    it('should analyze syntax in text', function (done) {
-      program.analyzeSyntaxFromString(text, options, function (err, syntax) {
-        assert.equal(err, null);
-        assert.equal(typeof syntax, 'object');
-        assert.equal(Array.isArray(syntax.sentences), true);
-        assert.equal(Array.isArray(syntax.tokens), true);
-        done();
-      });
-    });
+  it(`should analyze syntax in text`, () => {
+    const output = run(`${cmd} syntaxOfText "${text}"`, cwd);
+    assert.equal(output.includes(`Tags:`), true);
+    assert.equal(output.includes(`NOUN`), true);
+    assert.equal(output.includes(`VERB`), true);
+    assert.equal(output.includes(`PUNCT`), true);
   });
 
-  describe('analyzeSyntaxFromFile', function () {
-    it('should analyze syntax in a file', function (done) {
-      program.analyzeSyntaxFromFile(bucketName, fileName, options, function (err, syntax) {
-        assert.equal(err, null);
-        assert.equal(typeof syntax, 'object');
-        assert.equal(Array.isArray(syntax.sentences), true);
-        assert.equal(Array.isArray(syntax.tokens), true);
-        done();
-      });
-    });
+  it('should analyze syntax in a file', () => {
+    const output = run(`${cmd} syntaxInFile ${bucketName} ${fileName}`, cwd);
+    assert.equal(output.includes(`Tags:`), true);
+    assert.equal(output.includes(`NOUN`), true);
+    assert.equal(output.includes(`VERB`), true);
+    assert.equal(output.includes(`PUNCT`), true);
   });
 });

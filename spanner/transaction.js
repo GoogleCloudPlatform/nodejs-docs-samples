ccb7c00f1b01ad312e722cc5f5ab8438a75ac031
Jakub Vrana <jakub@vrana.cz>
2017-02-15 16:52:01 +0100

    Remove bogus .value after calling toJSON()

--- spanner/transaction.js
+++ spanner/transaction.js
@@ -122,7 +122,7 @@ function readWriteTransaction (instanceId, databaseId) {
           // Note: MarketingBudget is an INT64, which comes from Cloud Spanner
           // as a string - so we convert it to a number with parseInt()
           const rows = results[0].map((row) => row.toJSON());
-          secondBudget = parseInt(rows[0].MarketingBudget.value);
+          secondBudget = parseInt(rows[0].MarketingBudget);
           console.log(`The second album's marketing budget: ${secondBudget}`);
 
           // Makes sure the second album's budget is sufficient
@@ -136,7 +136,7 @@ function readWriteTransaction (instanceId, databaseId) {
           // Gets first album's budget
           // As above, MarketingBudget is an INT64 and comes as a string
           const rows = results[0].map((row) => row.toJSON());
-          firstBudget = parseInt(rows[0].MarketingBudget.value);
+          firstBudget = parseInt(rows[0].MarketingBudget);
           console.log(`The first album's marketing budget: ${firstBudget}`);
         })
       ]);

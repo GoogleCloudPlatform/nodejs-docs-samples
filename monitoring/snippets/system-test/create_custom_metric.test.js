106b2691248e55c551b142813b5c634490632ebe
Jason Dobry <jmdobry@users.noreply.github.com>
2017-04-24 14:40:11 -0700

    Cleanup App Engine samples and re-work tests. (#354)

--- monitoring/snippets/system-test/create_custom_metric.test.js
+++ monitoring/snippets/system-test/create_custom_metric.test.js
@@ -1,29 +1,32 @@
-// Copyright 2015-2016, Google, Inc.
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//    http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+/**
+ * Copyright 2017, Google, Inc.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 'use strict';
 
-require(`../../system-test/_setup`);
-
 const customMetricsExample = require('../create_custom_metric');
+const test = require(`ava`);
+const tools = require(`@google-cloud/nodejs-repo-tools`);
 
 /** Refactored out to keep lines shorter */
 function getPointValue (timeSeries) {
   return timeSeries.timeSeries[0].points[0].value.int64Value;
 }
 
-test.before(stubConsole);
-test.after.always(restoreConsole);
+test.before(tools.checkCredentials);
+test.before(tools.stubConsole);
+test.after.always(tools.restoreConsole);
 
 // TODO(anassri): Fix results[2] being undefined
 test.skip.cb('should create and read back a custom metric', (t) => {

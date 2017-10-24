85b4219287e21530840e130eecaae316431cc483
Luke Sneeringer <luke@sneeringer.com>
2017-10-24 12:13:22 -0700

    Repo Migration (#4)

--- video-intelligence/system-test/analyze.test.js
+++ video-intelligence/system-test/analyze.test.js
@@ -29,7 +29,8 @@ const shortUrl = `gs://nodejs-docs-samples-video/quickstart_short.mp4`;
 const file = `resources/cat.mp4`;
 
 // analyze_faces
-test.serial(`should analyze faces in a GCS file`, async (t) => {
+// This is under whitelist and therefore will fail in most circumstances.
+test.skip(`should analyze faces in a GCS file`, async t => {
   const output = await tools.runAsync(`${cmd} faces ${url}`, cwd);
   t.regex(output, /Thumbnail size: \d+/);
   t.regex(output, /Start:.*\d+\.\d+s/);
@@ -37,40 +38,46 @@ test.serial(`should analyze faces in a GCS file`, async (t) => {
 });
 
 // analyze_labels_gcs (one scene)
-test.serial(`should analyze labels in a GCS file with one scene`, async (t) => {
+test.serial(`should analyze labels in a GCS file with one scene`, async t => {
   const output = await tools.runAsync(`${cmd} labels-gcs ${shortUrl}`, cwd);
   t.regex(output, /Label shirt occurs at:/);
   t.regex(output, /Confidence: \d+\.\d+/);
 });
 
 // analyze_labels_gcs (multiple scenes)
-test.serial(`should analyze labels in a GCS file with multiple scenes`, async (t) => {
-  const output = await tools.runAsync(`${cmd} labels-gcs ${url}`, cwd);
-  t.regex(output, /Label shirt occurs at:/);
-  t.regex(output, /Confidence: \d+\.\d+/);
-});
+test.serial(
+  `should analyze labels in a GCS file with multiple scenes`,
+  async t => {
+    const output = await tools.runAsync(`${cmd} labels-gcs ${url}`, cwd);
+    t.regex(output, /Label shirt occurs at:/);
+    t.regex(output, /Confidence: \d+\.\d+/);
+  }
+);
 
 // analyze_labels_local
-test.serial(`should analyze labels in a local file`, async (t) => {
+test.serial(`should analyze labels in a local file`, async t => {
   const output = await tools.runAsync(`${cmd} labels-file ${file}`, cwd);
   t.regex(output, /Label whiskers occurs at:/);
   t.regex(output, /Confidence: \d+\.\d+/);
 });
 
 // analyze_shots (multiple shots)
-test.serial(`should analyze shots in a GCS file with multiple shots`, async (t) => {
-  const output = await tools.runAsync(`${cmd} shots ${url}`, cwd);
-  t.regex(output, /Scene 0 occurs from:/);
-});
+test.serial(
+  `should analyze shots in a GCS file with multiple shots`,
+  async t => {
+    const output = await tools.runAsync(`${cmd} shots ${url}`, cwd);
+    t.regex(output, /Scene 0 occurs from:/);
+  }
+);
 
 // analyze_shots (one shot)
-test.serial(`should analyze shots in a GCS file with one shot`, async (t) => {
+test.serial(`should analyze shots in a GCS file with one shot`, async t => {
   const output = await tools.runAsync(`${cmd} shots ${shortUrl}`, cwd);
   t.regex(output, /The entire video is one shot./);
 });
 
 // analyze_safe_search
-test.serial(`should analyze safe search results in a GCS file`, async (t) => {
+test.serial(`should analyze safe search results in a GCS file`, async t => {
   const output = await tools.runAsync(`${cmd} safe-search ${url}`, cwd);
   t.regex(output, /Time: \d+\.\d+s/);
   t.regex(output, /Explicit annotation results:/);

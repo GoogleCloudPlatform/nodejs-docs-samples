9ad005b553ebb02396b4aa81ed0d5a2888d6d1a6
carieshmarie <leecaroline@google.com>
2018-01-09 13:52:54 -0800

    Update app.js (#548)
    Update environment variable

--- appengine/pubsub/app.js
+++ appengine/pubsub/app.js
@@ -24,8 +24,8 @@ const process = require('process'); // Required for mocking environment variable
 
 // By default, the client will authenticate using the service account file
 // specified by the GOOGLE_APPLICATION_CREDENTIALS environment variable and use
-// the project specified by the GCLOUD_PROJECT environment variable. See
-// https://googlecloudplatform.github.io/gcloud-node/#/docs/google-cloud/latest/guides/authentication
+// the project specified by the GOOGLE_CLOUD_PROJECT environment variable. See
+// https://github.com/GoogleCloudPlatform/google-cloud-node/blob/master/docs/authentication
 // These environment variables are set automatically on Google App Engine
 const PubSub = require('@google-cloud/pubsub');
 

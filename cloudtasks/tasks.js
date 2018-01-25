1873978e5dbb0282075655fae79ad68ca8a1268b
Andrew Gorcester <andrew.gorcester@gmail.com>
2018-01-25 10:39:29 -0800

    Fix underscore/space confusion in tasks tags

--- cloudtasks/tasks.js
+++ cloudtasks/tasks.js
@@ -70,7 +70,7 @@ function createTask (project, location, queue) {
 }
 
 function pullTask (project, location, queue) {
-  // [START cloud_tasks_pull_and acknowledge_task]
+  // [START cloud_tasks_pull_and_acknowledge_task]
   const google = require('googleapis');
   const cloudtasks = google.cloudtasks('v2beta2');
 
@@ -163,7 +163,7 @@ function acknowledgeTask (task) {
       callback(authClient);
     });
   }
-  // [END cloud_tasks_pull_and acknowledge_task]
+  // [END cloud_tasks_pull_and_acknowledge_task]
 }
 
 require(`yargs`) // eslint-disable-line

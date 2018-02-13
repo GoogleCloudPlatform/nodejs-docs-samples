e11a1a6d02f109f29beb50999b6598516dfe9970
ArieFish <15861456+ArieFish@users.noreply.github.com>
2018-02-13 11:33:09 -0500

    Bug fix for helloGCSGeneric()
    This function tried to reference 'event.context', which is 'undefined' when this function is triggered from a GCS change, causing helloGCSGeneric() to crash.

--- functions/helloworld/index.js
+++ functions/helloworld/index.js
@@ -103,10 +103,9 @@ exports.helloGCS = (event, callback) => {
  */
 exports.helloGCSGeneric = (event, callback) => {
   const file = event.data;
-  const context = event.context;
 
-  console.log(`Event ${context.eventId}`);
-  console.log(`  Event Type: ${context.eventType}`);
+  console.log(`  Event ${event.eventId}`);
+  console.log(`  Event Type: ${event.eventType}`);
   console.log(`  Bucket: ${file.bucket}`);
   console.log(`  File: ${file.name}`);
   console.log(`  Metageneration: ${file.metageneration}`);

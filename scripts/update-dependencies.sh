c85b87288e1bb592733638b5cafebe08bf0dca5f
Ace Nassri <anassri@google.com>
2018-03-05 15:14:53 -0800

    Update dependencies

--- scripts/update-dependencies.sh
+++ scripts/update-dependencies.sh
@@ -20,6 +20,6 @@
 #   Update dependencies in node_modules (yarn upgrade)
 #   Run tests, and skip that directory if they fail (yarn test -- --fail-fast)
 #   If the directory has not been skipped, add 'yarn.lock' and 'package.json' to the current commit
-find . -name "package.json" -not -path "*/node_modules/*" -execdir sh -c "ncu -u && ! git diff --quiet --exit-code . && npm install && npm test -- --fail-fast && git add package.json" \;
+find . -name "package.json" -not -path "*/node_modules/*" -execdir sh -c "ncu -u && ! git diff --quiet --exit-code . && npm install && npm test && git add package.json" \;
 
 # Note: this script deliberately avoids including broken dependencies.

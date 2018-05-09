4e23a7e8f8a9464ce24b0e0d4875ba8eef9fe0b7
F. Hinkelmann <fhinkel@vt.edu>
2018-05-09 19:40:30 -0400

    Add sample demonstrating startup script (#80)
    Add sample demonstrating startup script
    
    Add a sample that demonstrates how to create a VM
    and install Apache and a custom homepage on startup.
    
    Drive-by fix: Use lint config generated by repo-tools, do not use
    Node 8 features like destructuring.

---
+++ compute/startup-script/index.js
@@ -0,0 +1,145 @@
+/**
+ * Copyright 2018, Google, Inc.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+'use strict';
+
+const Compute = require('@google-cloud/compute');
+const http = require('http');
+
+const compute = new Compute();
+
+const zone = compute.zone('us-central1-a');
+
+// callback(error, externalIp)
+function createVm(name, callback) {
+  // Create a new VM, using default ubuntu image. The startup script
+  // installs apache and a custom homepage.
+  const config = {
+    os: 'ubuntu',
+    http: true,
+    metadata: {
+      items: [
+        {
+          key: 'startup-script',
+          value: `#! /bin/bash
+
+        # Installs apache and a custom homepage
+        apt-get update
+        apt-get install -y apache2
+        cat <<EOF > /var/www/html/index.html
+        <html><body><h1>Hello World</h1>
+          <p>This page was created from a simple start up script!</p>
+        </body></html>`,
+        },
+      ],
+    },
+  };
+
+  const vm = zone.vm(name);
+
+  vm
+    .create(config)
+    .then(data => {
+      const operation = data[1];
+      return operation.promise();
+    })
+    .then(() => {
+      return vm.getMetadata();
+    })
+    .then(data => {
+      const metadata = data[0];
+
+      // External IP of the VM.
+      const ip = metadata['networkInterfaces'][0]['accessConfigs'][0]['natIP'];
+      console.log('Booting new VM with IP http://' + ip + '...');
+
+      // Ping the VM to determine when the HTTP server is ready.
+      const timer = setInterval(
+        ip => {
+          http
+            .get('http://' + ip, res => {
+              const statusCode = res.statusCode;
+              if (statusCode === 200) {
+                clearTimeout(timer);
+                // HTTP server is ready.
+                console.log('Ready!');
+                callback(null, ip);
+              }
+            })
+            .on('error', () => {
+              // HTTP server is not ready yet.
+              process.stdout.write('.');
+            });
+        },
+        2000,
+        ip
+      );
+    })
+    .catch(err => callback(err));
+}
+
+// List all VMs and their external IPs in a given zone.
+// callback(error, [[name, ip], [name, ip], ...])
+function listVms(callback) {
+  zone
+    .getVMs()
+    .then(data => {
+      const vms = data[0];
+      let results = vms.map(vm => vm.getMetadata());
+      return Promise.all(results);
+    })
+    .then(res =>
+      callback(
+        null,
+        res.map(data => {
+          return {
+            ip: data[0]['networkInterfaces'][0]['accessConfigs']
+              ? data[0]['networkInterfaces'][0]['accessConfigs'][0]['natIP']
+              : 'no external ip',
+            name: data[0].name,
+          };
+        })
+      )
+    )
+    .catch(err => callback(err));
+}
+
+function deleteVm(name, callback) {
+  const vm = zone.vm(name);
+  vm
+    .delete()
+    .then(data => {
+      console.log('Deleting ...');
+      const operation = data[0];
+      return operation.promise();
+    })
+    .then(() => {
+      // VM deleted
+      callback(null, name);
+    })
+    .catch(err => callback(err));
+}
+
+exports.create = (name, cb) => {
+  createVm(name, cb);
+};
+
+exports.list = cb => {
+  listVms(cb);
+};
+
+exports.delete = (name, cb) => {
+  deleteVm(name, cb);
+};
---
+++ compute/startup-script/system-test/index.test.js
@@ -0,0 +1,58 @@
+/**
+ * Copyright 2018, Google, Inc.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+'use strict';
+
+const test = require(`ava`);
+const tools = require(`@google-cloud/nodejs-repo-tools`);
+const uuid = require('uuid');
+
+const example = require(`../index`);
+
+test.before(tools.checkCredentials);
+test.beforeEach(tools.stubConsole);
+test.afterEach.always(tools.restoreConsole);
+
+test.cb(`should list vms`, t => {
+  example.list((err, result) => {
+    t.ifError(err);
+    t.truthy(result);
+    t.true(Array.isArray(result));
+    t.end();
+  });
+});
+
+test.cb(`should create vm`, t => {
+  var TESTS_PREFIX = 'gcloud-tests-';
+  const name = generateName('vm-with-apache');
+
+  function generateName(customPrefix) {
+    return [TESTS_PREFIX, customPrefix + '-', uuid.v4().replace('-', '')]
+      .join('')
+      .substr(0, 61);
+  }
+
+  example.create(name, (err, result) => {
+    t.ifError(err);
+    t.truthy(result);
+
+    // Clean up newly created vm.
+    example.delete(name, (err, result) => {
+      t.ifError(err);
+      t.truthy(result);
+      t.end();
+    });
+  });
+});

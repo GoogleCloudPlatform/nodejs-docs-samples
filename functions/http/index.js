c9b892f6d72751da484b090a7e7aadaa0ddb7932
Ace Nassri <anassri@google.com>
2018-05-30 16:57:59 -0700

    Change HTTP sample comments

--- functions/http/index.js
+++ functions/http/index.js
@@ -19,15 +19,16 @@
 /**
  * Responds to any HTTP request that can provide a "message" field in the body.
  *
- * @param {Object} req Cloud Function request context.
- * @param {Object} res Cloud Function response context.
+ * @param {Object} req ExpresJS object containing the received HTTP request.
+ * @param {Object} res ExpressJS object containing the HTTP response to send.
  */
 exports.helloWorld = (req, res) => {
   if (req.body.message === undefined) {
     // This is an error case, as "message" is required
     res.status(400).send('No message defined!');
   } else {
-    // Everything is ok
+    // Everything is ok - call request-terminating method to signal function
+    // completion. (Otherwise, the function may continue to run until timeout.)
     console.log(req.body.message);
     res.status(200).end();
   }

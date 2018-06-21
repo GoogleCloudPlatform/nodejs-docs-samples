fad659a1073249074c1c029307ca662464e2dfcd
Averi Kitsch <aekitsch@gmail.com>
2018-06-21 15:11:55 -0700

    updated with client library

--- appengine/cloudtasks/createTask.js
+++ appengine/cloudtasks/createTask.js
@@ -1,105 +1,109 @@
 /**
- * Copyright 2017, Google, Inc.
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *    http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+* Copyright 2018, Google, Inc.
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*    http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
 
 'use strict';
 
-const {google} = require('googleapis');
-const cloudtasks = google.cloudtasks('v2beta2');
-
 /**
- * Create a task for a given queue with an arbitrary payload.
- */
+* Create a task for a given queue with an arbitrary payload.
+*/
 function createTask (project, location, queue, options) {
   // [START cloud_tasks_appengine_create_task]
-  google.auth.getClient({
-    scopes: ['https://www.googleapis.com/auth/cloud-platform']
-  }).then(authClient => {
-    const task = {
-      app_engine_http_request: {
-        http_method: 'POST',
-        relative_url: '/log_payload'
-      }
-    };
+  // Imports the Google Cloud Tasks library.
+  const cloudTasks = require('@google-cloud/tasks');
 
-    if (options.payload !== undefined) {
-      task.app_engine_http_request.payload = Buffer.from(options.payload).toString('base64');
+  // Instantiates a client.
+  const client = new cloudTasks.CloudTasksClient();
+
+  // Construct the fully qualified queue name.
+  const parent = client.queuePath(project, location, queue);
+
+  const task = {
+    appEngineHttpRequest: {
+      httpMethod: 'POST',
+      relativeUrl: '/log_payload'
     }
+  };
 
-    if (options.inSeconds !== undefined) {
-      task.schedule_time = (new Date(options.inSeconds * 1000 + Date.now())).toISOString();
-    }
+  if (options.payload !== undefined) {
+    task.appEngineHttpRequest.payload = Buffer.from(options.payload).toString('base64');
+  }
 
-    const request = {
-      parent: `projects/${project}/locations/${location}/queues/${queue}`, // TODO: Update placeholder value.
-      resource: {
-        task: task
-      },
-      auth: authClient
+  if (options.inSeconds !== undefined) {
+    task.scheduleTime = {
+      seconds: (options.inSeconds + Date.now() / 1000)
     };
+  }
 
-    console.log('Sending task %j', task);
-    return cloudtasks.projects.locations.queues.tasks.create(request);
-  }).then(response => {
-    console.log('Created task.', response.name);
-    console.log(JSON.stringify(response, null, 2));
-  }).catch(console.error);
-  // [END cloud_tasks_appengine_create_task]
+  const request = {
+    parent: parent,
+    task: task
+  };
+
+  console.log('Sending task %j', task);
+  // Send create task request.
+  client.createTask(request).then(response => {
+    const task = response[0].name;
+    console.log(`Created task ${task}`);
+  }).catch(err => {
+    console.error(`Error in createTask: ${err.message || err}`);
+  });
+// [END cloud_tasks_appengine_create_task]
 }
 
 const cli = require(`yargs`)
-  .options({
-    location: {
-      alias: 'l',
-      description: 'Location of the queue to add the task to.',
-      type: 'string',
-      requiresArg: true,
-      required: true
-    },
-    queue: {
-      alias: 'q',
-      description: 'ID (short name) of the queue to add the task to.',
-      type: 'string',
-      requiresArg: true,
-      required: true
-    },
-    project: {
-      alias: 'p',
-      description: 'Project of the queue to add the task to.',
-      default: process.env.GCLOUD_PROJECT,
-      type: 'string',
-      requiresArg: true,
-      required: true
-    },
-    payload: {
-      alias: 'd',
-      description: '(Optional) Payload to attach to the push queue.',
-      type: 'string',
-      requiresArg: true
-    },
-    inSeconds: {
-      alias: 's',
-      description: '(Optional) The number of seconds from now to schedule task attempt.',
-      type: 'number',
-      requiresArg: true
-    }
-  })
-  .example(`node $0 --project my-project-id`)
-  .wrap(120)
-  .recommendCommands()
-  .epilogue(`For more information, see https://cloud.google.com/cloud-tasks`)
-  .strict();
+.options({
+  location: {
+    alias: 'l',
+    description: 'Location of the queue to add the task to.',
+    type: 'string',
+    requiresArg: true,
+    required: true
+  },
+  queue: {
+    alias: 'q',
+    description: 'ID (short name) of the queue to add the task to.',
+    type: 'string',
+    requiresArg: true,
+    required: true
+  },
+  project: {
+    alias: 'p',
+    description: 'Project of the queue to add the task to.',
+    default: process.env.GCLOUD_PROJECT,
+    type: 'string',
+    requiresArg: true,
+    required: true
+  },
+  payload: {
+    alias: 'd',
+    description: '(Optional) Payload to attach to the push queue.',
+    type: 'string',
+    requiresArg: true
+  },
+  inSeconds: {
+    alias: 's',
+    description: '(Optional) The number of seconds from now to schedule task attempt.',
+    type: 'number',
+    requiresArg: true
+  }
+})
+.example(`node $0 --project my-project-id`)
+.wrap(120)
+.recommendCommands()
+.epilogue(`For more information, see https://cloud.google.com/cloud-tasks`)
+.strict();
 
 if (module === require.main) {
   const opts = cli.help().parse(process.argv.slice(2));

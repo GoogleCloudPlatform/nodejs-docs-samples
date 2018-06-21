141d2e4deb1b6bc89cbcc9488f65e34083705b13
F. Hinkelmann <fhinkel@vt.edu>
2018-06-21 15:10:46 -0400

    Add mailjet and sendgrid back in (#110)
    Mostly a revert of 518d762fb001e230e5fbd2638bbaa45f77858c2d
    
    Samples are needed for pages in the documentation.

---
+++ compute/test/sendgrid.test.js
@@ -0,0 +1,58 @@
+/**
+ * Copyright 2017, Google, Inc.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+'use strict';
+
+const proxyquire = require(`proxyquire`).noPreserveCache();
+const test = require(`ava`);
+const tools = require(`@google-cloud/nodejs-repo-tools`);
+
+process.env.SENDGRID_API_KEY = `foo`;
+
+test.beforeEach(tools.stubConsole);
+test.afterEach.always(tools.restoreConsole);
+
+test.cb(`should send an email`, t => {
+  proxyquire(`../sendgrid`, {
+    sendgrid: key => {
+      t.is(key, `foo`);
+      return {
+        emptyRequest: x => x,
+        API: request => {
+          t.deepEqual(request, {
+            method: `POST`,
+            path: `/v3/mail/send`,
+            body: {
+              personalizations: [
+                {
+                  to: [{email: `to_email@example.com`}],
+                  subject: `Sendgrid test email from Node.js on Google Cloud Platform`,
+                },
+              ],
+              from: {email: `from_email@example.com`},
+              content: [
+                {
+                  type: `text/plain`,
+                  value: `Hello!\n\nThis a Sendgrid test email from Node.js on Google Cloud Platform.`,
+                },
+              ],
+            },
+          });
+          t.end();
+        },
+      };
+    },
+  });
+});

43494aab13d26d0446858c8cbb62a7ac8eec8bac
Jeffrey Rennie <rennie@google.com>
2018-06-22 14:44:07 -0700

    Correctly generates configs.

--- jeff.ps1
+++ jeff.ps1
@@ -1,18 +1,26 @@
-$pj = gci -Recurse package.json
-foreach ($json in $pj) {
-     $package = $pj | ConvertFrom-Json
-     if ($package.scripts.test ) { # -or test or system_test subdirectory is present.
-         # Generate path/../.cfg  Replace /s with -s.
-         # Example foo/bar/package.json => foo-bar.cfg
-         # Add to .kokoro/.
-    } else {
-        # Look for subfolder test or tests...
-        # Repeat above.
-     }
+# A script for inspecting the source tree and generating test configs.
+$packageJsons = Get-ChildItem -Recurse package.json
+$testDirs = Get-ChildItem -Recurse *test | Where-Object { Test-Path -Path $_ -PathType Container}
+
+function Collect-Names {
+    # Use a dictionary to de-duplicate the directories.
+    $names = @{}
+    foreach ($item in $input) {
+        $relPath = [string] (Resolve-Path -Relative (Split-Path -Parent $item))
+        # replace slashes with hyphenes.
+        $name = $relPath.Replace('/', '-')
+        $name = $relPath.Replace('\', '-')
+        # Remove the '.-' at the beginning of the string.
+        $name = $name.Substring(2)
+        $names[$name] = $true
+    }
+    return $names.Keys
 }
 
+$names = ($packageJsons + $testDirs | Collect-Names)
 
 # Replace cloudtasks with "foo/bar"
+$template = @'
 # Format: //devtools/kokoro/config/proto/build.proto
 
 # Set the folder in which the tests are run
@@ -25,4 +33,10 @@ env_vars: {
 env_vars: {
     key: "TRAMPOLINE_BUILD_FILE"
     value: "github/nodejs-docs-samples/.kokoro/build.sh"
+}
+'@
+
+foreach ($name in $names) {
+    $template.Replace('cloudtasks', $name) `
+        | Out-File -FilePath ".kokoro/$name.cfg" -Encoding utf8 
 }

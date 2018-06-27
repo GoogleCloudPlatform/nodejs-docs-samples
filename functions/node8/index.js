27d58d5d52376c102b88b777d3326bed7b695c07
Ace Nassri <anassri@google.com>
2018-06-27 12:52:07 -0700

    Add region tags

--- functions/node8/index.js
+++ functions/node8/index.js
@@ -5,6 +5,8 @@
  * @param {object} event The Cloud Functions event.
  * @param {function} callback The callback function.
  */
+
+// [START functions_tips_retry_node8]
 exports.avoidInfiniteRetries = async (event) => {
   const eventAge = Date.now() - Date.parse(event.timestamp);
   const eventMaxAge = 10000;
@@ -18,3 +20,4 @@ exports.avoidInfiniteRetries = async (event) => {
   // Do what the function is supposed to do
   console.log(`Processing event ${event} with age ${eventAge} ms.`);
 };
+// [END functions_tips_retry_node8]

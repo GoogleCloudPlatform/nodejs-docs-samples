1453f75ab4e8290065ddcd8e85cb93c4210e308b
Ace Nassri <anassri@google.com>
2018-07-23 13:08:46 -0700

    Make logging sample an HTTP function (#691)
    * Update index.js
    
    * Clarify mocking comments (#682)
    
    * fix: just adding : for log consistency (#694)
    
    * Update index.js
    
    * Update index.js
    
    * Update index.js
    
    * Fix test

--- functions/log/index.js
+++ functions/log/index.js
@@ -16,10 +16,10 @@
 'use strict';
 
 // [START functions_log_helloworld]
-exports.helloWorld = (event, callback) => {
+exports.helloWorld = (req, res) => {
   console.log('I am a log entry!');
   console.error('I am an error!');
-  callback();
+  res.end();
 };
 // [END functions_log_helloworld]
 

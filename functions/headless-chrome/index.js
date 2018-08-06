65f4240c01510eee7bda16e5498892ed96ffff60
Steren <steren.giannini@gmail.com>
2018-08-06 08:54:25 -0700

    await browser close

--- functions/headless-chrome/index.js
+++ functions/headless-chrome/index.js
@@ -32,7 +32,7 @@ exports.screenshot = async (req, res) => {
 
   await page.goto(url);
   const imageBuffer = await page.screenshot();
-  browser.close();
+  await browser.close();
 
   res.set('Content-Type', 'image/png');
   res.send(imageBuffer);

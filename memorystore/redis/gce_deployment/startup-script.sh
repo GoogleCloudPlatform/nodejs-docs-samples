04e20bc6b05b1d3bc90d994a227c178037b89a51
Jisha Abubaker <jishaa@google.com>
2018-08-10 11:17:56 -0700

    Fixing GCE deployment script for Cloud Memorystore

--- memorystore/redis/gce_deployment/startup-script.sh
+++ memorystore/redis/gce_deployment/startup-script.sh
@@ -34,7 +34,7 @@ apt-get install -yq ca-certificates supervisor nodejs build-essential
 curl -s "https://storage.googleapis.com/signals-agents/logging/google-fluentd-install.sh" | bash
 service google-fluentd restart &
 
-gsutil cp $GCS_APP_LOCATION /app.tar
+gsutil cp "$GCS_APP_LOCATION"** /
 mkdir -p /app
 tar -x -f /app.tar -C /app
 cd /app

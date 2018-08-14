cca79ce727213ec6e180274867ef55dc9d5a1e23
Ace Nassri <anassri@google.com>
2018-08-14 11:57:35 -0700

    Initial commit of Typescript sample

---
+++ appengine/typescript/index.ts
@@ -0,0 +1,35 @@
+// Copyright 2018 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the 'License');
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an 'AS IS' BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+/* tslint:disable:no-console no-var-requires */
+
+declare var require: any;
+declare var process: {
+  env: {
+      PORT: string,
+  },
+};
+
+const PORT = process.env.PORT || 8080;
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("🎉 Hello TypeScript! 🎉");
+});
+
+const server = app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`);
+});

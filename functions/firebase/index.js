4e4247a2bfb01813ff554c296c391a8d4a65ccf2
Ace Nassri <anassri@google.com>
2018-08-22 17:33:52 -0700

    Node 6: add Firebase snippets (#697)
    * Firebase snippets: initial commit
    
    * Remove analytics sample

---
+++ functions/firebase/index.js
@@ -0,0 +1,63 @@
+// [START functions_firebase_rtdb]
+/**
+ * Triggered by a change to a Firebase RTDB reference.
+ *
+ * @param {!Object} event The Cloud Functions event.
+ * @param {!Function} The callback function.
+ */
+exports.helloRTDB = (event, callback) => {
+  const triggerResource = event.resource;
+
+  console.log(`Function triggered by change to: ${triggerResource}`);
+  console.log(`Admin?: ${!!event.auth.admin}`);
+  console.log(`Delta:`);
+  console.log(JSON.stringify(event.delta, null, 2));
+
+  // Don't forget to call the callback.
+  callback();
+};
+// [END functions_firebase_rtdb]
+
+// [START functions_firebase_firestore]
+/**
+ * Triggered by a change to a Firestore document.
+ *
+ * @param {!Object} event The Cloud Functions event.
+ * @param {!Function} The callback function.
+ */
+exports.helloFirestore = (event, callback) => {
+  const triggerResource = event.resource;
+
+  // We're just going to log the resource string and the
+  // full event to prove that it worked.
+  console.log(`Function triggered by change to: ${triggerResource}`);
+  console.log(JSON.stringify(event));
+
+  // Don't forget to call the callback.
+  callback();
+};
+// [END functions_firebase_firestore]
+
+// [START functions_firebase_auth]
+/**
+ * Triggered by a change to a Firebase Auth user object.
+ *
+ * @param {!Object} event The Cloud Functions event.
+ * @param {!Function} The callback function.
+ */
+exports.helloAuth = (event, callback) => {
+  try {
+    const data = event.data;
+    console.log(`Function triggered by change to user: ${data.uid}`);
+    console.log(`Created at: ${data.metadata.createdAt}`);
+
+    if (event.data.email) {
+      console.log(`Email: ${data.email}`);
+    }
+  } catch (err) {
+    console.error(err);
+  }
+  // Don't forget to call the callback.
+  callback();
+};
+// [END functions_firebase_auth]

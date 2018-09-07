ebb42e716363dc5fac2183c2a187522c0f9b6e56
Ace Nassri <anassri@google.com>
2018-09-07 11:05:45 -0700

    Add code to start GCF emulator

--- .kokoro/build.sh
+++ .kokoro/build.sh
@@ -30,6 +30,11 @@ export GOOGLE_APPLICATION_CREDENTIALS=${KOKORO_GFILE_DIR}/secrets-key.json
 gcloud auth activate-service-account --key-file "$GOOGLE_APPLICATION_CREDENTIALS"
 gcloud config set project $GCLOUD_PROJECT
 
+# Start functions emulator, if appropriate
+if [[ $PROJECT == "functions/*" ]]; then
+  functions-emulator start
+fi
+
 npm test
 
 exit $?

d754fa5099721d9e3e6264c0aea7979ed98761b2
Nirupa Anantha Kumar <nirupa-kumar@users.noreply.github.com>
2018-10-15 14:10:13 -0700

    fix(samples): Adding vision_product_search_tutorial_import tags (#221)

--- vision/samples/productSearch/importProductSets.v1p3beta1.js
+++ vision/samples/productSearch/importProductSets.v1p3beta1.js
@@ -17,7 +17,9 @@
 // [START vision_product_search_import_product_images]
 function importProductSets(projectId, location, gcsUri) {
   // Imports the Google Cloud client library
+  // [START vision_product_search_tutorial_import]
   const vision = require('@google-cloud/vision').v1p3beta1;
+  // [END vision_product_search_tutorial_import]
   // Creates a client
   const client = new vision.ProductSearchClient();
 

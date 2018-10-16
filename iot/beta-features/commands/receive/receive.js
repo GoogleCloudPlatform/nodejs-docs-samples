0a8bdc13bed546fc9f8b4a647ef28c0d135a3dc0
Alex Hong <alexanderhong.2@gmail.com>
2018-10-16 15:33:16 -0700

    add iot commands beta samples & tests (#767)
    * add iot commands beta samples & tests
    * update copyright header
    * add disclaimer to commands beta resources
    * update nodejs-repo-tools version number

---
+++ iot/beta-features/commands/receive/receive.js
@@ -0,0 +1,162 @@
+/**
+ * Copyright 2018 Google LLC
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+'use strict';
+
+// [START iot_mqtt_include]
+const fs = require('fs');
+const jwt = require('jsonwebtoken');
+const mqtt = require('mqtt');
+// [END iot_mqtt_include]
+
+console.log('Google Cloud IoT Core MQTT example.');
+let argv = require(`yargs`)
+  .options({
+    projectId: {
+      default: process.env.GCLOUD_PROJECT || process.env.GOOGLE_CLOUD_PROJECT,
+      description: 'The Project ID to use. Defaults to the value of the GCLOUD_PROJECT or GOOGLE_CLOUD_PROJECT environment variables.',
+      requiresArg: true,
+      type: 'string'
+    },
+    cloudRegion: {
+      default: 'us-central1',
+      description: 'GCP cloud region.',
+      requiresArg: true,
+      type: 'string'
+    },
+    registryId: {
+      description: 'Cloud IoT registry ID.',
+      requiresArg: true,
+      demandOption: true,
+      type: 'string'
+    },
+    deviceId: {
+      description: 'Cloud IoT device ID.',
+      requiresArg: true,
+      demandOption: true,
+      type: 'string'
+    },
+    privateKeyFile: {
+      description: 'Path to private key file.',
+      requiresArg: true,
+      demandOption: true,
+      type: 'string'
+    },
+    algorithm: {
+      description: 'Encryption algorithm to generate the JWT.',
+      requiresArg: true,
+      demandOption: true,
+      choices: ['RS256', 'ES256'],
+      type: 'string'
+    },
+    maxDuration: {
+      default: -1,
+      description: 'Max number of minutes to run before ending the client. Set to -1 for no maximum',
+      requiresArg: true,
+      type: 'number'
+    },
+    tokenExpMins: {
+      default: 20,
+      description: 'Minutes to JWT token expiration.',
+      requiresArg: true,
+      type: 'number'
+    },
+    mqttBridgeHostname: {
+      default: 'mqtt.googleapis.com',
+      description: 'MQTT bridge hostname.',
+      requiresArg: true,
+      type: 'string'
+    },
+    mqttBridgePort: {
+      default: 8883,
+      description: 'MQTT bridge port.',
+      requiresArg: true,
+      type: 'number'
+    }
+  })
+  .example(`node $0 --projectId=blue-jet-123 \\\n\t--registryId=my-registry --deviceId=my-node-device \\\n\t--privateKeyFile=../rsa_private.pem --algorithm=RS256 \\\n\t --cloudRegion=us-central1`)
+  .wrap(120)
+  .recommendCommands()
+  .epilogue(`For more information, see https://cloud.google.com/iot-core/docs`)
+  .help()
+  .strict()
+  .argv;
+
+// Create a Cloud IoT Core JWT for the given project id, signed with the given
+// private key.
+// [START iot_mqtt_jwt]
+function createJwt (projectId, privateKeyFile, algorithm) {
+  const token = {
+    'iat': parseInt(Date.now() / 1000),
+    'exp': parseInt(Date.now() / 1000) + 20 * 60, // 20 minutes
+    'aud': projectId
+  };
+  const privateKey = fs.readFileSync(privateKeyFile);
+  return jwt.sign(token, privateKey, { algorithm: algorithm });
+}
+// [END iot_mqtt_jwt]
+
+// [START iot_mqtt_run]
+const mqttClientId = `projects/${argv.projectId}/locations/${argv.cloudRegion}/registries/${argv.registryId}/devices/${argv.deviceId}`;
+let connectionArgs = {
+  host: argv.mqttBridgeHostname,
+  port: argv.mqttBridgePort,
+  clientId: mqttClientId,
+  username: 'unused',
+  password: createJwt(argv.projectId, argv.privateKeyFile, argv.algorithm),
+  protocol: 'mqtts',
+  qos: 1,
+  secureProtocol: 'TLSv1_2_method'
+};
+
+// Create a client, and connect to the Google MQTT bridge.
+let client = mqtt.connect(connectionArgs);
+
+// Subscribe to the /devices/{device-id}/commands topic to receive commands.
+client.subscribe(`/devices/${argv.deviceId}/commands/#`);
+
+if (argv.maxDuration > 0) {
+  setTimeout(() => {
+    console.log(`Closing connection to MQTT after ${argv.maxDuration} seconds.`);
+    client.end();
+  }, argv.maxDuration * 60 * 1000);
+}
+
+client.on('connect', (success) => {
+  console.log('connect');
+  if (!success) {
+    console.log('Client not connected...');
+  } else {
+    // TODO: wait for commands
+    console.log('Client connected, waiting for commands');
+  }
+});
+
+client.on('close', () => {
+  console.log('close');
+});
+
+client.on('error', (err) => {
+  console.log('error', err);
+});
+
+client.on('message', (topic, message, packet) => {
+  console.log('message received: ', Buffer.from(message, 'base64').toString('ascii'));
+});
+
+client.on('packetsend', () => {
+  // Note: logging packet send is very verbose
+});
+// [END iot_mqtt_run]
---
+++ iot/beta-features/commands/receive/receiveTest.js
@@ -0,0 +1,77 @@
+/**
+ * Copyright 2018 Google LLC
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+'use strict';
+
+const path = require(`path`);
+const PubSub = require(`@google-cloud/pubsub`);
+const test = require(`ava`);
+const tools = require(`@google-cloud/nodejs-repo-tools`);
+const uuid = require(`uuid`);
+
+const topicName = `nodejs-docs-samples-test-iot-${uuid.v4()}`;
+const registryName = `nodejs-test-registry-iot-${uuid.v4()}`;
+const localRegName = `${registryName}-rsa256`;
+const localDevice = `test-device-rsa256`;
+
+const helper = `node manager.js`;
+const receiveCmd = `node receive.js`;
+const sendCmd = `node send.js`;
+const receiveCmdSuffix = `--privateKeyFile=resources/rsa_private.pem --algorithm=RS256`;
+// TODO: update manager paths when going from beta->GA
+const cwdHelper = path.join(__dirname, `../../../manager`);
+const cwdSend = path.join(__dirname, `../send`);
+const installDeps = `npm install`;
+
+const pubsub = PubSub();
+
+test.before(tools.checkCredentials);
+test.before(async () => {
+  let pubsubRes = await pubsub.createTopic(topicName);
+  const topic = pubsubRes[0];
+  console.log(`Topic ${topic.name} created.`);
+});
+
+test.after.always(async () => {
+  const topic = pubsub.topic(topicName);
+  await topic.delete();
+  console.log(`Topic ${topic.name} deleted.`);
+});
+
+test(`should receive command message`, async (t) => {
+  await tools.runAsync(installDeps, cwdHelper);
+  await tools.runAsync(`${helper} setupIotTopic ${topicName}`, cwdHelper);
+  await tools.runAsync(
+    `${helper} createRegistry ${localRegName} ${topicName}`, cwdHelper);
+  await tools.runAsync(
+    `${helper} createRsa256Device ${localDevice} ${localRegName} ./resources/rsa_cert.pem`, cwdHelper);
+
+  // This command needs to run asynchronously without await to ensure the send comand happens while
+  // mqtt client is available. Limit client to last only 15 seconds (0.25 minutes)
+  let out = tools.runAsync(
+    `${receiveCmd} --deviceId=${localDevice} --registryId=${localRegName} --maxDuration=0.25 ${receiveCmdSuffix}`);
+
+  await tools.runAsync(
+    `${sendCmd} sendCommand ${localDevice} ${localRegName} "me want cookies"`, cwdSend);
+
+  // await for original command to resolve before checking regex
+  t.regex(await out, new RegExp(`me want cookies`));
+
+  await tools.runAsync(
+    `${helper} getDeviceState ${localDevice} ${localRegName}`, cwdHelper);
+  await tools.runAsync(
+    `${helper} deleteDevice ${localDevice} ${localRegName}`, cwdHelper);
+  await tools.runAsync(`${helper} deleteRegistry ${localRegName}`, cwdHelper);
+});

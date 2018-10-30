8aeed2b70d99f0d6a2ede7958c8dc27fd2cb6c7e
noerog <32459203+noerog@users.noreply.github.com>
2018-10-30 13:42:06 -0400

    Fix lint errors.

--- healthcare/datasets/datasets.js
+++ healthcare/datasets/datasets.js
@@ -18,7 +18,7 @@
 const {google} = require('googleapis');
 
 // [START healthcare_create_dataset]
-function createDataset(client, projectId, cloudRegion, datasetId) {
+function createDataset (client, projectId, cloudRegion, datasetId) {
   // Client retrieved in callback
   // getClient(serviceAccountJson, function(client) {...});
   // const cloudRegion = 'us-central1';
@@ -29,17 +29,17 @@ function createDataset(client, projectId, cloudRegion, datasetId) {
   const request = {parent: parentName, datasetId: datasetId};
 
   client.projects.locations.datasets.create(request)
-      .then(() => {
-        console.log(`Created dataset: ${datasetId}`);
-      })
-      .catch(err => {
-        console.log('ERROR:', err);
-      });
+    .then(() => {
+      console.log(`Created dataset: ${datasetId}`);
+    })
+    .catch(err => {
+      console.log('ERROR:', err);
+    });
 }
 // [END healthcare_create_dataset]
 
 // [START healthcare_delete_dataset]
-function deleteDataset(client, projectId, cloudRegion, datasetId, cb) {
+function deleteDataset (client, projectId, cloudRegion, datasetId, cb) {
   // Client retrieved in callback
   // getClient(serviceAccountJson, function(client) {...});
   // const cloudRegion = 'us-central1';
@@ -51,17 +51,17 @@ function deleteDataset(client, projectId, cloudRegion, datasetId, cb) {
   const request = {name: datasetName};
 
   client.projects.locations.datasets.delete(request)
-      .then(() => {
-        console.log(`Deleted dataset: ${datasetId}`);
-      })
-      .catch(err => {
-        console.log('ERROR:', err);
-      });
+    .then(() => {
+      console.log(`Deleted dataset: ${datasetId}`);
+    })
+    .catch(err => {
+      console.log('ERROR:', err);
+    });
 }
 // [END healthcare_delete_dataset]
 
 // [START healthcare_get_dataset]
-function getDataset(client, projectId, cloudRegion, datasetId) {
+function getDataset (client, projectId, cloudRegion, datasetId) {
   // Client retrieved in callback
   // getClient(serviceAccountJson, function(client) {...});
   // const cloudRegion = 'us-central1';
@@ -73,17 +73,17 @@ function getDataset(client, projectId, cloudRegion, datasetId) {
   const request = {name: datasetName};
 
   client.projects.locations.datasets.get(request)
-      .then(results => {
-        console.log('Got dataset:\n', results.data);
-      })
-      .catch(err => {
-        console.log('ERROR:', err);
-      });
+    .then(results => {
+      console.log('Got dataset:\n', results.data);
+    })
+    .catch(err => {
+      console.log('ERROR:', err);
+    });
 }
 // [END healthcare_get_dataset]
 
 // [START healthcare_list_datasets]
-function listDatasets(client, projectId, cloudRegion) {
+function listDatasets (client, projectId, cloudRegion) {
   // Client retrieved in callback
   // getClient(serviceAccountJson, function(client) {...});
   // const cloudRegion = 'us-central1';
@@ -93,17 +93,17 @@ function listDatasets(client, projectId, cloudRegion) {
   const request = {parent: parentName};
 
   client.projects.locations.datasets.list(request)
-      .then(results => {
-        console.log('Datasets:', results.data);
-      })
-      .catch(err => {
-        console.log('ERROR:', err);
-      });
+    .then(results => {
+      console.log('Datasets:', results.data);
+    })
+    .catch(err => {
+      console.log('ERROR:', err);
+    });
 }
 // [END healthcare_list_datasets]
 
 // [START healthcare_patch_dataset]
-function patchDataset(client, projectId, cloudRegion, datasetId, timeZone) {
+function patchDataset (client, projectId, cloudRegion, datasetId, timeZone) {
   // Client retrieved in callback
   // getClient(serviceAccountJson, function(client) {...});
   // const cloudRegion = 'us-central1';
@@ -120,21 +120,21 @@ function patchDataset(client, projectId, cloudRegion, datasetId, timeZone) {
   };
 
   client.projects.locations.datasets.patch(request)
-      .then(results => {
-        console.log(
-            `Dataset ${datasetId} patched with time zone ${
-              results.data.timeZone}`);
-      })
-      .catch(err => {
-        console.log('ERROR:', err);
-      });
+    .then(results => {
+      console.log(
+        `Dataset ${datasetId} patched with time zone ${
+          results.data.timeZone}`);
+    })
+    .catch(err => {
+      console.log('ERROR:', err);
+    });
 }
 // [END healthcare_patch_dataset]
 
 // [START healthcare_deidentify_dataset]
-function deidentifyDataset(
-    client, projectId, cloudRegion, sourceDatasetId, destinationDatasetId,
-    whitelistTags) {
+function deidentifyDataset (
+  client, projectId, cloudRegion, sourceDatasetId, destinationDatasetId,
+  whitelistTags) {
   // Client retrieved in callback
   // getClient(serviceAccountJson, function(client) {...});
   // const cloudRegion = 'us-central1';
@@ -143,9 +143,9 @@ function deidentifyDataset(
   // const destinationDatasetId = 'my-destination-dataset';
   // const whitelistTags = 'PatientID';
   const sourceDatasetName = `projects/${projectId}/locations/${
-      cloudRegion}/datasets/${sourceDatasetId}`;
+    cloudRegion}/datasets/${sourceDatasetId}`;
   const destinationDatasetName = `projects/${projectId}/locations/${
-      cloudRegion}/datasets/${destinationDatasetId}`;
+    cloudRegion}/datasets/${destinationDatasetId}`;
 
   const request = {
     sourceDataset: sourceDatasetName,
@@ -154,143 +154,143 @@ function deidentifyDataset(
   };
 
   client.projects.locations.datasets.deidentify(request)
-      .then(results => {
-        console.log(`De-identified data written from dataset
+    .then(results => {
+      console.log(`De-identified data written from dataset
             ${sourceDatasetId} to dataset ${destinationDatasetId}`);
-      })
-      .catch(err => {
-        console.log('ERROR:', err);
-      });
+    })
+    .catch(err => {
+      console.log('ERROR:', err);
+    });
 }
 // [END healthcare_deidentify_dataset]
 
 // [START healthcare_get_client]
 // Returns an authorized API client by discovering the Healthcare API with
 // the provided API key.
-function getClient(apiKey, serviceAccountJson, cb) {
+function getClient (apiKey, serviceAccountJson, cb) {
   const API_VERSION = 'v1alpha';
   const DISCOVERY_API = 'https://healthcare.googleapis.com/$discovery/rest';
 
   google.auth
-      .getClient({scopes: ['https://www.googleapis.com/auth/cloud-platform']})
-      .then(authClient => {
-        const discoveryUrl = `${DISCOVERY_API}?labels=CHC_ALPHA&version=${
-            API_VERSION}&key=${apiKey}`;
+    .getClient({scopes: ['https://www.googleapis.com/auth/cloud-platform']})
+    .then(authClient => {
+      const discoveryUrl = `${DISCOVERY_API}?labels=CHC_ALPHA&version=${
+        API_VERSION}&key=${apiKey}`;
 
-        google.options({auth: authClient});
+      google.options({auth: authClient});
 
-        google.discoverAPI(discoveryUrl)
-            .then((client) => {
-              cb(client);
-            })
-            .catch((err) => {
-              console.log(`Error during API discovery: ${err}`);
-            });
-      });
+      google.discoverAPI(discoveryUrl)
+        .then((client) => {
+          cb(client);
+        })
+        .catch((err) => {
+          console.log(`Error during API discovery: ${err}`);
+        });
+    });
 }
 // [END healthcare_get_client]
 
 require(`yargs`)  // eslint-disable-line
-    .demand(1)
-    .options({
-      apiKey: {
-        alias: 'a',
-        default: process.env.API_KEY,
-        description: 'The API key used for discovering the API. ' +
+  .demand(1)
+  .options({
+    apiKey: {
+      alias: 'a',
+      default: process.env.API_KEY,
+      description: 'The API key used for discovering the API. ' +
             'Defaults to the value of the API_KEY environment variable.',
-        requiresArg: true,
-        type: 'string'
-      },
-      cloudRegion: {
-        alias: 'c',
-        default: 'us-central1',
-        requiresArg: true,
-        type: 'string'
-      },
-      projectId: {
-        alias: 'p',
-        default: process.env.GCLOUD_PROJECT || process.env.GOOGLE_CLOUD_PROJECT,
-        description:
+      requiresArg: true,
+      type: 'string'
+    },
+    cloudRegion: {
+      alias: 'c',
+      default: 'us-central1',
+      requiresArg: true,
+      type: 'string'
+    },
+    projectId: {
+      alias: 'p',
+      default: process.env.GCLOUD_PROJECT || process.env.GOOGLE_CLOUD_PROJECT,
+      description:
             'The Project ID to use. Defaults to the value of the ' +
             'GCLOUD_PROJECT or GOOGLE_CLOUD_PROJECT environment variables.',
-        requiresArg: true,
-        type: 'string'
-      },
-      serviceAccount: {
-        alias: 's',
-        default: process.env.GOOGLE_APPLICATION_CREDENTIALS,
-        description: 'The path to your service credentials JSON.',
-        requiresArg: true,
-        type: 'string'
-      }
+      requiresArg: true,
+      type: 'string'
+    },
+    serviceAccount: {
+      alias: 's',
+      default: process.env.GOOGLE_APPLICATION_CREDENTIALS,
+      description: 'The path to your service credentials JSON.',
+      requiresArg: true,
+      type: 'string'
+    }
+  })
+  .command(
+    `createDataset <datasetId>`, `Creates a new health dataset.`, {},
+    (opts) => {
+      const cb = function (client) {
+        createDataset(
+          client, opts.projectId, opts.cloudRegion, opts.datasetId);
+      };
+      getClient(opts.apiKey, opts.serviceAccount, cb);
     })
-    .command(
-        `createDataset <datasetId>`, `Creates a new health dataset.`, {},
-        (opts) => {
-          const cb = function(client) {
-            createDataset(
-                client, opts.projectId, opts.cloudRegion, opts.datasetId);
-          };
-          getClient(opts.apiKey, opts.serviceAccount, cb);
-        })
-    .command(
-        `deleteDataset <datasetId>`,
-        `Deletes the specified health dataset and all data contained
+  .command(
+    `deleteDataset <datasetId>`,
+    `Deletes the specified health dataset and all data contained
         in the dataset.`,
-        {},
-        (opts) => {
-          const cb = function(client) {
-            deleteDataset(
-                client, opts.projectId, opts.cloudRegion, opts.datasetId);
-          };
-          getClient(opts.apiKey, opts.serviceAccount, cb);
-        })
-    .command(
-        `getDataset <datasetId>`,
-        `Gets any metadata associated with a dataset.`, {},
-        (opts) => {
-          const cb = function(client) {
-            getDataset(
-                client, opts.projectId, opts.cloudRegion, opts.datasetId);
-          };
-          getClient(opts.apiKey, opts.serviceAccount, cb);
-        })
-    .command(
-        `listDatasets`, `Lists the datasets in the given GCP project.`, {},
-        (opts) => {
-          const cb = function(client) {
-            listDatasets(client, opts.projectId, opts.cloudRegion);
-          };
-          getClient(opts.apiKey, opts.serviceAccount, cb);
-        })
-    .command(
-        `patchDataset <datasetId> <timeZone>`, `Updates dataset metadata.`, {},
-        (opts) => {
-          const cb = function(client) {
-            patchDataset(
-                client, opts.projectId, opts.cloudRegion, opts.datasetId,
-                opts.timeZone);
-          };
-          getClient(opts.apiKey, opts.serviceAccount, cb);
-        })
-    .command(
-        `deidentifyDataset <sourceDatasetId> <destinationDatasetId>
+    {},
+    (opts) => {
+      const cb = function (client) {
+        deleteDataset(
+          client, opts.projectId, opts.cloudRegion, opts.datasetId);
+      };
+      getClient(opts.apiKey, opts.serviceAccount, cb);
+    })
+  .command(
+    `getDataset <datasetId>`,
+    `Gets any metadata associated with a dataset.`, {},
+    (opts) => {
+      const cb = function (client) {
+        getDataset(
+          client, opts.projectId, opts.cloudRegion, opts.datasetId);
+      };
+      getClient(opts.apiKey, opts.serviceAccount, cb);
+    })
+  .command(
+    `listDatasets`, `Lists the datasets in the given GCP project.`, {},
+    (opts) => {
+      const cb = function (client) {
+        listDatasets(client, opts.projectId, opts.cloudRegion);
+      };
+      getClient(opts.apiKey, opts.serviceAccount, cb);
+    })
+  .command(
+    `patchDataset <datasetId> <timeZone>`, `Updates dataset metadata.`, {},
+    (opts) => {
+      const cb = function (client) {
+        patchDataset(
+          client, opts.projectId, opts.cloudRegion, opts.datasetId,
+          opts.timeZone);
+      };
+      getClient(opts.apiKey, opts.serviceAccount, cb);
+    })
+  .command(
+    `deidentifyDataset <sourceDatasetId> <destinationDatasetId>
         <whitelistTags>`,
-        `Creates a new dataset containing de-identified data from the
+    `Creates a new dataset containing de-identified data from the
         source dataset.`,
-        {},
-        (opts) => {
-          const cb = function(client) {
-            deidentifyDataset(
-                client, opts.projectId, opts.cloudRegion, opts.sourceDatasetId,
-                opts.destinationDatasetId, opts.whitelistTags);
-          };
-          getClient(opts.apiKey, opts.serviceAccount, cb);
-        })
-    .wrap(120)
-    .recommendCommands()
-    .epilogue(
-        `For more information, see https://cloud.google.com/healthcare/docs`)
-    .help()
-    .strict()
-    .argv;
+    {},
+    (opts) => {
+      const cb = function (client) {
+        deidentifyDataset(
+          client, opts.projectId, opts.cloudRegion, opts.sourceDatasetId,
+          opts.destinationDatasetId, opts.whitelistTags);
+      };
+      getClient(opts.apiKey, opts.serviceAccount, cb);
+    })
+  .wrap(120)
+  .recommendCommands()
+  .epilogue(
+    `For more information, see https://cloud.google.com/healthcare/docs`)
+  .help()
+  .strict()
+  .argv;

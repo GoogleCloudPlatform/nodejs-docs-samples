b5ce298ef81c3ea765b55731b1606fa31fae9255
Franziska Hinkelmann <franzih@chromium.org>
2018-11-08 20:48:23 -0500

    Add linter for healthcare/datasets
    Ref: https://github.com/GoogleCloudPlatform/nodejs-docs-samples/issues/836

--- healthcare/datasets/system-test/datasets.test.js
+++ healthcare/datasets/system-test/datasets.test.js
@@ -15,60 +15,63 @@
 
 'use strict';
 
-const path = require(`path`);
-const test = require(`ava`);
-const tools = require(`@google-cloud/nodejs-repo-tools`);
-const uuid = require(`uuid`);
+const path = require('path');
+const test = require('ava');
+const tools = require('@google-cloud/nodejs-repo-tools');
+const uuid = require('uuid');
 
-const cmd = `node datasets.js`;
-const cwd = path.join(__dirname, `..`);
+const cmd = 'node datasets.js';
+const cwd = path.join(__dirname, '..');
 const datasetId = `dataset-${uuid.v4()}`.replace(/-/gi, '_');
 const destinationDatasetId = `destination-${uuid.v4()}`.replace(/-/gi, '_');
 const whitelistTags = 'PatientID';
 
 test.before(tools.checkCredentials);
 test.after.always(async () => {
-  try {
-    await tools.runAsync(
-      `${cmd} deleteDataset ${destinationDatasetId}`, cwd);
-  } catch (err) { } // Ignore error
+  await tools.runAsync(`${cmd} deleteDataset ${destinationDatasetId}`, cwd);
 });
 
-test.serial(`should create a dataset`, async t => {
-  const output = await tools.runAsync(
-    `${cmd} createDataset ${datasetId}`, cwd);
+test.serial('should create a dataset', async t => {
+  const output = await tools.runAsync(`${cmd} createDataset ${datasetId}`, cwd);
   t.is(output, `Created dataset: ${datasetId}`);
 });
 
-test.serial(`should get a dataset`, async t => {
-  const output = await tools.runAsync(
-    `${cmd} getDataset ${datasetId}`, cwd);
+test.serial('should get a dataset', async t => {
+  const output = await tools.runAsync(`${cmd} getDataset ${datasetId}`, cwd);
   t.regex(output, /name/);
   t.regex(output, /timeZone/);
 });
 
-test.serial(`should patch a dataset`, async t => {
+test.serial('should patch a dataset', async t => {
   const patchTimeZone = 'GMT';
   const output = await tools.runAsync(
-    `${cmd} patchDataset ${datasetId} ${patchTimeZone}`, cwd);
+    `${cmd} patchDataset ${datasetId} ${patchTimeZone}`,
+    cwd
+  );
   t.is(output, `Dataset ${datasetId} patched with time zone ${patchTimeZone}`);
 });
 
-test.serial(`should list datasets`, async t => {
-  const output = await tools.runAsync(
-    `${cmd} listDatasets`, cwd);
+test.serial('should list datasets', async t => {
+  const output = await tools.runAsync(`${cmd} listDatasets`, cwd);
   t.regex(output, /datasets/);
 });
 
-test.serial(`should de-identify data in a dataset and write to a new dataset`, async t => {
-  const output = await tools.runAsync(
-    `${cmd} deidentifyDataset ${datasetId} ${destinationDatasetId} ${whitelistTags}`, cwd);
-  t.is(output, `De-identified data written from dataset
-            ${datasetId} to dataset ${destinationDatasetId}`);
-});
+test.serial(
+  'should de-identify data in a dataset and write to a new dataset',
+  async t => {
+    const output = await tools.runAsync(
+      `${cmd} deidentifyDataset ${datasetId} ${destinationDatasetId} ${whitelistTags}`,
+      cwd
+    );
+    t.is(
+      output,
+      `De-identified data written from dataset
+            ${datasetId} to dataset ${destinationDatasetId}`
+    );
+  }
+);
 
-test.serial(`should delete a dataset`, async t => {
-  const output = await tools.runAsync(
-    `${cmd} deleteDataset ${datasetId}`, cwd);
+test.serial('should delete a dataset', async t => {
+  const output = await tools.runAsync(`${cmd} deleteDataset ${datasetId}`, cwd);
   t.is(output, `Deleted dataset: ${datasetId}`);
 });

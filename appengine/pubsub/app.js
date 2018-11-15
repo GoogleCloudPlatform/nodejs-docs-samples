9eb1aeecda319dffd028b6cdbe0b007bedb0cce6
Franziska Hinkelmann <franzih@chromium.org>
2018-11-14 23:24:16 -0500

    Rollback to older pubsub
    `21` throws this error:
    
    Error: google/pubsub/v1/pubsub.proto could not be found in
    /tmpfs/.../appengine/pubsub/node_modules/@google-cloud/pubsub/build/protos

--- appengine/pubsub/app.js
+++ appengine/pubsub/app.js
@@ -26,7 +26,7 @@ const process = require('process'); // Required for mocking environment variable
 // the project specified by the GOOGLE_CLOUD_PROJECT environment variable. See
 // https://github.com/GoogleCloudPlatform/google-cloud-node/blob/master/docs/authentication.md
 // These environment variables are set automatically on Google App Engine
-const {PubSub} = require('@google-cloud/pubsub');
+const PubSub = require('@google-cloud/pubsub');
 
 // Instantiate a pubsub client
 const pubsub = new PubSub();

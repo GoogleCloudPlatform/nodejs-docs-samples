ec5d30cc5adda7bf436e13c590f209685baf8b8b
Justin Beckwith <justin.beckwith@gmail.com>
2018-12-21 19:49:55 -0800

    fix: modernize the samples (#240)

---
+++ compute/test/samples.test.js
@@ -0,0 +1,72 @@
+/**
+ * Copyright 2018, Google, Inc.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+'use strict';
+
+const uuid = require('uuid');
+const execa = require('execa');
+const {assert} = require('chai');
+const Compute = require('@google-cloud/compute');
+
+const exec = async cmd => (await execa.shell(cmd)).stdout;
+const compute = new Compute();
+
+describe('quickstart', () => {
+  const name = `gcloud-ubuntu-${uuid.v4().split('-')[0]}`;
+  after(async () => deleteVM(name));
+  it('should run the quickstart', async () => {
+    const output = await exec(`node quickstart ${name}`);
+    assert.match(output, /Virtual machine created!/);
+  });
+});
+
+describe('lifecycle', async () => {
+  const name = `gcloud-ubuntu-${uuid.v4().split('-')[0]}`;
+
+  it('should create a VM', async () => {
+    const output = await exec(`node createVM ${name}`);
+    assert.match(output, /Virtual machine created!/);
+  });
+
+  it('should list the VMs', async () => {
+    const output = await exec('node listVMs');
+    assert.match(output, /Found \d+ VMs!/);
+  });
+
+  it('should delete the VM', async () => {
+    const output = await exec(`node deleteVM ${name}`);
+    assert.match(output, /VM deleted!/);
+  });
+});
+
+describe('start-up script', async () => {
+  const name = `gcloud-apache-${uuid.v4().split('-')[0]}`;
+  after(async () => deleteVM(name));
+  it('should create vm with startup script', async () => {
+    const output = await exec(`node startupScript ${name}`);
+    assert.match(output, /created succesfully$/);
+  });
+});
+
+/**
+ * Utility function to delete a VM.
+ * @param {string} name
+ */
+async function deleteVM(name) {
+  const zone = compute.zone('us-central1-c');
+  const vm = zone.vm(name);
+  const [operation] = await vm.delete();
+  await operation.promise();
+}

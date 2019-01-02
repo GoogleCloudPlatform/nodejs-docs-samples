8c21811f4895c94a892e117f5a67733d676919ad
Justin Beckwith <justin.beckwith@gmail.com>
2019-01-02 11:20:48 -0600

    docs: use the latest sendgrid npm module (#246)

--- compute/sendgrid.js
+++ compute/sendgrid.js
@@ -14,36 +14,19 @@
 'use strict';
 
 // [START send]
-// This sample is based off of https://www.npmjs.com/package/sendgrid#without-mail-helper-class
-const Sendgrid = require('sendgrid')(
-  process.env.SENDGRID_API_KEY || '<your-sendgrid-api-key>'
-);
+// This sample is based off of:
+// https://github.com/sendgrid/sendgrid-nodejs/tree/master/packages/mail
+const sendgrid = require('@sendgrid/mail');
+sendgrid.setApiKey(process.env.SENDGRID_API_KEY || '<your-sendgrid-api-key>');
 
-async function sendgrid() {
-  const request = Sendgrid.emptyRequest({
-    method: 'POST',
-    path: '/v3/mail/send',
-    body: {
-      personalizations: [
-        {
-          to: [{email: 'to_email@example.com'}],
-          subject: 'Sendgrid test email from Node.js on Google Cloud Platform',
-        },
-      ],
-      from: {email: 'from_email@example.com'},
-      content: [
-        {
-          type: 'text/plain',
-          value:
-            'Hello!\n\nThis a Sendgrid test email from Node.js on Google Cloud Platform.',
-        },
-      ],
-    },
+async function sendgridExample() {
+  await sendgrid.send({
+    to: 'to_email@example.com',
+    from: 'from_email@example.com',
+    subject: 'Sendgrid test email from Node.js on Google Cloud Platform',
+    text:
+      'Well hello! This is a Sendgrid test email from Node.js on Google Cloud Platform.',
   });
-
-  const response = await Sendgrid.API(request);
-  console.log(response);
 }
-sendgrid().catch(console.error);
-
+sendgridExample().catch(console.error);
 // [END send]

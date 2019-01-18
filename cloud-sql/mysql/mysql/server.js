55d59c87e24b433966a864fbd5b001a56674d355
Kurtis Van Gent <kvg@google.com>
2019-01-17 16:36:27 -0800

    Address some lint errors.

--- cloud-sql/mysql/mysql/server.js
+++ cloud-sql/mysql/mysql/server.js
@@ -47,21 +47,21 @@ const logger = winston.createLogger({
 
 // [START cloud_sql_mysql_mysql_create]
 const pool = mysql.createPool({
-  user            : process.env.DB_USER, // e.g. 'my-db-user'
-  password        : process.env.DB_PASS, // e.g. 'my-db-password'
-  database        : process.env.DB_NAME, // e.g. 'my-database'
+  user: process.env.DB_USER, // e.g. 'my-db-user'
+  password: process.env.DB_PASS, // e.g. 'my-db-password'
+  database: process.env.DB_NAME, // e.g. 'my-database'
   // If connecting via unix domain socket, specify the path
   socketPath: `/cloudsql/${process.env.CLOUD_SQL_CONNECTION_NAME}`,
   // If connecting via TCP, enter the IP and port instead
-  // host           : 'localhost',
-  // port           : 3306,
+  // host: 'localhost',
+  // port: 3306,
 
   //[START_EXCLUDE]
 
   // [START cloud_sql_mysql_mysql_limit]
   // 'connectionLimit' is the maximum number of connections the pool is allowed
   // to keep at once.
-  connectionLimit : 5,
+  connectionLimit: 5,
   // [END cloud_sql_mysql_mysql_limit]
 
   // [START cloud_sql_mysql_mysql_timeout]
@@ -96,69 +96,70 @@ app.on('listening', async function() {
       `CREATE TABLE IF NOT EXISTS votes
       ( vote_id SERIAL NOT NULL, time_cast timestamp NOT NULL,
       candidate CHAR(6) NOT NULL, PRIMARY KEY (vote_id) );`
-  )
+  );
 });
 
 // Serve the index page, showing vote tallies.
-app.get('/', async (req, res, next) => {
+app.get('/', async (req, res) => {
   // Get the 5 most recent votes.
   const recentResultPromise = pool.query(
-      "SELECT candidate, time_cast FROM votes ORDER BY time_cast DESC LIMIT 5")
-      .then((rows) => { return rows });
+    'SELECT candidate, time_cast FROM votes ORDER BY time_cast DESC LIMIT 5')
+      .then((rows) => {
+        return rows;
+      });
 
-  const stmt = "SELECT COUNT(vote_id) as count FROM votes WHERE candidate=?";
+  const stmt = 'SELECT COUNT(vote_id) as count FROM votes WHERE candidate=?';
   // Get the total number of "TABS" votes.
   const tabsResultPromise = pool.query(stmt, ["TABS"])
-      .then((rows) => { return rows[0].count });
-
+    .then((rows) => {
+      return rows[0].count;
+    });
   // Get the total number of "SPACES" votes.
   const spacesResultPromise = pool.query(stmt, ["SPACES"])
-      .then((rows) => { return rows[0].count });
+    .then((rows) => {
+      return rows[0].count;
+    });
 
   res.render('index.pug', {
     recentVotes: await recentResultPromise,
     tabCount: await tabsResultPromise,
-    spaceCount: await spacesResultPromise
-  })
+    spaceCount: await spacesResultPromise,
+  });
 });
 
 // Handle incoming vote requests and inserting them into the database.
-app.post('/', async (req, res, next) => {
-    const team = req.body.team;
-    const timestamp = new Date();
+app.post('/', async (req, res) => {
+const team = req.body.team;
+const timestamp = new Date();
 
-    if(!team || (team !== 'TABS' && team !== 'SPACES')){
-      res.status(400).send('Invalid team specified.').end();
-    }
+if(!team || (team !== 'TABS' && team !== 'SPACES')){
+  res.status(400).send('Invalid team specified.').end();
+}
 
-    // [START cloud_sql_mysql_mysql_connection]
-    try {
-      const stmt = "INSERT INTO votes (time_cast, candidate) VALUES (?, ?)";
-      // Pool.query automatically checks out, uses, and releases a connection
-      // back into the pool, ensuring it is always returned successfully.
-      await pool.query(stmt, [timestamp, team])
-    } catch (err) {
-      // If something goes wrong, handle the error in this section. This might
-      // involve retrying or adjusting parameters depending on the situation.
-      // [START_EXCLUDE]
-      logger.err(err);
-      res
-        .status(500)
-        .send(
-            "Unable to successfully cast vote! Please check the application "
-            + "logs for more details."
-        )
-        .end()
-      // [END_EXCLUDE]
-    }
-    // [END cloud_sql_mysql_mysql_connection]
+// [START cloud_sql_mysql_mysql_connection]
+try {
+  const stmt = "INSERT INTO votes (time_cast, candidate) VALUES (?, ?)";
+  // Pool.query automatically checks out, uses, and releases a connection
+  // back into the pool, ensuring it is always returned successfully.
+  await pool.query(stmt, [timestamp, team])
+} catch (err) {
+  // If something goes wrong, handle the error in this section. This might
+  // involve retrying or adjusting parameters depending on the situation.
+  // [START_EXCLUDE]
+  logger.err(err);
+  res
+    .status(500)
+    .send('Unable to successfully cast vote! Please check the application logs for more details.')
+    .end();
+  // [END_EXCLUDE]
+}
+// [END cloud_sql_mysql_mysql_connection]
 
   res
     .status(200)
     .send('Successfully voted for ' + team + ' at ' + timestamp)
     .end();
 });
-
 
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {

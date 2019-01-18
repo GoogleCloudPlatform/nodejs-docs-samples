a38e431305413165eb90c4161e45b5a10a3750bd
Kurtis Van Gent <kvg@google.com>
2019-01-17 17:00:46 -0800

    Fix remaining lint issues.

--- cloud-sql/mysql/mysql/server.js
+++ cloud-sql/mysql/mysql/server.js
@@ -93,7 +93,7 @@ const pool = mysql.createPool({
 app.on('listening', async function() {
   // Wait for tables to be created (if they don't already exist).
   await pool.query(
-      `CREATE TABLE IF NOT EXISTS votes
+    `CREATE TABLE IF NOT EXISTS votes
       ( vote_id SERIAL NOT NULL, time_cast timestamp NOT NULL,
       candidate CHAR(6) NOT NULL, PRIMARY KEY (vote_id) );`
   );
@@ -102,23 +102,23 @@ app.on('listening', async function() {
 // Serve the index page, showing vote tallies.
 app.get('/', async (req, res) => {
   // Get the 5 most recent votes.
-  const recentResultPromise = pool.query(
-    'SELECT candidate, time_cast FROM votes ORDER BY time_cast DESC LIMIT 5')
-      .then((rows) => {
-        return rows;
-      });
+  const recentResultPromise = pool
+    .query(
+      'SELECT candidate, time_cast FROM votes ORDER BY time_cast DESC LIMIT 5'
+    )
+    .then(rows => {
+      return rows;
+    });
 
   const stmt = 'SELECT COUNT(vote_id) as count FROM votes WHERE candidate=?';
   // Get the total number of "TABS" votes.
-  const tabsResultPromise = pool.query(stmt, ["TABS"])
-    .then((rows) => {
-      return rows[0].count;
-    });
+  const tabsResultPromise = pool.query(stmt, ['TABS']).then(rows => {
+    return rows[0].count;
+  });
   // Get the total number of "SPACES" votes.
-  const spacesResultPromise = pool.query(stmt, ["SPACES"])
-    .then((rows) => {
-      return rows[0].count;
-    });
+  const spacesResultPromise = pool.query(stmt, ['SPACES']).then(rows => {
+    return rows[0].count;
+  });
 
   res.render('index.pug', {
     recentVotes: await recentResultPromise,
@@ -129,31 +129,36 @@ app.get('/', async (req, res) => {
 
 // Handle incoming vote requests and inserting them into the database.
 app.post('/', async (req, res) => {
-const team = req.body.team;
-const timestamp = new Date();
+  const team = req.body.team;
+  const timestamp = new Date();
 
-if(!team || (team !== 'TABS' && team !== 'SPACES')){
-  res.status(400).send('Invalid team specified.').end();
-}
+  if (!team || (team !== 'TABS' && team !== 'SPACES')) {
+    res
+      .status(400)
+      .send('Invalid team specified.')
+      .end();
+  }
 
-// [START cloud_sql_mysql_mysql_connection]
-try {
-  const stmt = "INSERT INTO votes (time_cast, candidate) VALUES (?, ?)";
-  // Pool.query automatically checks out, uses, and releases a connection
-  // back into the pool, ensuring it is always returned successfully.
-  await pool.query(stmt, [timestamp, team])
-} catch (err) {
-  // If something goes wrong, handle the error in this section. This might
-  // involve retrying or adjusting parameters depending on the situation.
-  // [START_EXCLUDE]
-  logger.err(err);
-  res
-    .status(500)
-    .send('Unable to successfully cast vote! Please check the application logs for more details.')
-    .end();
-  // [END_EXCLUDE]
-}
-// [END cloud_sql_mysql_mysql_connection]
+  // [START cloud_sql_mysql_mysql_connection]
+  try {
+    const stmt = 'INSERT INTO votes (time_cast, candidate) VALUES (?, ?)';
+    // Pool.query automatically checks out, uses, and releases a connection
+    // back into the pool, ensuring it is always returned successfully.
+    await pool.query(stmt, [timestamp, team]);
+  } catch (err) {
+    // If something goes wrong, handle the error in this section. This might
+    // involve retrying or adjusting parameters depending on the situation.
+    // [START_EXCLUDE]
+    logger.err(err);
+    res
+      .status(500)
+      .send(
+        'Unable to successfully cast vote! Please check the application logs for more details.'
+      )
+      .end();
+    // [END_EXCLUDE]
+  }
+  // [END cloud_sql_mysql_mysql_connection]
 
   res
     .status(200)

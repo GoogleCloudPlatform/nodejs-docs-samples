a128c50d1f89924b337e6debcdc9e58394f744b9
Leah E. Cole <6719667+leahecole@users.noreply.github.com>
2019-04-03 14:54:28 -0700

    docs(samples): Add samples for translate v3 beta (#234)

---
+++ translate/system-test/v3beta1/translate_translate_text_beta.test.js
@@ -0,0 +1,36 @@
+/**
+ * Copyright 2019, Google, Inc.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+'use strict';
+
+const {assert} = require('chai');
+const {TranslationServiceClient} = require('@google-cloud/translate').v3beta1;
+const execa = require('execa');
+const exec = async cmd => (await execa.shell(cmd)).stdout;
+
+const REGION_TAG = 'translate_translate_text_beta';
+
+describe(REGION_TAG, () => {
+  it('should translate the input text', async () => {
+    const translationClient = new TranslationServiceClient();
+    const projectId = await translationClient.getProjectId();
+    const location = `global`;
+    const text = `"Hello world"`;
+    const output = await exec(
+      `node v3beta1/${REGION_TAG}.js ${projectId} ${location} ${text}`
+    );
+    assert.match(output, /Translation: Zdravo svet/);
+  });
+});

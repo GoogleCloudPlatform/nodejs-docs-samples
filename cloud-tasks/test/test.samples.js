56fa69ab63599e640474ca2bb39c9cba0589f07a
Justin Beckwith <justin.beckwith@gmail.com>
2019-04-04 16:03:25 -0700

    refactor: use execSync for tests (#195)

--- cloud-tasks/test/test.samples.js
+++ cloud-tasks/test/test.samples.js
@@ -14,31 +14,28 @@
 
 'use strict';
 
-const path = require('path');
-const assert = require('assert');
-const execa = require('execa');
+const {assert} = require('chai');
+const {execSync} = require('child_process');
 const uuid = require('uuid');
 
 const PROJECT_ID = process.env.GCLOUD_PROJECT;
 const queueName = `gcloud-${uuid.v4().split('-')[0]}`;
-const cwd = path.join(__dirname, '..');
-const exec = cmd => execa.shell(cmd, {cwd});
 
 describe('Cloud Task Sample Tests', () => {
-  it('should create a queue', async () => {
-    const {stdout} = await exec(`node createQueue ${PROJECT_ID} ${queueName}`);
-    assert.ok(stdout.includes('Created queue'));
+  it('should create a queue', () => {
+    const stdout = execSync(`node createQueue ${PROJECT_ID} ${queueName}`);
+    assert.match(stdout, /Created queue/);
   });
 
-  it('should create a task', async () => {
-    const {stdout} = await exec(
+  it('should create a task', () => {
+    const stdout = execSync(
       `node createTask --project=${PROJECT_ID} --location=us-central1 --queue=${queueName}`
     );
-    assert.ok(stdout.includes('Created task'));
+    assert.match(stdout, /Created task/);
   });
 
-  it('should delete a queue', async () => {
-    const {stdout} = await exec(`node deleteQueue ${PROJECT_ID} ${queueName}`);
-    assert.ok(stdout.includes('Deleted queue'));
+  it('should delete a queue', () => {
+    const stdout = execSync(`node deleteQueue ${PROJECT_ID} ${queueName}`);
+    assert.match(stdout, /Deleted queue/);
   });
 });

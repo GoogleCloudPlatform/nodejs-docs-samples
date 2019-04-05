d5f2b9a7fcbdd71156e3b3182657c8c409980a38
Jonathan Lui <jonathanlui@google.com>
2019-04-04 18:36:15 -0700

    refactor: wrap execSync with encoding: utf-8 (#218)

--- video-intelligence/system-test/analyze.test.js
+++ video-intelligence/system-test/analyze.test.js
@@ -18,7 +18,9 @@
 'use strict';
 
 const {assert} = require('chai');
-const {execSync} = require('child_process');
+const cp = require('child_process');
+
+const execSync = cmd => cp.execSync(cmd, {encoding: 'utf-8'});
 
 const cmd = 'node analyze.js';
 const url = 'gs://nodejs-docs-samples-video/quickstart.mp4';

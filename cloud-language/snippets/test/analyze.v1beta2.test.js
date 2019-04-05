bd3241aa42fe0d8b4449bd1c1bd5b7808f9e202f
Jonathan Lui <jonathanlui@google.com>
2019-04-04 18:39:53 -0700

    refactor: wrap execSync with encoding: utf-8 (#226)

--- cloud-language/snippets/test/analyze.v1beta2.test.js
+++ cloud-language/snippets/test/analyze.v1beta2.test.js
@@ -19,8 +19,10 @@ const fs = require('fs');
 const path = require('path');
 const {Storage} = require('@google-cloud/storage');
 const {assert} = require('chai');
-const {execSync} = require('child_process');
+const cp = require('child_process');
 const uuid = require('uuid');
+
+const execSync = cmd => cp.execSync(cmd, {encoding: 'utf-8'});
 
 const storage = new Storage();
 const cmd = 'node analyze.v1beta2.js';

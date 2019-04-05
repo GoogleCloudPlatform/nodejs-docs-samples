2539f9effc13ed7ea9de62684191ceb507037bb5
Jonathan Lui <jonathanlui@google.com>
2019-04-04 18:33:47 -0700

    refactor: wrap execSync with encoding: utf-8 (#196)

--- cloud-tasks/test/test.samples.js
+++ cloud-tasks/test/test.samples.js
@@ -15,8 +15,10 @@
 'use strict';
 
 const {assert} = require('chai');
-const {execSync} = require('child_process');
+const cp = require('child_process');
 const uuid = require('uuid');
+
+const execSync = cmd => cp.execSync(cmd, {encoding: 'utf-8'});
 
 const PROJECT_ID = process.env.GCLOUD_PROJECT;
 const queueName = `gcloud-${uuid.v4().split('-')[0]}`;

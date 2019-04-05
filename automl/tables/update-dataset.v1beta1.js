42e987da6ec4521314d5d40939df44c0d37bf854
Nirupa Anantha Kumar <nirupa-kumar@users.noreply.github.com>
2019-04-05 13:25:53 -0700

    docs(samples): automl tables beta samples (#155)
    * Automl Tables beta samples
    
    * skipping tests
    
    * skipping tests
    
    * skipping tests
    
    * skipping tests

---
+++ automl/tables/update-dataset.v1beta1.js
@@ -0,0 +1,67 @@
+/**
+ * Copyright 2019, Google LLC
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+`use strict`;
+async function main(
+  projectId = 'YOUR_PROJECT_ID',
+  computeRegion = 'YOUR_REGION_NAME',
+  datasetId = 'YOUR_DATASET_ID',
+  displayName = 'DISPLAY_NAME'
+) {
+  // [START automl_tables_update_dataset]
+  const automl = require(`@google-cloud/automl`);
+
+  const client = new automl.v1beta1.AutoMlClient();
+
+  /**
+   * Demonstrates using the AutoML client to update a dataset by ID.
+   * TODO(developer): Uncomment the following lines before running the sample.
+   */
+  // const projectId = '[PROJECT_ID]' e.g., "my-gcloud-project";
+  // const computeRegion = '[REGION_NAME]' e.g., "us-central1";
+  // const datasetId = '[DATASET_ID]' e.g., "TBL2246891593778855936";
+  // const displayName = '[DISPLAY_NAME]' e.g., "myUpdateDataset";
+
+  // Get the full path of the dataset.
+  const datasetFullId = client.datasetPath(projectId, computeRegion, datasetId);
+
+  // Update the dataset display name.
+  const dataset = {
+    name: datasetFullId,
+    displayName: displayName,
+  };
+
+  // Add the update mask to particular field.
+  const fieldMask = `displayName`;
+  const updateMask = {path: fieldMask};
+
+  // Update the information about a given dataset.
+  client
+    .updateDataset({dataset: dataset, updatemask: updateMask})
+    .then(responses => {
+      const dataset = responses[0];
+
+      // Display the dataset information.
+      console.log(`Dataset name: ${dataset.name}`);
+      console.log(`Dataset Id: ${dataset.name.split(`/`).pop(-1)}`);
+      console.log(`Updated dataset display name: ${dataset.displayName}`);
+      console.log(`Dataset example count: ${dataset.exampleCount}`);
+    })
+    .catch(err => {
+      console.error(err);
+    });
+  // [END automl_tables_update_dataset]
+}
+main(...process.argv.slice(2)).catch(console.error());
---
+++ automl/test/automlTablesDataset.v1beta1.test.js
@@ -0,0 +1,141 @@
+/**
+ * Copyright 2019, Google LLC
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+'use strict';
+
+const {assert} = require('chai');
+const execa = require('execa');
+
+/** Tests for AutoML Tables "Dataset API" sample. */
+
+const cmdDataset = 'node automlTablesDataset.js';
+
+// TODO(developer): Before running the test cases, set the environment variables
+// PROJECT_ID, REGION_NAME and change the values of datasetId,
+// bigQueryDatasetId, importDataCsv, outputGcsUri, outputGcsUri and
+// outputBigQueryUri.
+//const projectId = process.env.PROJECT_ID;
+//const computeRegion = process.env.REGION_NAME;
+const datasetName = 'test_table_dataset';
+const filter = 'tablesDatasetMetadata:*';
+const datasetId = 'TBL2246891593778855936';
+const bigQueryDatasetId = 'TBL5314616996204118016';
+const importDataCsv = 'gs://automl-tables/input/train.csv';
+const updateDatasetDisplayName = 'test_table_dataset_01';
+const dataTypeCode = 'CATEGORY';
+const outputGcsUri = 'gs://automl-tables/export-data/';
+const outputBigQueryUri = 'bq://automl-tables-bg-output';
+
+const exec = async cmd => (await execa.shell(cmd)).stdout;
+
+describe('Tables DatasetAPI', () => {
+  it.skip(`should create, import, update and delete a dataset`, async () => {
+    // Create dataset
+    let output = await exec(`${cmdDataset} create-dataset "${datasetName}"`);
+    const parsedOut = output.split('\n');
+    const outputDatasetId = parsedOut[1].split(':')[1].trim();
+    assert.match(output, /Dataset display name:/);
+
+    // Import data
+    output = await exec(
+      `${cmdDataset} import-data "${outputDatasetId}" "${importDataCsv}"`
+    );
+    assert.match(output, /Processing import.../);
+
+    // Update dataset
+    output = await exec(
+      `${cmdDataset} update-dataset "${outputDatasetId}"` +
+        ` "${updateDatasetDisplayName}"`
+    );
+    assert.match(output, /Dataset Id:/);
+
+    // Delete dataset
+    output = await exec(`${cmdDataset} delete-dataset "${outputDatasetId}"`);
+    assert.match(output, /Dataset delete details:/);
+  });
+
+  it.skip(`should list datasets`, async () => {
+    // List dataset
+    const output = await exec(`${cmdDataset} list-datasets "${filter}"`);
+    assert.match(output, /Dataset Id:/);
+  });
+
+  it.skip(`should get preexisting dataset`, async () => {
+    // Get dataset
+    const output = await exec(`${cmdDataset} get-dataset "${datasetId}"`);
+    assert.match(output, /Dataset Id:/);
+  });
+
+  it.skip(`should get,list,update tablespec and columnspec`, async () => {
+    // List table
+    let output = await exec(
+      `${cmdDataset} list-table-specs` + ` "${datasetId}" "${filter}"`
+    );
+    let parsedOut = output.split('\n');
+    const outputTableId = parsedOut[1]
+      .split(':')[1]
+      .trim()
+      .split('/')[7];
+    assert.match(output, /Table Id:/);
+
+    // Get table
+    output = await exec(
+      `${cmdDataset} get-table-spec` + ` "${datasetId}" "${outputTableId}"`
+    );
+    assert.match(output, /Table Id:/);
+
+    // List column
+    output = await exec(
+      `${cmdDataset} list-column-specs` + ` "${datasetId}" "${outputTableId}"`
+    );
+    parsedOut = output.split('\n');
+    const outputColumnId = parsedOut[1]
+      .split(':')[1]
+      .trim()
+      .split('/')[9];
+    assert.match(output, /Column Id:/);
+
+    // Get column
+    output = await exec(
+      `${cmdDataset} get-column-spec` +
+        ` "${datasetId}" "${outputTableId}" "${outputColumnId}"`
+    );
+    assert.match(output, /Column Id:/);
+
+    // Update column
+    output = await exec(
+      `${cmdDataset} update-column-spec` +
+        ` "${datasetId}" "${outputTableId}" "${outputColumnId}" "${dataTypeCode}"`
+    );
+    assert.match(output, /Column Id:/);
+  });
+
+  it.skip(`should export CSV dataset`, async () => {
+    // Export data to csv
+    const output = await exec(
+      `${cmdDataset} export-data-to-csv` + ` "${datasetId}" "${outputGcsUri}"`
+    );
+    assert.match(output, /Processing export.../);
+  });
+
+  it.skip(`should export BigQuery dataset`, async () => {
+    // Export data to bigquery
+    const output = await exec(
+      `${cmdDataset} export-data-to-bigquery` +
+        ` "${bigQueryDatasetId}" "${outputBigQueryUri}"`
+    );
+    assert.match(output, /Processing export.../);
+  });
+});

ff9eebcccfe28b0f2d6c63785df1f66a60cd40f5
Gandharv Kumar Garg <gandharvkumargarg@gmail.com>
2019-06-07 21:21:12 +0530

    docs: fix authentication docs URL in comment (#397)

--- vision/samples/textDetection.js
+++ vision/samples/textDetection.js
@@ -29,7 +29,7 @@ const readdir = promisify(fs.readdir);
 // By default, the client will authenticate using the service account file
 // specified by the GOOGLE_APPLICATION_CREDENTIALS environment variable and use
 // the project specified by the GCLOUD_PROJECT environment variable. See
-// https://googlecloudplatform.github.io/gcloud-node/#/docs/google-cloud/latest/guides/authentication
+// https://cloud.google.com/docs/authentication/getting-started#setting_the_environment_variable
 
 // Instantiate a vision client
 const client = new vision.ImageAnnotatorClient();

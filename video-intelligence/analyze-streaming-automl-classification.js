102db8ac50ffb915163eaa3da2408dac43eb4681
Leah E. Cole <6719667+leahecole@users.noreply.github.com>
2019-09-04 20:55:02 -0700

    feat: add streaming automl classification samples (#284)
    * Add streaming automl + test
    
    * Make the thing work, with lots of debugs
    
    * passing tests, Remove debug statements + lint
    
    * Fix the license quotes
    
    * Revert license quotes
    
    * Remove commas from license
    
    * Remove commas take 2
    
    * fix typo
    
    * Fix header
    
    * Update model
    
    * Change video codec, add comment

--- video-intelligence/analyze-streaming-automl-classification.js
+++ video-intelligence/analyze-streaming-automl-classification.js
@@ -15,12 +15,19 @@
 
 'use strict';
 
-async function main(path = 'YOUR_LOCAL_FILE') {
-  // [START video_streaming_label_detection_beta]
+async function main(
+  path = 'YOUR_LOCAL_FILE',
+  projectId = 'YOUR_GCP_PROJECT',
+  modelId = 'YOUR_AUTOML_MODELID'
+) {
+  // [START video_streaming_automl_classification_beta]
   /**
    * TODO(developer): Uncomment these variables before running the sample.
    */
   // const path = 'Local file to analyze, e.g. ./my-file.mp4';
+  // const modelId = 'autoMl model'
+  // const projectId = 'Your GCP Project'
+
   const {
     StreamingVideoIntelligenceServiceClient,
   } = require('@google-cloud/video-intelligence').v1p3beta1;
@@ -28,17 +35,26 @@ async function main(path = 'YOUR_LOCAL_FILE') {
 
   // Instantiates a client
   const client = new StreamingVideoIntelligenceServiceClient();
+
   // Streaming configuration
+  const modelPath = `projects/${projectId}/locations/us-central1/models/${modelId}`;
   const configRequest = {
     videoConfig: {
-      feature: 'STREAMING_LABEL_DETECTION',
+      feature: 'STREAMING_AUTOML_CLASSIFICATION',
+      automlClassificationConfig: {
+        modelName: modelPath,
+      },
     },
   };
+
   const readStream = fs.createReadStream(path, {
     highWaterMark: 5 * 1024 * 1024, //chunk size set to 5MB (recommended less than 10MB)
     encoding: 'base64',
   });
   //Load file content
+  // Note: Input videos must have supported video codecs. See
+  // https://cloud.google.com/video-intelligence/docs/streaming/streaming#supported_video_codecs
+  // for more details.
   const chunks = [];
   readStream
     .on('data', chunk => {
@@ -56,19 +72,24 @@ async function main(path = 'YOUR_LOCAL_FILE') {
       stream.end();
     });
 
-  const stream = client.streamingAnnotateVideo().on('data', response => {
-    //Gets annotations for video
-    const annotations = response.annotationResults;
-    const labels = annotations.labelAnnotations;
-    labels.forEach(label => {
-      console.log(
-        `Label ${label.entity.description} occurs at: ${label.frames[0]
-          .timeOffset.seconds || 0}` +
-          `.${(label.frames[0].timeOffset.nanos / 1e6).toFixed(0)}s`
-      );
-      console.log(` Confidence: ${label.frames[0].confidence}`);
+  const stream = client
+    .streamingAnnotateVideo()
+    .on('data', response => {
+      //Gets annotations for video
+      const annotations = response.annotationResults;
+      const labels = annotations.labelAnnotations;
+      labels.forEach(label => {
+        console.log(
+          `Label ${label.entity.description} occurs at: ${label.frames[0]
+            .timeOffset.seconds || 0}` +
+            `.${(label.frames[0].timeOffset.nanos / 1e6).toFixed(0)}s`
+        );
+        console.log(` Confidence: ${label.frames[0].confidence}`);
+      });
+    })
+    .on('error', response => {
+      console.error(response);
     });
-  });
-  // [END video_streaming_label_detection_beta]
+  // [END video_streaming_automl_classification_beta]
 }
 main(...process.argv.slice(2)).catch(console.error());

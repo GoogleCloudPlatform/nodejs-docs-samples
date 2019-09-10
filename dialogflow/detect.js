748a3961ce860f6e90259d65a5bfef7a982683d6
Dave Gramlich <callmehiphop@gmail.com>
2019-09-09 20:16:41 -0400

    docs(samples): remove session option from query params (#427)

--- dialogflow/detect.js
+++ dialogflow/detect.js
@@ -1,5 +1,5 @@
 /**
- * Copyright 2017, Google, Inc.
+ * Copyright 2017 Google LLC
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -195,9 +195,6 @@ async function streamingDetectIntent(
 
   const initialStreamRequest = {
     session: sessionPath,
-    queryParams: {
-      session: sessionClient.sessionPath(projectId, sessionId),
-    },
     queryInput: {
       audioConfig: {
         audioEncoding: encoding,

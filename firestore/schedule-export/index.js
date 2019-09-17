28ca0980182854341749ec87d0db545148649149
JLara310 <1543140+jlara310@users.noreply.github.com>
2019-09-17 16:06:07 -0700

    Run linter

--- firestore/schedule-export/index.js
+++ firestore/schedule-export/index.js
@@ -4,23 +4,25 @@ const client = new firestore.v1.FirestoreAdminClient();
 const bucket = 'gs://BUCKET_NAME';
 
 exports.scheduledFirestoreExport = (event, context) => {
+  const databaseName = client.databasePath(
+    process.env.GCLOUD_PROJECT,
+    '(default)'
+  );
 
-const databaseName =
- client.databasePath(process.env.GCLOUD_PROJECT, '(default)');
-
-return client.exportDocuments({
-  name: databaseName,
-  outputUriPrefix: bucket,
-  // Leave collectionIDs empty to export all collections
-  // or define a list of collection IDs:
-  // collectionIds: ['users', 'posts']
-  collectionIds: []
-  })
-  .then(responses => {
-    const response = responses[0];
-    console.log(`Operation Name: ${response['name']}`);
-  })
-  .catch(err => {
-    console.error(err);
-  });
+  return client
+    .exportDocuments({
+      name: databaseName,
+      outputUriPrefix: bucket,
+      // Leave collectionIDs empty to export all collections
+      // or define a list of collection IDs:
+      // collectionIds: ['users', 'posts']
+      collectionIds: [],
+    })
+    .then(responses => {
+      const response = responses[0];
+      console.log(`Operation Name: ${response['name']}`);
+    })
+    .catch(err => {
+      console.error(err);
+    });
 };

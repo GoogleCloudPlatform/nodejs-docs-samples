e8aece919cb708371cf90106cae78d45883ee8a8
JLara310 <1543140+jlara310@users.noreply.github.com>
2019-09-18 16:29:55 -0700

    Describe integration test requirements

--- firestore/schedule-export/test/index.test.js
+++ firestore/schedule-export/test/index.test.js
@@ -1,6 +1,13 @@
 const assert = require('assert');
 const cloudFunction = require('../');
 
+/*
+ * Integration test depends on env vars
+ * FIRESTORE_EXPORT_BUCKET and GCLOUD_PROJECT
+ * Assumes GAE service account has correct IAM permissions:
+ * https://cloud.google.com/firestore/docs/solutions/schedule-export#configure_access_permissions
+ */
+
 describe('firestore/schedule-export', () => {
   it('initiates an export', () => {
     cloudFunction.scheduledFirestoreExport().then(response => {

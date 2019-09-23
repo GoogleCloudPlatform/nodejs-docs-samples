cb31585cdf3297994d97064ea1123ce494620cc1
Grant Timmerman <timmerman+devrel@google.com>
2019-09-23 09:49:07 -0300

    refactor: prefer template strings

--- functions/tokenservice/functions/index.js
+++ functions/tokenservice/functions/index.js
@@ -49,8 +49,7 @@ function generateAccessToken(
       method: 'POST',
       headers: {
         // Set Service Account Credentials
-        Authorization:
-          serviceAccountTokenType + ' ' + serviceAccountAccessToken,
+        Authorization: `${serviceAccountTokenType} ${serviceAccountAccessToken}`,
       },
     };
 
@@ -120,7 +119,7 @@ function retrieveCredentials(context) {
     });
     get_req.on('error', e => {
       //console.log('Error retrieving credentials', e.message);
-      return 'Error retrieving token' + e.message;
+      return `Error retrieving token: ${e.message}`;
     });
     get_req.end();
   });

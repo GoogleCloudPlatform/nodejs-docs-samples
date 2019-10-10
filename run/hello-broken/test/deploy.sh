ea339c2c1c83741824a6eb834b24f1a2e50c4d85
Adam Ross <adamross@google.com>
2019-10-10 09:32:02 -0700

    run/hello-broken: fix broken test and clarify suite names

--- run/hello-broken/test/deploy.sh
+++ run/hello-broken/test/deploy.sh
@@ -31,6 +31,7 @@ gcloud beta run deploy "${SERVICE_NAME}" \
   ${FLAGS} \
   --platform=managed \
   --quiet 
+set +x
 
 echo 'Cloud Run Links:'
 echo "- Logs: https://console.cloud.google.com/logs/viewer?project=${GOOGLE_CLOUD_PROJECT}&resource=cloud_run_revision%2Fservice_name%2F${SERVICE_NAME}"

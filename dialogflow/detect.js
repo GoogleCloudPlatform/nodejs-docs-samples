0481f188b88bb7b5b77b4e09eb47746c68a78cae
Noah Negrey <nnegrey@users.noreply.github.com>
2019-11-20 16:41:58 -0700

    docs: move code outside of region tags (#473)

--- dialogflow/detect.js
+++ dialogflow/detect.js
@@ -15,13 +15,6 @@
 
 'use strict';
 
-const util = require('util');
-const fs = require('fs');
-const {struct} = require('pb-util');
-const {Transform, pipeline} = require('stream');
-
-const pump = util.promisify(pipeline);
-
 function detectTextIntent(projectId, sessionId, queries, languageCode) {
   // [START dialogflow_detect_intent_text]
 
@@ -113,6 +106,8 @@ async function detectEventIntent(
   eventName,
   languageCode
 ) {
+  const {struct} = require('pb-util');
+
   // Imports the Dialogflow library
   const dialogflow = require('dialogflow');
 
@@ -136,7 +131,31 @@ async function detectEventIntent(
 
   const [response] = await sessionClient.detectIntent(request);
   console.log('Detected intent');
-  logQueryResult(sessionClient, response.queryResult);
+  const result = response.queryResult;
+  // Instantiates a context client
+  const contextClient = new dialogflow.ContextsClient();
+
+  console.log(`  Query: ${result.queryText}`);
+  console.log(`  Response: ${result.fulfillmentText}`);
+  if (result.intent) {
+    console.log(`  Intent: ${result.intent.displayName}`);
+  } else {
+    console.log(`  No intent matched.`);
+  }
+  const parameters = JSON.stringify(struct.decode(result.parameters));
+  console.log(`  Parameters: ${parameters}`);
+  if (result.outputContexts && result.outputContexts.length) {
+    console.log(`  Output contexts:`);
+    result.outputContexts.forEach(context => {
+      const contextId = contextClient.matchContextFromContextName(context.name);
+      const contextParameters = JSON.stringify(
+        struct.decode(context.parameters)
+      );
+      console.log(`    ${contextId}`);
+      console.log(`      lifespan: ${context.lifespanCount}`);
+      console.log(`      parameters: ${contextParameters}`);
+    });
+  }
 }
 
 async function detectAudioIntent(
@@ -148,6 +167,9 @@ async function detectAudioIntent(
   languageCode
 ) {
   // [START dialogflow_detect_intent_audio]
+  const fs = require('fs');
+  const util = require('util');
+  const {struct} = require('pb-util');
   // Imports the Dialogflow library
   const dialogflow = require('dialogflow');
 
@@ -176,7 +198,31 @@ async function detectAudioIntent(
   const [response] = await sessionClient.detectIntent(request);
 
   console.log('Detected intent:');
-  logQueryResult(sessionClient, response.queryResult);
+  const result = response.queryResult;
+  // Instantiates a context client
+  const contextClient = new dialogflow.ContextsClient();
+
+  console.log(`  Query: ${result.queryText}`);
+  console.log(`  Response: ${result.fulfillmentText}`);
+  if (result.intent) {
+    console.log(`  Intent: ${result.intent.displayName}`);
+  } else {
+    console.log(`  No intent matched.`);
+  }
+  const parameters = JSON.stringify(struct.decode(result.parameters));
+  console.log(`  Parameters: ${parameters}`);
+  if (result.outputContexts && result.outputContexts.length) {
+    console.log(`  Output contexts:`);
+    result.outputContexts.forEach(context => {
+      const contextId = contextClient.matchContextFromContextName(context.name);
+      const contextParameters = JSON.stringify(
+        struct.decode(context.parameters)
+      );
+      console.log(`    ${contextId}`);
+      console.log(`      lifespan: ${context.lifespanCount}`);
+      console.log(`      parameters: ${contextParameters}`);
+    });
+  }
   // [END dialogflow_detect_intent_audio]
 }
 
@@ -189,6 +235,12 @@ async function streamingDetectIntent(
   languageCode
 ) {
   // [START dialogflow_detect_intent_streaming]
+  const fs = require('fs');
+  const util = require('util');
+  const {Transform, pipeline} = require('stream');
+  const {struct} = require('pb-util');
+
+  const pump = util.promisify(pipeline);
   // Imports the Dialogflow library
   const dialogflow = require('dialogflow');
 
@@ -231,7 +283,34 @@ async function streamingDetectIntent(
         );
       } else {
         console.log(`Detected intent:`);
-        logQueryResult(sessionClient, data.queryResult);
+
+        const result = data.queryResult;
+        // Instantiates a context client
+        const contextClient = new dialogflow.ContextsClient();
+
+        console.log(`  Query: ${result.queryText}`);
+        console.log(`  Response: ${result.fulfillmentText}`);
+        if (result.intent) {
+          console.log(`  Intent: ${result.intent.displayName}`);
+        } else {
+          console.log(`  No intent matched.`);
+        }
+        const parameters = JSON.stringify(struct.decode(result.parameters));
+        console.log(`  Parameters: ${parameters}`);
+        if (result.outputContexts && result.outputContexts.length) {
+          console.log(`  Output contexts:`);
+          result.outputContexts.forEach(context => {
+            const contextId = contextClient.matchContextFromContextName(
+              context.name
+            );
+            const contextParameters = JSON.stringify(
+              struct.decode(context.parameters)
+            );
+            console.log(`    ${contextId}`);
+            console.log(`      lifespan: ${context.lifespanCount}`);
+            console.log(`      parameters: ${contextParameters}`);
+          });
+        }
       }
     });
 
@@ -252,36 +331,6 @@ async function streamingDetectIntent(
     detectStream
   );
   // [END dialogflow_detect_intent_streaming]
-}
-
-function logQueryResult(sessionClient, result) {
-  // Imports the Dialogflow library
-  const dialogflow = require('dialogflow');
-
-  // Instantiates a context client
-  const contextClient = new dialogflow.ContextsClient();
-
-  console.log(`  Query: ${result.queryText}`);
-  console.log(`  Response: ${result.fulfillmentText}`);
-  if (result.intent) {
-    console.log(`  Intent: ${result.intent.displayName}`);
-  } else {
-    console.log(`  No intent matched.`);
-  }
-  const parameters = JSON.stringify(struct.decode(result.parameters));
-  console.log(`  Parameters: ${parameters}`);
-  if (result.outputContexts && result.outputContexts.length) {
-    console.log(`  Output contexts:`);
-    result.outputContexts.forEach(context => {
-      const contextId = contextClient.matchContextFromContextName(context.name);
-      const contextParameters = JSON.stringify(
-        struct.decode(context.parameters)
-      );
-      console.log(`    ${contextId}`);
-      console.log(`      lifespan: ${context.lifespanCount}`);
-      console.log(`      parameters: ${contextParameters}`);
-    });
-  }
 }
 
 const cli = require(`yargs`)

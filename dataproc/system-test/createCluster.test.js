3e734c9db756d3718838dceebf6f54b4b6e6f1b9
Brad Miro <bmiro@google.com>
2019-12-30 16:01:56 -0500

    fix: overhauled quickstart (#280)
    * Added create cluster sample for Cloud Dataproc
    
    * Modify test and slight change to Dataproc success message
    
    * Changed region tag to include 'dataproc'
    
    * changed dataproc imports to explicit v1
    
    * Added create cluster sample for Cloud Dataproc
    
    Updated READMEs
    
    ignore createCluster until it lands
    
    Modify test and slight change to Dataproc success message
    
    changed dataproc imports to explicit v1
    
    * Adding updated Dataproc quickstart
    
    * Added mocha commands imports to tests
    
    Co-authored-by: Benjamin E. Coe <bencoe@google.com>

--- dataproc/system-test/createCluster.test.js
+++ dataproc/system-test/createCluster.test.js
@@ -20,10 +20,10 @@ const cp = require('child_process');
 const uuid = require('uuid');
 
 const region = 'us-central1';
-const clusterName = `test-${uuid()}`;
+const clusterName = `node-cc-test-${uuid()}`;
 
 const dataproc = require('@google-cloud/dataproc').v1;
-const client = new dataproc.v1.ClusterControllerClient({
+const clusterClient = new dataproc.ClusterControllerClient({
   apiEndpoint: `${region}-dataproc.googleapis.com`,
 });
 
@@ -40,7 +40,7 @@ describe('create a dataproc cluster', () => {
   });
 
   after(async () => {
-    await client.deleteCluster({
+    await clusterClient.deleteCluster({
       projectId: projectId,
       region: region,
       clusterName: clusterName,
--- dataproc/system-test/quickstart.test.js
+++ dataproc/system-test/quickstart.test.js
@@ -12,15 +12,74 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+'use strict';
+
 const {assert} = require('chai');
-const {describe, it} = require('mocha');
+const {describe, it, before, after} = require('mocha');
 const cp = require('child_process');
+const uuid = require('uuid');
+
+const dataproc = require('@google-cloud/dataproc').v1;
+const {Storage} = require('@google-cloud/storage');
+
+const myUuid = uuid();
+const region = 'us-central1';
+const clusterName = `node-qs-test-${myUuid}`;
+const bucketName = `node-dataproc-qs-test-${myUuid}`;
+const projectId = process.env.GCLOUD_PROJECT;
+const jobFileName = 'sum.py';
+const jobFilePath = `gs://${bucketName}/${jobFileName}`;
+const sortCode =
+  'import pyspark\n' +
+  'sc = pyspark.SparkContext()\n' +
+  'rdd = sc.parallelize((1,2,3,4,5))\n' +
+  'sum = rdd.reduce(lambda x, y: x + y)\n';
+
+const clusterClient = new dataproc.v1.ClusterControllerClient({
+  apiEndpoint: `${region}-dataproc.googleapis.com`,
+});
+
+const storage = new Storage();
 
 const execSync = cmd => cp.execSync(cmd, {encoding: 'utf-8'});
 
-describe('dataproc samples', () => {
-  it('should run the quickstart', async () => {
-    const stdout = execSync('node quickstart');
-    assert.match(stdout, /Total resources:/);
+describe('execute the quickstart', () => {
+  before(async () => {
+    const [bucket] = await storage.createBucket(bucketName);
+    await bucket.file(jobFileName).save(sortCode);
+  });
+
+  it('should execute the quickstart', async () => {
+    const stdout = execSync(
+      `node quickstart.js "${projectId}" "${region}" "${clusterName}" "${jobFilePath}"`
+    );
+    assert.match(stdout, /Cluster created successfully/);
+    assert.match(stdout, /Submitted job/);
+    assert.match(stdout, /finished with state DONE:/);
+    assert.match(stdout, /successfully deleted/);
+  });
+
+  after(async () => {
+    await storage
+      .bucket(bucketName)
+      .file(jobFileName)
+      .delete();
+    await storage.bucket(bucketName).delete();
+
+    const [clusters] = await clusterClient.listClusters({
+      projectId: projectId,
+      region: region,
+    });
+
+    for (const cluster of clusters) {
+      if (cluster.clusterName === clusterName) {
+        await clusterClient.deleteCluster({
+          projectId: projectId,
+          region: region,
+          clusterName: clusterName,
+        });
+        break;
+      }
+    }
   });
 });

43aa1980d7b15aa9db782f3d56c5618b77101990
Sofia Leon <sofialeon@google.com>
2020-03-18 23:51:53 -0700

    revert

--- iot/manager/system-test/manager.test.js
+++ iot/manager/system-test/manager.test.js
@@ -77,16 +77,16 @@ after(async () => {
   console.log(`Topic ${topicName} deleted.`);
 
   // Cleans up the registry by removing all associations and deleting all devices.
-  // tools.run(`${cmd} unbindAllDevices ${registryName}`, cwd);
-  // tools.run(`${cmd} clearRegistry ${registryName}`, cwd);
-  await childProcess.execSync(`${cmd} unbindAllDevices ${registryName}`, {
-    cwd,
-    shell: true,
-  });
-  await childProcess.execSync(`${cmd} clearRegistry ${registryName}`, {
-    cwd,
-    shell: true,
-  });
+  tools.run(`${cmd} unbindAllDevices ${registryName}`, cwd);
+  tools.run(`${cmd} clearRegistry ${registryName}`, cwd);
+  // await childProcess.execSync(`${cmd} unbindAllDevices ${registryName}`, {
+  //   cwd,
+  //   shell: true,
+  // });
+  // await childProcess.execSync(`${cmd} clearRegistry ${registryName}`, {
+  //   cwd,
+  //   shell: true,
+  // });
 
   console.log('Deleted test registry.');
 });

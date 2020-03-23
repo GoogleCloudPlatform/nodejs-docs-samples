ba1de66055a7cc1558c21fc6d34a7d8c1f078c81
Benedict Noero <noero@google.com>
2020-03-23 19:38:27 +0000

    Fix HL7v2 list messages response, update tests to use correct directories when calling execSync

--- healthcare/hl7v2/system-test/hl7v2_messages.test.js
+++ healthcare/hl7v2/system-test/hl7v2_messages.test.js
@@ -46,14 +46,14 @@ before(() => {
   );
   execSync(
     `node createDataset.js ${projectId} ${cloudRegion} ${datasetId}`,
-    cwdDatasets
+    {cwd: cwdDatasets}
   );
 });
 after(() => {
   try {
     execSync(
       `node deleteDataset.js ${projectId} ${cloudRegion} ${datasetId}`,
-      cwdDatasets
+      {cwd: cwdDatasets}
     );
   } catch (err) {} // Ignore error
 });
@@ -61,11 +61,11 @@ after(() => {
 it('should create an HL7v2 message', () => {
   execSync(
     `node createHl7v2Store.js ${projectId} ${cloudRegion} ${datasetId} ${hl7v2StoreId}`,
-    cwd
+    {cwd}
   );
   const output = execSync(
     `node createHl7v2Message.js ${projectId} ${cloudRegion} ${datasetId} ${hl7v2StoreId} ${messageFile}`,
-    cwd
+    {cwd}
   );
   assert.ok(output.includes('Created HL7v2 message'));
 });
@@ -73,7 +73,7 @@ it('should create an HL7v2 message', () => {
 it('should ingest an HL7v2 message', () => {
   const output = execSync(
     `node ingestHl7v2Message.js ${projectId} ${cloudRegion} ${datasetId} ${hl7v2StoreId} ${messageFile}`,
-    cwd
+    {cwd}
   );
   assert.ok(output.includes('Ingested HL7v2 message'));
 });
@@ -81,7 +81,7 @@ it('should ingest an HL7v2 message', () => {
 it('should get an HL7v2 message', () => {
   const output = execSync(
     `node getHl7v2Message.js ${projectId} ${cloudRegion} ${datasetId} ${hl7v2StoreId} ${messageId}`,
-    cwd
+    {cwd}
   );
   assert.ok(output.includes('Got HL7v2 message'));
 });
@@ -89,7 +89,7 @@ it('should get an HL7v2 message', () => {
 it('should list HL7v2 messages', () => {
   const output = execSync(
     `node listHl7v2Messages.js ${projectId} ${cloudRegion} ${datasetId} ${hl7v2StoreId}`,
-    cwd
+    {cwd}
   );
   assert.ok(output.includes('HL7v2 messages'));
 });
@@ -97,7 +97,7 @@ it('should list HL7v2 messages', () => {
 it('should patch an HL7v2 message', () => {
   const output = execSync(
     `node patchHl7v2Message.js ${projectId} ${cloudRegion} ${datasetId} ${hl7v2StoreId} ${messageId} ${labelKey} ${labelValue}`,
-    cwd
+    {cwd}
   );
   assert.ok(output.includes('Patched HL7v2 message'));
 });
@@ -105,13 +105,13 @@ it('should patch an HL7v2 message', () => {
 it('should delete an HL7v2 message', () => {
   const output = execSync(
     `node deleteHl7v2Message.js ${projectId} ${cloudRegion} ${datasetId} ${hl7v2StoreId} ${messageId}`,
-    cwd
+    {cwd}
   );
   assert.ok(output.includes('Deleted HL7v2 message'));
 
   // Clean up
   execSync(
     `node deleteHl7v2Store.js ${projectId} ${cloudRegion} ${datasetId} ${hl7v2StoreId}`,
-    cwd
+    {cwd}
   );
 });
--- healthcare/hl7v2/system-test/hl7v2_stores.test.js
+++ healthcare/hl7v2/system-test/hl7v2_stores.test.js
@@ -47,7 +47,7 @@ before(async () => {
   console.log(`Topic ${topic.name} created.`);
   execSync(
     `node createDataset.js ${projectId} ${cloudRegion} ${datasetId}`,
-    cwdDatasets
+    {cwd: cwdDatasets}
   );
 });
 after(async () => {
@@ -56,7 +56,7 @@ after(async () => {
     console.log(`Topic ${topicName} deleted.`);
     execSync(
       `node deleteDataset.js ${projectId} ${cloudRegion} ${datasetId}`,
-      cwdDatasets
+      {cwd: cwdDatasets}
     );
   } catch (err) {} // Ignore error
 });
@@ -64,7 +64,7 @@ after(async () => {
 it('should create an HL7v2 store', () => {
   const output = execSync(
     `node createHl7v2Store.js ${projectId} ${cloudRegion} ${datasetId} ${hl7v2StoreId}`,
-    cwd
+    {cwd}
   );
   assert.ok(output.includes('Created HL7v2 store'));
 });
@@ -72,7 +72,7 @@ it('should create an HL7v2 store', () => {
 it('should get an HL7v2 store', () => {
   const output = execSync(
     `node getHl7v2Store.js ${projectId} ${cloudRegion} ${datasetId} ${hl7v2StoreId}`,
-    cwd
+    {cwd}
   );
   assert.ok(output.includes('name'));
 });
@@ -80,7 +80,7 @@ it('should get an HL7v2 store', () => {
 it('should patch an HL7v2 store', () => {
   const output = execSync(
     `node patchHl7v2Store.js ${projectId} ${cloudRegion} ${datasetId} ${hl7v2StoreId} ${topicName}`,
-    cwd
+    {cwd}
   );
   assert.ok(output.includes('Patched HL7v2 store'));
 });
@@ -88,7 +88,7 @@ it('should patch an HL7v2 store', () => {
 it('should list HL7v2 stores', () => {
   const output = execSync(
     `node listHl7v2Stores.js ${projectId} ${cloudRegion} ${datasetId}`,
-    cwd
+    {cwd}
   );
   assert.ok(output.includes('hl7V2Stores'));
 });
@@ -99,7 +99,7 @@ it('should create and get an HL7v2 store IAM policy', () => {
 
   let output = execSync(
     `node setHl7v2StoreIamPolicy.js ${projectId} ${cloudRegion} ${datasetId} ${hl7v2StoreId} ${localMember} ${localRole}`,
-    cwd
+    {cwd}
   );
   assert.ok(output.includes, 'ETAG');
 
@@ -112,7 +112,7 @@ it('should create and get an HL7v2 store IAM policy', () => {
 it('should delete an HL7v2 Store', () => {
   const output = execSync(
     `node deleteHl7v2Store ${projectId} ${cloudRegion} ${datasetId} ${hl7v2StoreId}`,
-    cwd
+    {cwd}
   );
   assert.ok(output.includes('Deleted HL7v2 store'));
 });

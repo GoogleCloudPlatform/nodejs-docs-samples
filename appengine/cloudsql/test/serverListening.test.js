6d25fdaa100dec1d05eea725b5c43e6196473998
Sofia Leon <sofialeon@google.com>
2020-03-31 12:49:54 -0700

    chore: change repo tools testing

--- appengine/cloudsql/test/serverListening.test.js
+++ appengine/cloudsql/test/serverListening.test.js
@@ -5,8 +5,8 @@ const {expect} = require('chai');
 describe('server listening', () => {
   it('should be listening', async () => {
     const isOpen = await waitPort({port: 8080});
-    console.log("is open?"+await isOpen);
+    console.log(`is open?${await isOpen}`);
     expect(await isOpen).to.be.true;
     server.close();
-  })
+  });
 });

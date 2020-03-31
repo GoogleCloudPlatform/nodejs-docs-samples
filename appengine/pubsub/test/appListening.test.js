7d39335d55757366ac2d0a7bae719f322a4f6e66
Sofia Leon <sofialeon@google.com>
2020-03-31 16:48:58 -0700

    fix: failing test

--- appengine/pubsub/test/appListening.test.js
+++ appengine/pubsub/test/appListening.test.js
@@ -4,7 +4,7 @@ const PORT = process.env.PORT || 8080;
 
 describe('server listening', () => {
   it('should be listening', async () => {
-    const server = require('../app.js');
+    const {server} = require('../app.js');
     const isOpen = await waitPort({port: PORT});
     expect(isOpen).to.be.true;
     server.close();

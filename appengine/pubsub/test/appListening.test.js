b2a56de0b15e5c8c8795ea3e59e1562310213c7c
Sofia Leon <sofialeon@google.com>
2020-04-01 09:48:57 -0700

    fix: test

--- appengine/pubsub/test/appListening.test.js
+++ appengine/pubsub/test/appListening.test.js
@@ -2,12 +2,13 @@ const waitPort = require('wait-port');
 const {expect} = require('chai');
 const PORT = process.env.PORT || 8080;
 const childProcess = require('child_process');
+const appPath = path.join(__dirname, '../app.js');
+const path = require('path');
 
 describe('server listening', () => {
   it('should be listening', async () => {
-    const child = await childProcess.exec('node app.js');
+    await childProcess.exec(`node ${appPath}`);
     const isOpen = await waitPort({port: PORT});
     expect(isOpen).to.be.true;
-    process.kill(child.pid, 'SIGTERM');
   });
 });

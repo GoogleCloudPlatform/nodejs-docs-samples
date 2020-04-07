2b7aadcc5ff1827c0797b5fa745d77cba9d56a57
Sofia Leon <sofialeon@google.com>
2020-04-06 18:12:24 -0700

    feat: add node sample for cloud compser tutorial

---
+++ composer/composerJSTutorial.js
@@ -0,0 +1,46 @@
+/* eslint-disable func-style */
+// Copyright 2020 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+module.exports = function main(
+  line = 'tampa, 106, january, null, null, 08--17-2019'
+) {
+  // [START transform_CSV_to_JSON]
+
+  function transformCSVtoJSON(line) {
+    const values = line.split(',');
+    const properties = [
+      'location',
+      'average_temperature',
+      'month',
+      'inches_of_rain',
+      'is_current',
+      'latest_measurement',
+    ];
+    const weatherInCity = {};
+
+    for (let count = 0; count < values.length; count++) {
+      if (values[count] !== 'null') {
+        weatherInCity[properties[count]] = values[count];
+      }
+    }
+
+    const jsonString = JSON.stringify(weatherInCity);
+    return jsonString;
+  }
+
+  transformCSVtoJSON(line);
+  // [END transform_CSV_to_JSON]
+  return transformCSVtoJSON(line);
+};

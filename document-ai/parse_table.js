cad4bbf85755ea9a65d9cd3fc6e2d2c328caadc7
Eric Schmidt <erschmid@google.com>
2020-04-08 14:40:10 -0700

    chore: update region tags (#33)

--- document-ai/parse_table.js
+++ document-ai/parse_table.js
@@ -26,7 +26,7 @@ async function main(
   location,
   gcsInputUri = 'gs://cloud-samples-data/documentai/invoice.pdf'
 ) {
-  // [START document_quickstart]
+  // [START documentai_parse_table]
   /**
    * TODO(developer): Uncomment these variables before running the sample.
    */
@@ -102,7 +102,7 @@ async function main(
       }
     }
   }
-  // [END document_quickstart]
+  // [END documentai_parse_table]
   await parseTable();
 }
 

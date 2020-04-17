160d467ac7ba8d1a008036b2c1c406769848b746
Sofia Leon <sofialeon@google.com>
2020-04-17 09:44:30 -0700

    chore: add parser option

--- appengine/typescript/index.ts
+++ appengine/typescript/index.ts
@@ -11,7 +11,8 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-import express = require('express');
+
+const express = require('express');
 
 const PORT = Number(process.env.PORT) || 8080;
 const app = express();

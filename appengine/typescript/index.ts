950f3abd832fd7a0e7ff600b4445409697b9e884
Sofia Leon <sofialeon@google.com>
2020-04-17 10:21:29 -0700

    fix: npm run lint

--- appengine/typescript/index.ts
+++ appengine/typescript/index.ts
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-import express = require('express');
+const express = require('express');
 
 const PORT = Number(process.env.PORT) || 8080;
 const app = express();

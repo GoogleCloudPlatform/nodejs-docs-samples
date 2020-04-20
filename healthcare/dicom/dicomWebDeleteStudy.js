16b8706169b5bc3c37883a4d12fd1fc294195235
Benedict Noero <noero@google.com>
2020-04-20 17:45:53 -0400

    healthcare: update datasets and DICOM samples from v1beta1 to v1

--- healthcare/dicom/dicomWebDeleteStudy.js
+++ healthcare/dicom/dicomWebDeleteStudy.js
@@ -25,7 +25,7 @@ const main = (
 ) => {
   // [START healthcare_dicomweb_delete_study]
   const {google} = require('googleapis');
-  const healthcare = google.healthcare('v1beta1');
+  const healthcare = google.healthcare('v1');
 
   const dicomWebDeleteStudy = async () => {
     const auth = await google.auth.getClient({

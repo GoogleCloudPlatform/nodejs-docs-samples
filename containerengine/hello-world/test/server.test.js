1ef7b86a5f62fcf93564a868c13a7a5ab8677aaa
Anthony <wens.ajw@gmail.com>
2020-05-01 14:05:29 -0700

    Update server.test.js

--- containerengine/hello-world/test/server.test.js
+++ containerengine/hello-world/test/server.test.js
@@ -2,8 +2,7 @@ const supertest = require('supertest');
 const path = require('path');
 const app = require(path.join(__dirname, '../', 'server.js'));
 
-describe('all', () => {
-  it('should be listening', async () => {
-    await supertest(app).get('/').expect(200);
-  });
+
+it('should be listening', async () => {
+  await supertest(app).get('/').expect(200);
 });

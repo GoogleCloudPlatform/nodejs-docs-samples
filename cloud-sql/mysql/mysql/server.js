086a1c9aa62f3580729b652d576e66786e5fce76
Shubha Rajan <shubhadayini@google.com>
2020-05-04 21:13:41 -0700

    Update server.js

--- cloud-sql/mysql/mysql/server.js
+++ cloud-sql/mysql/mysql/server.js
@@ -48,7 +48,7 @@ const createPool = async () => {
     user: process.env.DB_USER, // e.g. 'my-db-user'
     password: process.env.DB_PASS, // e.g. 'my-db-password'
     database: process.env.DB_NAME, // e.g. 'my-database'
-    // // If connecting via unix domain socket, specify the path
+    // If connecting via unix domain socket, specify the path
     socketPath: `/cloudsql/${process.env.CLOUD_SQL_CONNECTION_NAME}`,
     // If connecting via TCP, enter the IP and port instead
     // host: 'localhost',

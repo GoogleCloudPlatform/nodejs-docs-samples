e238fe5cfc08cefec4e0b3498cc1ddbbf33461d2
Franziska Hinkelmann <franziska.hinkelmann@gmail.com>
2020-06-07 16:43:25 -0400

    DO NOT MERGE - test missing sponge.xml

--- run/hello-broken/test/system.test.js
+++ run/hello-broken/test/system.test.js
@@ -12,96 +12,96 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-const assert = require('assert');
-const request = require('got');
+// const assert = require('assert');
+// const request = require('got');
 
-const get = (route, base_url) => {
-  const {ID_TOKEN} = process.env;
-  if (!ID_TOKEN) {
-    throw Error('"ID_TOKEN" environment variable is required.');
-  }
+// const get = (route, base_url) => {
+//   const {ID_TOKEN} = process.env;
+//   if (!ID_TOKEN) {
+//     throw Error('"ID_TOKEN" environment variable is required.');
+//   }
 
-  return request(route, {
-    baseUrl: base_url.trim(),
-    headers: {
-      Authorization: `Bearer ${ID_TOKEN.trim()}`,
-    },
-    throwHttpErrors: false,
-  });
-};
+//   return request(route, {
+//     baseUrl: base_url.trim(),
+//     headers: {
+//       Authorization: `Bearer ${ID_TOKEN.trim()}`,
+//     },
+//     throwHttpErrors: false,
+//   });
+// };
 
 describe('End-to-End Tests', () => {
-  const {NAME} = process.env;
-  if (!NAME) {
-    throw Error('"NAME" environment variable is required. For example: Cosmos');
-  }
+  // const {NAME} = process.env;
+  // if (!NAME) {
+  //   throw Error('"NAME" environment variable is required. For example: Cosmos');
+  // }
 
   describe('Service relying on defaults', () => {
-    const {BASE_URL} = process.env;
-    if (!BASE_URL) {
-      throw Error(
-        '"BASE_URL" environment variable is required. For example: https://service-x8xabcdefg-uc.a.run.app'
-      );
-    }
+    // const {BASE_URL} = process.env;
+    // if (!BASE_URL) {
+    //   throw Error(
+    //     '"BASE_URL" environment variable is required. For example: https://service-x8xabcdefg-uc.a.run.app'
+    //   );
+    // }
 
     it('Broken resource fails on any request', async () => {
-      const response = await get('/', BASE_URL);
-      assert.strictEqual(
-        response.statusCode,
-        500,
-        'Internal service error not found'
-      );
+      // const response = await get('/', BASE_URL);
+      // assert.strictEqual(
+      //   response.statusCode,
+      //   500,
+      //   'Internal service error not found'
+      // );
     });
 
-    it('Fixed resource falls back to a default value', async () => {
-      const response = await get('/improved', BASE_URL);
-      assert.strictEqual(
-        response.statusCode,
-        200,
-        'Did not fallback to default as expected'
-      );
-      assert.strictEqual(
-        response.body,
-        `Hello World!`,
-        `Expected fallback "World" not found`
-      );
-    });
-  });
+  //   it('Fixed resource falls back to a default value', async () => {
+  //     const response = await get('/improved', BASE_URL);
+  //     assert.strictEqual(
+  //       response.statusCode,
+  //       200,
+  //       'Did not fallback to default as expected'
+  //     );
+  //     assert.strictEqual(
+  //       response.body,
+  //       `Hello World!`,
+  //       `Expected fallback "World" not found`
+  //     );
+  //   });
+  // });
 
-  describe('Service with specified $NAME', () => {
-    const {BASE_URL_OVERRIDE} = process.env;
-    if (!BASE_URL_OVERRIDE) {
-      throw Error(
-        '"BASE_URL_OVERRIDE" environment variable is required. For example: https://service-x8xabcdefg-uc.a.run.app'
-      );
-    }
+  // describe('Service with specified $NAME', () => {
+  //   const {BASE_URL_OVERRIDE} = process.env;
+  //   if (!BASE_URL_OVERRIDE) {
+  //     throw Error(
+  //       '"BASE_URL_OVERRIDE" environment variable is required. For example: https://service-x8xabcdefg-uc.a.run.app'
+  //     );
+  //   }
 
-    it('Broken resource uses the NAME override', async () => {
-      const response = await get('/', BASE_URL_OVERRIDE);
-      assert.strictEqual(
-        response.statusCode,
-        200,
-        'Did not use the NAME override'
-      );
-      assert.strictEqual(
-        response.body,
-        `Hello ${NAME}!`,
-        `Expected override "${NAME}" not found`
-      );
-    });
+  //   it('Broken resource uses the NAME override', async () => {
+  //     const response = await get('/', BASE_URL_OVERRIDE);
+  //     assert.strictEqual(
+  //       response.statusCode,
+  //       200,
+  //       'Did not use the NAME override'
+  //     );
+  //     assert.strictEqual(
+  //       response.body,
+  //       `Hello ${NAME}!`,
+  //       `Expected override "${NAME}" not found`
+  //     );
+  //   });
 
-    it('Fixed resource uses the NAME override', async () => {
-      const response = await get('/improved', BASE_URL_OVERRIDE);
-      assert.strictEqual(
-        response.statusCode,
-        200,
-        'Did not fallback to default as expected'
-      );
-      assert.strictEqual(
-        response.body,
-        `Hello ${NAME}!`,
-        `Expected override "${NAME}" not found`
-      );
-    });
+  //   it('Fixed resource uses the NAME override', async () => {
+  //     const response = await get('/improved', BASE_URL_OVERRIDE);
+  //     assert.strictEqual(
+  //       response.statusCode,
+  //       200,
+  //       'Did not fallback to default as expected'
+  //     );
+  //     assert.strictEqual(
+  //       response.body,
+  //       `Hello ${NAME}!`,
+  //       `Expected override "${NAME}" not found`
+  //     );
+  //   });
   });
 });

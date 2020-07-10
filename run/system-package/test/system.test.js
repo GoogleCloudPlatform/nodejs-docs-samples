57008f0f2cb8b3702daf1bc8a46d145f512fa4ff
Adam Ross <adamross@google.com>
2020-07-10 11:46:29 -0700

    test(run/system-package): add minimal end-to-end test

---
+++ run/system-package/test/system.test.js
@@ -0,0 +1,50 @@
+// Copyright 2019 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+const assert = require('assert');
+const got = require('got');
+const {resolve} = require('url');
+
+const request = (method, route, base_url) => {
+  const {ID_TOKEN} = process.env;
+  if (!ID_TOKEN) {
+    throw Error('"ID_TOKEN" environment variable is required.');
+  }
+
+  return got(resolve(base_url.trim(), route), {
+    headers: {
+      Authorization: `Bearer ${ID_TOKEN.trim()}`,
+    },
+    method: method || 'get',
+    throwHttpErrors: false,
+  });
+};
+
+describe('End-to-End Tests', () => {
+  const {BASE_URL} = process.env;
+  if (!BASE_URL) {
+    throw Error(
+      '"BASE_URL" environment variable is required. For example: https://service-x8xabcdefg-uc.a.run.app'
+    );
+  }
+
+  it('post(/diagram.png) without request parameters is a bad request', async () => {
+    const response = await request('get', '/diagram.png', BASE_URL);
+    assert.strictEqual(
+      response.statusCode,
+      400,
+      'Bad Requests status not found'
+    );
+  });
+});

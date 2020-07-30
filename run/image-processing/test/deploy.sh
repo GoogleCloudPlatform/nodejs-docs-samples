664f33282e1847ebfbd4aa96d7874b68045b1622
Adam Ross <adamross@google.com>
2020-07-30 16:15:42 -0700

    test(run): add shallow end-to-end tests to cloud run sample apps. (#1918)
    Co-authored-by: F. Hinkelmann <franziska.hinkelmann@gmail.com>

--- run/image-processing/test/deploy.sh
+++ run/image-processing/test/deploy.sh
@@ -28,8 +28,10 @@ set -x
 gcloud run deploy "${SERVICE_NAME}" \
   --image="${CONTAINER_IMAGE}" \
   --region="${REGION:-us-central1}" \
+  ${FLAGS} \
   --platform=managed \
   --quiet
+set +x
 
 echo 'Cloud Run Links:'
 echo "- Logs: https://console.cloud.google.com/logs/viewer?project=${GOOGLE_CLOUD_PROJECT}&resource=cloud_run_revision%2Fservice_name%2F${SERVICE_NAME}"

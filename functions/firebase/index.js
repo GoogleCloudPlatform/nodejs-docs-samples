d4f4c740394c0994489e44a0dfa6b5bb35e8d8bd
Ace Nassri <anassri@google.com>
2020-09-01 14:51:50 -0700

    chore(functions/firebase): use Node 12 function signatures (#1964)
    * chore(functions/firebase): use Node 12 syntax
    
    * Update index.js

--- functions/firebase/index.js
+++ functions/firebase/index.js
@@ -68,12 +68,11 @@ exports.helloFirestore = (event) => {
  */
 exports.helloAuth = (event) => {
   try {
-    const {data} = event;
-    console.log(`Function triggered by change to user: ${data.uid}`);
-    console.log(`Created at: ${data.metadata.createdAt}`);
+    console.log(`Function triggered by change to user: ${event.uid}`);
+    console.log(`Created at: ${event.metadata.createdAt}`);
 
-    if (event.data.email) {
-      console.log(`Email: ${data.email}`);
+    if (event.email) {
+      console.log(`Email: ${event.email}`);
     }
   } catch (err) {
     console.error(err);
@@ -93,7 +92,7 @@ exports.makeUpperCase = (event) => {
   const {resource} = event;
   const affectedDoc = firestore.doc(resource.split('/documents/')[1]);
 
-  const curValue = event.data.value.fields.original.stringValue;
+  const curValue = event.value.fields.original.stringValue;
   const newValue = curValue.toUpperCase();
   console.log(`Replacing value: ${curValue} --> ${newValue}`);
 
@@ -127,13 +126,11 @@ exports.helloAnalytics = (event) => {
 /**
  * Triggered by a change to a Firebase Remote Config value.
  *
- * @param {object} data The Cloud Functions event data.
+ * @param {object} event The Cloud Functions event.
  */
 exports.helloRemoteConfig = (event) => {
-  const {data} = event;
-
-  console.log(`Update type: ${data.updateType}`);
-  console.log(`Origin: ${data.updateOrigin}`);
-  console.log(`Version: ${data.versionNumber}`);
+  console.log(`Update type: ${event.updateType}`);
+  console.log(`Origin: ${event.updateOrigin}`);
+  console.log(`Version: ${event.versionNumber}`);
 };
 // [END functions_firebase_remote_config]

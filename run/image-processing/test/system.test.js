095905837ce5b300bea9d58816064999f37e1d46
averikitsch <akitsch@google.com>
2020-10-22 10:56:49 -0700

    Set up testing

--- run/image-processing/test/system.test.js
+++ run/image-processing/test/system.test.js
@@ -15,36 +15,99 @@
 const assert = require('assert');
 const got = require('got');
 const {resolve} = require('url');
+const {GoogleAuth} = require('google-auth-library');
+const auth = new GoogleAuth();
 
-const request = (method, route, base_url) => {
-  const {ID_TOKEN} = process.env;
-  if (!ID_TOKEN) {
-    throw Error('"ID_TOKEN" environment variable is required.');
-  }
-
-  return got(resolve(base_url.trim(), route), {
-    headers: {
-      Authorization: `Bearer ${ID_TOKEN.trim()}`,
-    },
-    method: method || 'get',
-    throwHttpErrors: false,
-  });
-};
-
+let BASE_URL, ID_TOKEN;
 describe('End-to-End Tests', () => {
-  const {BASE_URL} = process.env;
-  if (!BASE_URL) {
-    throw Error(
-      '"BASE_URL" environment variable is required. For example: https://service-x8xabcdefg-uc.a.run.app'
-    );
+  // Retrieve Cloud Run service test config
+  const {GOOGLE_CLOUD_PROJECT} = process.env;
+  if (!GOOGLE_CLOUD_PROJECT) {
+    throw Error('"GOOGLE_CLOUD_PROJECT" env var not found.');
   }
+  let {SERVICE_NAME} = process.env;
+  if (!SERVICE_NAME) {
+    console.log('"SERVICE_NAME" env var not found. Defaulting to "idp-sql"');
+    SERVICE_NAME = "idp-sql";
+  }
+  const {SAMPLE_VERSION} = process.env;
+  const PLATFORM = 'managed';
+  const REGION = 'us-central1';
+
+  before(async () => {
+    // Deploy service using Cloud Build
+    const buildCmd = `gcloud builds submit --project ${GOOGLE_CLOUD_PROJECT} ` +
+      `--config ./test/e2e_test_setup.yaml ` +
+      `--substitutions _SERVICE=${SERVICE_NAME},_PLATFORM=${PLATFORM},_REGION=${REGION}`;
+    if(SAMPLE_VERSION) buildCmd + `,_VERSION=${SAMPLE_VERSION}`;
+
+    console.log('Starting Cloud Build...');
+    execSync(buildCmd);
+    console.log('Cloud Build completed.');
+
+    // Retrieve URL of Cloud Run service
+    const url = execSync(
+      `gcloud run services describe ${SERVICE_NAME} --project=${GOOGLE_CLOUD_PROJECT} ` +
+      `--platform=${PLATFORM} --region=${REGION} --format='value(status.url)'`);
+    BASE_URL = url.toString('utf-8');
+    if (!BASE_URL) throw Error('Cloud Run service URL not found');
+
+    // Retrieve ID token for testing
+    let client = await auth.getIdTokenClient(BASE_URL);
+    const clientHeaders = await client.getRequestHeaders();
+    ID_TOKEN = clientHeaders['Authorization'];;
+    if (!ID_TOKEN) throw Error('Unable to acquire an ID token.');
+  })
+
+  after(() => {
+    const cleanUpCmd = `gcloud builds submit --project ${GOOGLE_CLOUD_PROJECT} ` +
+    `--config ./test/e2e_test_cleanup.yaml ` +
+    `--substitutions _SERVICE=${SERVICE_NAME},_PLATFORM=${PLATFORM},_REGION=${REGION}`
+    if(SAMPLE_VERSION) cleanUpCmd + `,_VERSION=${SAMPLE_VERSION}`;
+
+    execSync(cleanUpCmd);
+  })
 
   it('post(/) without body is a bad request', async () => {
-    const response = await request('post', '/', BASE_URL);
+    const options = {
+      prefixUrl: BASE_URL.trim(),
+      headers: {
+        Authorization: `Bearer ${ID_TOKEN.trim()}`,
+      },
+      method: 'POST',
+      throwHttpErrors: false,
+      retry: 3
+    };
+    const response = await got('', options);
     assert.strictEqual(
       response.statusCode,
       400,
       'Bad Requests status not found'
     );
   });
+
+  it('post(/) without body message is a bad request', async () => {
+    const options = {
+      prefixUrl: BASE_URL.trim(),
+      headers: {
+        Authorization: `Bearer ${ID_TOKEN.trim()}`,
+      },
+      method: 'POST',
+      body: {
+        test: "test"
+      },
+      throwHttpErrors: false,
+      retry: 3
+    };
+    const response = await got('', options);
+    assert.strictEqual(
+      response.statusCode,
+      400,
+      'Bad Requests status not found'
+    );
+  });
+
+  it('successfully processes an image', async () => {
+
+  })
 });

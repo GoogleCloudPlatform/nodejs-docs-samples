42c683a8317f6365c5857f50481837aebf94f4c8
Mike <45373284+munkhuushmgl@users.noreply.github.com>
2020-11-03 12:19:16 -0800

    samples: refactored speech transcribe sample to meet same standards a… (#491)
    * samples: refactored speech transcribe sample to meet same standards as python
    
    * refactored print statement
    
    * wrapped async

--- video-intelligence/analyze.js
+++ video-intelligence/analyze.js
@@ -269,34 +269,51 @@ async function analyzeVideoTranscription(gcsUri) {
    */
   // const gcsUri = 'GCS URI of video to analyze, e.g. gs://my-bucket/my-video.mp4';
 
-  const videoContext = {
-    speechTranscriptionConfig: {
-      languageCode: 'en-US',
-      enableAutomaticPunctuation: true,
-    },
-  };
+  async function analyzeVideoTranscript() {
+    const videoContext = {
+      speechTranscriptionConfig: {
+        languageCode: 'en-US',
+        enableAutomaticPunctuation: true,
+      },
+    };
 
-  const request = {
-    inputUri: gcsUri,
-    features: ['SPEECH_TRANSCRIPTION'],
-    videoContext: videoContext,
-  };
+    const request = {
+      inputUri: gcsUri,
+      features: ['SPEECH_TRANSCRIPTION'],
+      videoContext: videoContext,
+    };
 
-  const [operation] = await client.annotateVideo(request);
-  console.log('Waiting for operation to complete...');
-  const [operationResult] = await operation.promise();
-  console.log('Word level information:');
-  const alternative =
-    operationResult.annotationResults[0].speechTranscriptions[0]
-      .alternatives[0];
-  alternative.words.forEach(wordInfo => {
-    const start_time =
-      wordInfo.startTime.seconds + wordInfo.startTime.nanos * 1e-9;
-    const end_time = wordInfo.endTime.seconds + wordInfo.endTime.nanos * 1e-9;
-    console.log('\t' + start_time + 's - ' + end_time + 's: ' + wordInfo.word);
-  });
-  console.log('Transcription: ' + alternative.transcript);
+    const [operation] = await client.annotateVideo(request);
+    console.log('Waiting for operation to complete...');
+    const [operationResult] = await operation.promise();
+    // There is only one annotation_result since only
+    // one video is processed.
+    const annotationResults = operationResult.annotationResults[0];
 
+    for (const speechTranscription of annotationResults.speechTranscriptions) {
+      // The number of alternatives for each transcription is limited by
+      // SpeechTranscriptionConfig.max_alternatives.
+      // Each alternative is a different possible transcription
+      // and has its own confidence score.
+      for (const alternative of speechTranscription.alternatives) {
+        console.log('Alternative level information:');
+        console.log(`Transcript: ${alternative.transcript}`);
+        console.log(`Confidence: ${alternative.confidence}`);
+
+        console.log('Word level information:');
+        for (const wordInfo of alternative.words) {
+          const word = wordInfo.word;
+          const start_time =
+            wordInfo.startTime.seconds + wordInfo.startTime.nanos * 1e-9;
+          const end_time =
+            wordInfo.endTime.seconds + wordInfo.endTime.nanos * 1e-9;
+          console.log('\t' + start_time + 's - ' + end_time + 's: ' + word);
+        }
+      }
+    }
+  }
+
+  analyzeVideoTranscript();
   // [END video_speech_transcription_gcs]
 }
 

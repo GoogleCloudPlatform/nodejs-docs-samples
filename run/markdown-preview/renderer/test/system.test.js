463b97ea618bdd503054773a67d61e3f46f2db66
Averi Kitsch <akitsch@google.com>
2020-11-04 16:42:20 -0800

    Update e2e tests for markdown preview (#2042)
    * Update e2e tests
    
    * fix var name
    
    * extend timeout
    
    * Up timeout
    
    * trim url
    
    * fix got
    
    * lint

--- run/markdown-preview/renderer/test/system.test.js
+++ run/markdown-preview/renderer/test/system.test.js
@@ -14,32 +14,73 @@
 
 const assert = require('assert');
 const got = require('got');
-
-const request = (method, route, base_url) => {
-  const {ID_TOKEN} = process.env;
-  if (!ID_TOKEN) {
-    throw Error('"ID_TOKEN" environment variable is required.');
-  }
-
-  return got(route, new URL(base_url.trim()), {
-    headers: {
-      Authorization: `Bearer ${ID_TOKEN.trim()}`,
-    },
-    method: method || 'get',
-    throwHttpErrors: false,
-  });
-};
+const {execSync} = require('child_process');
+const {GoogleAuth} = require('google-auth-library');
+const auth = new GoogleAuth();
 
 describe('End-to-End Tests', () => {
-  const {BASE_URL} = process.env;
-  if (!BASE_URL) {
-    throw Error(
-      '"BASE_URL" environment variable is required. For example: https://service-x8xabcdefg-uc.a.run.app'
-    );
+  // Retrieve Cloud Run service test config
+  const {GOOGLE_CLOUD_PROJECT} = process.env;
+  if (!GOOGLE_CLOUD_PROJECT) {
+    throw Error('"GOOGLE_CLOUD_PROJECT" env var not found.');
   }
+  let {SERVICE_NAME} = process.env;
+  if (!SERVICE_NAME) {
+    console.log('"SERVICE_NAME" env var not found. Defaulting to "editor"');
+    SERVICE_NAME = 'renderer';
+  }
+  const {SAMPLE_VERSION} = process.env;
+  const PLATFORM = 'managed';
+  const REGION = 'us-central1';
+
+  let BASE_URL, ID_TOKEN;
+  before(async () => {
+    // Deploy service using Cloud Build
+    const buildCmd =
+      `gcloud builds submit --project ${GOOGLE_CLOUD_PROJECT} ` +
+      '--config ./test/e2e_test_setup.yaml ' +
+      `--substitutions _SERVICE=${SERVICE_NAME},_PLATFORM=${PLATFORM},_REGION=${REGION}`;
+    if (SAMPLE_VERSION) buildCmd + `,_VERSION=${SAMPLE_VERSION}`;
+
+    console.log('Starting Cloud Build...');
+    execSync(buildCmd);
+    console.log('Cloud Build completed.');
+
+    // Retrieve URL of Cloud Run service
+    const url = execSync(
+      `gcloud run services describe ${SERVICE_NAME} --project=${GOOGLE_CLOUD_PROJECT} ` +
+        `--platform=${PLATFORM} --region=${REGION} --format='value(status.url)'`
+    );
+    BASE_URL = url.toString('utf-8').trim();
+    if (!BASE_URL) throw Error('Cloud Run service URL not found');
+
+    const client = await auth.getIdTokenClient(BASE_URL);
+    const clientHeaders = await client.getRequestHeaders();
+    ID_TOKEN = clientHeaders['Authorization'];
+    if (!ID_TOKEN) throw Error('ID token could not be created');
+  });
+
+  after(() => {
+    const cleanUpCmd =
+      `gcloud builds submit --project ${GOOGLE_CLOUD_PROJECT} ` +
+      '--config ./test/e2e_test_cleanup.yaml ' +
+      `--substitutions _SERVICE=${SERVICE_NAME},_PLATFORM=${PLATFORM},_REGION=${REGION}`;
+    if (SAMPLE_VERSION) cleanUpCmd + `,_VERSION=${SAMPLE_VERSION}`;
+
+    execSync(cleanUpCmd);
+  });
 
   it('post(/) without body is a bad request', async () => {
-    const response = await request('post', '/', BASE_URL);
+    const options = {
+      prefixUrl: BASE_URL.trim(),
+      headers: {
+        Authorization: ID_TOKEN.trim(),
+      },
+      method: 'POST',
+      retry: 3,
+      throwHttpErrors: false,
+    };
+    const response = await got('', options);
     assert.strictEqual(
       response.statusCode,
       400,

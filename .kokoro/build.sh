25a66b9147b47d3c22bc0d9201c67ecdfc428ecc
Craig Labenz <craig.labenz@gmail.com>
2020-12-01 14:06:25 -0800

    Added missing DB_SOCKET_PATH env variable

--- .kokoro/build.sh
+++ .kokoro/build.sh
@@ -92,6 +92,7 @@ export GOOGLE_APPLICATION_CREDENTIALS=${KOKORO_GFILE_DIR}/secrets-key.json
 gcloud auth activate-service-account --key-file "$GOOGLE_APPLICATION_CREDENTIALS"
 gcloud config set project $GOOGLE_CLOUD_PROJECT
 
+export DB_SOCKET_PATH=$KOKORO_GFILE_DIR
 
 # Download and run the proxy if testing a Cloud SQL sample
 if [[ $SQL_CLIENT ]]; then

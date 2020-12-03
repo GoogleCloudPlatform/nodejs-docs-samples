7a4528f5b4c37d99342b7edf379a50276a058ccf
Eric Schmidt <erschmid@google.com>
2020-12-03 14:30:58 -0800

    feat: person detection samples to GA (#504)
    * feat: updates person detection to GA
    
    * chore: update file names and region tags
    
    * fix: removed renamed samples

--- video-intelligence/analyze-person-detection-gcs.js
+++ video-intelligence/analyze-person-detection-gcs.js
@@ -15,14 +15,14 @@
 'use strict';
 
 function main(gcsUri = 'YOUR_STORAGE_URI') {
-  // [START video_detect_person_gcs_beta]
+  // [START video_detect_person_gcs]
   /**
    * TODO(developer): Uncomment these variables before running the sample.
    */
   // const gcsUri = 'GCS URI of the video to analyze, e.g. gs://my-bucket/my-video.mp4';
 
   // Imports the Google Cloud Video Intelligence library + Node's fs library
-  const Video = require('@google-cloud/video-intelligence').v1p3beta1;
+  const Video = require('@google-cloud/video-intelligence').v1;
 
   // Creates a client
   const video = new Video.VideoIntelligenceServiceClient();
@@ -95,7 +95,7 @@ function main(gcsUri = 'YOUR_STORAGE_URI') {
   }
 
   detectPersonGCS();
-  // [END video_detect_person_gcs_beta]
+  // [END video_detect_person_gcs]
 }
 
 main(...process.argv.slice(2));

f8eb987d48d2484cef64e2a1e8f9e42d793572e3
averikitsch <akitsch@google.com>
2020-12-07 17:24:31 -0800

    fix: Update test runner for PubSub CloudRun sample

--- run/pubsub/test/system.test.js
+++ run/pubsub/test/system.test.js
@@ -14,36 +14,110 @@
 
 const assert = require('assert');
 const got = require('got');
+const {execSync} = require('child_process');
+const {GoogleAuth} = require('google-auth-library');
+const auth = new GoogleAuth();
 
-const request = (method, route, base_url) => {
-  const {ID_TOKEN} = process.env;
-  if (!ID_TOKEN) {
-    throw Error('"ID_TOKEN" environment variable is required.');
-  }
-
-  return got(new URL(route, base_url.trim()), {
-    headers: {
-      Authorization: `Bearer ${ID_TOKEN.trim()}`,
-    },
-    method: method || 'get',
-    throwHttpErrors: false,
-  });
-};
-
+let BASE_URL, ID_TOKEN;
 describe('End-to-End Tests', () => {
-  const {BASE_URL} = process.env;
-  if (!BASE_URL) {
-    throw Error(
-      '"BASE_URL" environment variable is required. For example: https://service-x8xabcdefg-uc.a.run.app'
+  // Retrieve Cloud Run service test config
+  const {GOOGLE_CLOUD_PROJECT} = process.env;
+  if (!GOOGLE_CLOUD_PROJECT) {
+    throw Error('"GOOGLE_CLOUD_PROJECT" env var not found.');
+  }
+  let {SERVICE_NAME} = process.env;
+  if (!SERVICE_NAME) {
+    SERVICE_NAME = 'pubsub';
+    console.log(
+      `"SERVICE_NAME" env var not found. Defaulting to "${SERVICE_NAME}"`
     );
   }
+  const {SAMPLE_VERSION} = process.env;
+  const PLATFORM = 'managed';
+  const REGION = 'us-central1';
+
+  before(async () => {
+    // Deploy service using Cloud Build
+    const buildCmd =
+      `gcloud builds submit --project ${GOOGLE_CLOUD_PROJECT} ` +
+      '--config ./test/e2e_test_setup.yaml ' +
+      `--substitutions _SERVICE=${SERVICE_NAME},_PLATFORM=${PLATFORM},_REGION=${REGION}`;
+    if (SAMPLE_VERSION) buildCmd += `,_VERSION=${SAMPLE_VERSION}`;
+
+    console.log('Starting Cloud Build...');
+    execSync(buildCmd);
+    console.log('Cloud Build completed.');
+
+    // Retrieve URL of Cloud Run service
+    const url = execSync(
+      `gcloud run services describe ${SERVICE_NAME} --project=${GOOGLE_CLOUD_PROJECT} ` +
+        `--platform=${PLATFORM} --region=${REGION} --format='value(status.url)'`
+    );
+
+    BASE_URL = url.toString('utf-8').trim();
+    if (!BASE_URL) throw Error('Cloud Run service URL not found');
+
+    // Retrieve ID token for testing
+    const client = await auth.getIdTokenClient(BASE_URL);
+    const clientHeaders = await client.getRequestHeaders();
+    ID_TOKEN = clientHeaders['Authorization'].trim();
+    if (!ID_TOKEN) throw Error('Unable to acquire an ID token.');
+  });
+
+  // after(() => {
+  //   const cleanUpCmd =
+  //     `gcloud builds submit --project ${GOOGLE_CLOUD_PROJECT} ` +
+  //     '--config ./test/e2e_test_cleanup.yaml ' +
+  //     `--substitutions _SERVICE=${SERVICE_NAME},_PLATFORM=${PLATFORM},_REGION=${REGION}`;
+  //   if (SAMPLE_VERSION) cleanUpCmd += `,_VERSION=${SAMPLE_VERSION}`;
+
+  //   execSync(cleanUpCmd);
+  // });
 
   it('post(/) without body is a bad request', async () => {
-    const response = await request('post', '/', BASE_URL);
-    assert.strictEqual(
-      response.statusCode,
-      400,
-      'Bad Requests status not found'
-    );
+    const options = {
+      headers: {
+        Authorization: ID_TOKEN
+      },
+      method: 'post',
+      throwHttpErrors: false,
+      retry: 3
+    };
+    const response = await got(BASE_URL, options);
+    assert.strictEqual(response.statusCode, 400);
+  });
+
+  it('post(/) without body message is a bad request', async () => {
+    const options = {
+      headers: {
+        Authorization: ID_TOKEN,
+      },
+      method: 'POST',
+      body: 'test',
+      throwHttpErrors: false,
+      retry: 3
+    };
+    const response = await got(BASE_URL, options);
+    assert.strictEqual(response.statusCode, 400);
+  });
+
+  it('post(/) with body message is successful', async () => {
+    const body = {
+      message: {
+        data: 'SGVsbG8gQ2xvdWQgUnVuIQ==',  // Hello Cloud Run!!
+      }
+    }
+    const options = {
+      headers: {
+        Authorization: ID_TOKEN,
+        'Content-Type': 'application/json'
+      },
+      method: 'POST',
+      body: JSON.stringify(body),
+      throwHttpErrors: false,
+      retry: 3,
+    };
+    const response = await got(BASE_URL, options);
+    assert.strictEqual(response.statusCode, 204);
   });
 });

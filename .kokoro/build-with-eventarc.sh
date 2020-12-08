649bf4e7435ddc31dca682cc4517dbcb4eb3e00c
Sofia Leon <sofialeon@google.com>
2020-12-08 14:25:00 -0800

    build: add system test config for event arc

---
+++ .kokoro/build-with-eventarc.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+
+# Copyright 2018 Google Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+set -e;
+
+export GOOGLE_CLOUD_PROJECT=nodejs-docs-samples-tests
+
+# Activate mocha config
+export MOCHA_REPORTER_OUTPUT=${PROJECT}_sponge_log.xml
+export MOCHA_REPORTER=xunit
+pushd github/nodejs-docs-samples
+mv .kokoro/.mocharc.js .
+popd
+
+# Update gcloud
+gcloud components update --quiet
+
+# Configure gcloud
+export GOOGLE_APPLICATION_CREDENTIALS=${KOKORO_GFILE_DIR}/secrets-key.json
+gcloud auth activate-service-account --key-file "$GOOGLE_APPLICATION_CREDENTIALS"
+gcloud config set project $GOOGLE_CLOUD_PROJECT
+
+export NODE_ENV=development
+
+
+cd github/nodejs-docs-samples/${PROJECT}
+
+# Configure gcloud
+export GOOGLE_APPLICATION_CREDENTIALS=${KOKORO_GFILE_DIR}/secrets-key.json
+gcloud auth activate-service-account --key-file "$GOOGLE_APPLICATION_CREDENTIALS"
+gcloud config set project $GOOGLE_CLOUD_PROJECT
+
+# Install dependencies and run tests
+npm install
+
+# If tests are running against master, configure Build Cop
+# to open issues on failures:
+if [[ $KOKORO_BUILD_ARTIFACTS_SUBDIR = *"release"* ]]; then
+	export MOCHA_REPORTER_SUITENAME=${PROJECT}
+	notify_buildcop() {
+		# Call the original trap function.
+		cleanup
+		chmod +x $KOKORO_GFILE_DIR/linux_amd64/buildcop
+		$KOKORO_GFILE_DIR/linux_amd64/buildcop
+	}
+	trap notify_buildcop EXIT HUP
+fi
+
+npm test
+npm run --if-present system-test
+
+exit $?

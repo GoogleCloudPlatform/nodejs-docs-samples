eb0763194a78964a3e1a1e1ec6af9b942c3501bd
Averi Kitsch <akitsch@google.com>
2020-12-08 03:39:14 -0800

    fix: Update test runner for Helloworld app (#2098)
    * fix: Update test runner for Helloworld app
    
    * fix: lint
    
    * fix: add check  for NAME
    
    * fix: add name as env var
    
    Co-authored-by: F. Hinkelmann <franziska.hinkelmann@gmail.com>

--- run/helloworld/test/system.test.js
+++ run/helloworld/test/system.test.js
@@ -14,47 +14,93 @@
 
 const assert = require('assert');
 const request = require('got');
+const {execSync} = require('child_process');
+const {GoogleAuth} = require('google-auth-library');
+const auth = new GoogleAuth();
 
 const get = (route, base_url) => {
-  const {ID_TOKEN} = process.env;
   if (!ID_TOKEN) {
     throw Error('"ID_TOKEN" environment variable is required.');
   }
 
   return request(new URL(route, base_url.trim()), {
     headers: {
-      Authorization: `Bearer ${ID_TOKEN.trim()}`,
+      Authorization: `${ID_TOKEN.trim()}`,
     },
     throwHttpErrors: false,
   });
 };
 
+let BASE_URL, ID_TOKEN;
 describe('End-to-End Tests', () => {
+  const {GOOGLE_CLOUD_PROJECT} = process.env;
+  if (!GOOGLE_CLOUD_PROJECT) {
+    throw Error('"GOOGLE_CLOUD_PROJECT" env var not found.');
+  }
+  let {SERVICE_NAME} = process.env;
+  if (!SERVICE_NAME) {
+    SERVICE_NAME = 'helloworld';
+    console.log(
+      `"SERVICE_NAME" env var not found. Defaulting to "${SERVICE_NAME}"`
+    );
+  }
   const {NAME} = process.env;
   if (!NAME) {
-    throw Error('"NAME" environment variable is required. For example: Cosmos');
+    throw Error('"NAME" env var not found.');
   }
+  const {SAMPLE_VERSION} = process.env;
+  const PLATFORM = 'managed';
+  const REGION = 'us-central1';
+  before(async () => {
+    // Deploy service using Cloud Build
+    const buildCmd =
+      `gcloud builds submit --project ${GOOGLE_CLOUD_PROJECT} ` +
+      '--config ./test/e2e_test_setup.yaml ' +
+      `--substitutions _SERVICE=${SERVICE_NAME},_PLATFORM=${PLATFORM},_REGION=${REGION}` +
+      `,_NAME=${NAME}`;
+    if (SAMPLE_VERSION) buildCmd + `,_VERSION=${SAMPLE_VERSION}`;
 
-  describe('Service relying on defaults', () => {
-    const {BASE_URL} = process.env;
-    if (!BASE_URL) {
-      throw Error(
-        '"BASE_URL" environment variable is required. For example: https://service-x8xabcdefg-uc.a.run.app'
-      );
-    }
+    console.log('Starting Cloud Build...');
+    execSync(buildCmd);
+    console.log('Cloud Build completed.');
 
-    it('Service uses the NAME override', async () => {
-      const response = await get('/', BASE_URL);
-      assert.strictEqual(
-        response.statusCode,
-        200,
-        'Did not fallback to default as expected'
-      );
-      assert.strictEqual(
-        response.body,
-        `Hello ${NAME}!`,
-        `Expected override "${NAME}" not found`
-      );
-    });
+    // Retrieve URL of Cloud Run service
+    const url = execSync(
+      `gcloud run services describe ${SERVICE_NAME} --project=${GOOGLE_CLOUD_PROJECT} ` +
+        `--platform=${PLATFORM} --region=${REGION} --format='value(status.url)'`
+    );
+
+    BASE_URL = url.toString('utf-8').trim();
+    if (!BASE_URL) throw Error('Cloud Run service URL not found');
+
+    // Retrieve ID token for testing
+    const client = await auth.getIdTokenClient(BASE_URL);
+    const clientHeaders = await client.getRequestHeaders();
+    ID_TOKEN = clientHeaders['Authorization'].trim();
+    if (!ID_TOKEN) throw Error('Unable to acquire an ID token.');
+  });
+
+  after(() => {
+    const cleanUpCmd =
+      `gcloud builds submit --project ${GOOGLE_CLOUD_PROJECT} ` +
+      '--config ./test/e2e_test_cleanup.yaml ' +
+      `--substitutions _SERVICE=${SERVICE_NAME},_PLATFORM=${PLATFORM},_REGION=${REGION}`;
+    if (SAMPLE_VERSION) cleanUpCmd + `,_VERSION=${SAMPLE_VERSION}`;
+
+    execSync(cleanUpCmd);
+  });
+
+  it('Service uses the NAME override', async () => {
+    const response = await get('/', BASE_URL);
+    assert.strictEqual(
+      response.statusCode,
+      200,
+      'Did not fallback to default as expected'
+    );
+    assert.strictEqual(
+      response.body,
+      `Hello ${NAME}!`,
+      `Expected override "${NAME}" not found`
+    );
   });
 });

1efb2378174e24ea274e313d7de9d2a3926a1818
Mike <45373284+munkhuushmgl@users.noreply.github.com>
2021-01-22 20:09:12 -0800

    chore: moved retry into correct place (#614)
    * chore: moved retry into correct place
    
    * removed eslint comment

--- translate/test/v3/translate_batch_translate_text_with_glossary.test.js
+++ translate/test/v3/translate_batch_translate_text_with_glossary.test.js
@@ -25,8 +25,7 @@ const execSync = cmd => cp.execSync(cmd, {encoding: 'utf-8'});
 
 const REGION_TAG = 'translate_batch_translate_text_with_glossary';
 
-describe(REGION_TAG, function () {
-  this.retries(3);
+describe(REGION_TAG, () => {
   const translationClient = new TranslationServiceClient();
   const location = 'us-central1';
   const glossaryId = `my_test_glossary_${uuid.v4()}`;
@@ -72,7 +71,8 @@ describe(REGION_TAG, function () {
     await operation.promise();
   });
 
-  it('should batch translate the input text with a glossary', async () => {
+  it('should batch translate the input text with a glossary', async function () {
+    this.retries(3);
     const projectId = await translationClient.getProjectId();
     const inputUri = 'gs://cloud-samples-data/translation/text.txt';
 

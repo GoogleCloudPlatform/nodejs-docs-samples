28dda7ed83759ecb6f54188f3d3dbe67662e6b3f
Ace Nassri <anassri@google.com>
2021-02-10 17:31:41 -0800

    Update index.js

--- functions/slack/index.js
+++ functions/slack/index.js
@@ -15,7 +15,7 @@
 'use strict';
 
 // [START functions_slack_setup]
-const {google} = require('googleapis/kgsearch');
+const {google} = require('googleapis/build/src/apis/kgsearch');
 const {verifyRequestSignature} = require('@slack/events-api');
 
 // Get a reference to the Knowledge Graph Search component

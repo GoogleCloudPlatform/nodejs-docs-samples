4aa82fbea1765f06a9ae944c2acd4500340e2b90
Ace Nassri <anassri@google.com>
2021-02-12 13:18:46 -0800

    Create termination.js

---
+++ functions/concepts/termination.js
@@ -0,0 +1,32 @@
+// Copyright 2021 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+'use strict';
+
+/* eslint-disable */
+
+exports.nodeTermination = async (event, context, callback) => {
+  // [START functions_concepts_node_termination]
+  // These will cause background tasks to stop executing immediately
+  return 1; // Returning a value
+  return (await Promise.resolve()); // Returning the result of a promise
+  return (await Promise.reject()); // Same behavior as resolved promises
+  callback(); // Invoking the callback function
+
+  // These will wait until the related background task finishes
+  return Promise.resolve(); // Returning the promise itself
+  return Promise.reject(); // Same behavior as to-be-resolved promises
+  setTimeout(callback, 10); // Invoke callback once all operations complete
+  // [END functions_concepts_node_termination]
+};

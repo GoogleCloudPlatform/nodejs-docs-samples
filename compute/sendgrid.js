34ef8eded5cc9def9e22a6b186988a6fbc4b6327
Harald Reingruber <haraldreingruber@users.noreply.github.com>
2021-05-19 09:43:11 +0200

    docs: fixes small typo (#553)
    * docs: Fixes small typo
    
    * docs: Add missing gce_startup_script end comment
    
    * lint fix
    
    * copyright fix
    
    Co-authored-by: Takashi Matsuo <tmatsuo@google.com>

--- compute/sendgrid.js
+++ compute/sendgrid.js
@@ -1,4 +1,4 @@
-// Copyright 2015-2016, Google, Inc.
+// Copyright 2015 Google, Inc
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
@@ -24,8 +24,7 @@ async function sendgridExample() {
     to: 'to_email@example.com',
     from: 'from_email@example.com',
     subject: 'Sendgrid test email from Node.js on Google Cloud Platform',
-    text:
-      'Well hello! This is a Sendgrid test email from Node.js on Google Cloud Platform.',
+    text: 'Well hello! This is a Sendgrid test email from Node.js on Google Cloud Platform.',
   });
 }
 sendgridExample();

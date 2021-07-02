2c371aefb6bd1512de7c5f192aa87f4241f3b8d0
Justin Beckwith <justin.beckwith@gmail.com>
2021-07-02 16:11:29 -0700

    fix(functions/speech-to-speech): remove usage of request (#2266)

--- functions/speech-to-speech/functions/test/index.test.js
+++ functions/speech-to-speech/functions/test/index.test.js
@@ -18,7 +18,8 @@ const uuid = require('uuid');
 const assert = require('assert');
 const fs = require('fs');
 const execPromise = require('child-process-promise').exec;
-let requestRetry = require('requestretry');
+const gaxios = require('gaxios');
+const waitPort = require('wait-port');
 
 const path = require('path');
 const cwd = path.join(__dirname, '..');
@@ -29,20 +30,20 @@ const storage = new Storage();
 const BASE_URL = 'http://localhost:8080';
 const outputBucket = storage.bucket(process.env.OUTPUT_BUCKET);
 
-requestRetry = requestRetry.defaults({
-  retryStrategy: requestRetry.RetryStrategies.NetworkError,
+gaxios.instance.defaults = {
   method: 'POST',
-  json: true,
   url: `${BASE_URL}/speechTranslate`,
-});
+  validateStatus: () => true,
+};
 
 describe('speechTranslate tests', () => {
   let ffProc;
-  before(() => {
+  before(async () => {
     ffProc = execPromise(
       'functions-framework --target=speechTranslate --signature-type=http',
       {timeout: 8000, shell: true, cwd}
     );
+    await waitPort({host: 'localhost', port: 8080});
   });
 
   after(async () => {
@@ -61,126 +62,126 @@ describe('speechTranslate tests', () => {
   describe('validate_request', () => {
     describe('Validate encoding field', () => {
       it('should fail if encoding field is missing.', async () => {
-        const response = await requestRetry({
-          body: {
+        const response = await gaxios.request({
+          data: {
             // encoding: 'LINEAR16',
             sampleRateHertz: 16000,
             languageCode: 'en',
             audioContent: 'base64-audio-content',
           },
         });
-        assert.strictEqual(response.statusCode, 400);
-        assert.strictEqual(response.body, 'Invalid encoding.');
+        assert.strictEqual(response.status, 400);
+        assert.strictEqual(response.data, 'Invalid encoding.');
       });
 
       it('should fail if encoding field is empty.', async () => {
-        const response = await requestRetry({
-          body: {
+        const response = await gaxios.request({
+          data: {
             encoding: '',
             sampleRateHertz: 16000,
             languageCode: 'en',
             audioContent: 'base64-audio-content',
           },
         });
-        assert.strictEqual(response.statusCode, 400);
-        assert.strictEqual(response.body, 'Invalid encoding.');
+        assert.strictEqual(response.status, 400);
+        assert.strictEqual(response.data, 'Invalid encoding.');
       });
     });
 
     describe('Validate sampleRateHertz field', () => {
       it('should fail if sampleRateHertz field is missing.', async () => {
-        const response = await requestRetry({
-          body: {
+        const response = await gaxios.request({
+          data: {
             encoding: 'LINEAR16',
             // sampleRateHertz: 16000,
             languageCode: 'en',
             audioContent: 'base64-audio-content',
           },
         });
-        assert.strictEqual(response.statusCode, 400);
-        assert.strictEqual(response.body, 'Sample rate hertz must be numeric.');
+        assert.strictEqual(response.status, 400);
+        assert.strictEqual(response.data, 'Sample rate hertz must be numeric.');
       });
 
       it('should fail if sampleRateHertz field is empty.', async () => {
-        const response = await requestRetry({
-          body: {
+        const response = await gaxios.request({
+          data: {
             encoding: 'LINEAR16',
             sampleRateHertz: '',
             languageCode: 'en',
             audioContent: 'base64-audio-content',
           },
         });
-        assert.strictEqual(response.statusCode, 400);
-        assert.strictEqual(response.body, 'Sample rate hertz must be numeric.');
+        assert.strictEqual(response.status, 400);
+        assert.strictEqual(response.data, 'Sample rate hertz must be numeric.');
       });
 
       it('should fail if sampleRateHertz field is not numeric.', async () => {
-        const response = await requestRetry({
-          body: {
+        const response = await gaxios.request({
+          data: {
             encoding: 'LINEAR16',
             sampleRateHertz: 'NaN',
             languageCode: 'en',
             audioContent: 'base64-audio-content',
           },
         });
-        assert.strictEqual(response.statusCode, 400);
-        assert.strictEqual(response.body, 'Sample rate hertz must be numeric.');
+        assert.strictEqual(response.status, 400);
+        assert.strictEqual(response.data, 'Sample rate hertz must be numeric.');
       });
     });
 
     describe('Validate languageCode field', () => {
       it('should fail if languageCode field is missing.', async () => {
-        const response = await requestRetry({
-          body: {
+        const response = await gaxios.request({
+          data: {
             encoding: 'LINEAR16',
             sampleRateHertz: 16000,
             // languageCode: 'en',
             audioContent: 'base64-audio-content',
           },
         });
-        assert.strictEqual(response.statusCode, 400);
-        assert.strictEqual(response.body, 'Invalid language code.');
+        assert.strictEqual(response.status, 400);
+        assert.strictEqual(response.data, 'Invalid language code.');
       });
 
       it('should fail if languageCode field is empty.', async () => {
-        const response = await requestRetry({
-          body: {
+        const response = await gaxios.request({
+          data: {
             encoding: 'LINEAR16',
             sampleRateHertz: 16000,
             languageCode: '',
             audioContent: 'base64-audio-content',
           },
         });
-        assert.strictEqual(response.statusCode, 400);
-        assert.strictEqual(response.body, 'Invalid language code.');
+        assert.strictEqual(response.status, 400);
+        assert.strictEqual(response.data, 'Invalid language code.');
       });
     });
 
     describe('Validate audioContent field', () => {
       it('should fail if audioContent field is missing.', async () => {
-        const response = await requestRetry({
-          body: {
+        const response = await gaxios.request({
+          data: {
             encoding: 'LINEAR16',
             sampleRateHertz: 16000,
             languageCode: 'en',
             // audioContent: 'base64-audio-content'
           },
         });
-        assert.strictEqual(response.statusCode, 400);
-        assert.strictEqual(response.body, 'Invalid audio content.');
+        assert.strictEqual(response.status, 400);
+        assert.strictEqual(response.data, 'Invalid audio content.');
       });
 
       it('should fail if audioContent field is empty.', async () => {
-        const response = await requestRetry({
-          body: {
+        const response = await gaxios.request({
+          data: {
             encoding: 'LINEAR16',
             sampleRateHertz: 16000,
             languageCode: 'en',
             audioContent: '',
           },
         });
-        assert.strictEqual(response.statusCode, 400);
-        assert.strictEqual(response.body, 'Invalid audio content.');
+        assert.strictEqual(response.status, 400);
+        assert.strictEqual(response.data, 'Invalid audio content.');
       });
     });
   });
@@ -190,8 +191,8 @@ describe('speechTranslate tests', () => {
 
     describe('call_speech_to_text call_text_to_speech call_text_translation chain_cloud_calls upload_to_cloud_storage validate_request', () => {
       it('should transcribe speech, translate it, and synthesize it in another language', async () => {
-        const response = await requestRetry({
-          body: {
+        const response = await gaxios.request({
+          data: {
             filename: gcsFilename,
             encoding: 'LINEAR16',
             sampleRateHertz: 24000,
@@ -200,14 +201,14 @@ describe('speechTranslate tests', () => {
           },
         });
 
-        assert.strictEqual(response.statusCode, 200);
+        assert.strictEqual(response.status, 200);
 
         // Test transcription
-        response.body.translations.forEach(translation => {
+        response.data.translations.forEach(translation => {
           assert.ifError(translation.error);
         });
         assert.strictEqual(
-          response.body['transcription'],
+          response.data.transcription,
           'this is a test please translate this message'
         );
 

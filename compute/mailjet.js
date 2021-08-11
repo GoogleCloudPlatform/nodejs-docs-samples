c5e7042f065ea1b26f0fec7927c5c31952ca29d4
Alexander Fenster <fenster@google.com>
2021-08-11 15:33:50 -0700

    feat!: new generated version of compute API (#537)

--- compute/mailjet.js
+++ compute/mailjet.js
@@ -1,4 +1,4 @@
-// Copyright 2017 Google LLC
+// Copyright 2021 Google LLC
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -18,7 +18,7 @@
 
 'use strict';
 
-// [START send]
+// [START compute_send]
 const mailer = require('nodemailer');
 const smtp = require('nodemailer-smtp-transport');
 
@@ -43,4 +43,4 @@ async function mailjet() {
   console.log(json);
 }
 mailjet();
-// [END send]
+// [END compute_send]
--- compute/sendgrid.js
+++ compute/sendgrid.js
@@ -1,9 +1,10 @@
-// Copyright 2015 Google, Inc
+// Copyright 2021 Google LLC
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//    http://www.apache.org/licenses/LICENSE-2.0
+//      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
@@ -13,7 +14,7 @@
 
 'use strict';
 
-// [START send]
+// [START compute_send]
 // This sample is based off of:
 // https://github.com/sendgrid/sendgrid-nodejs/tree/master/packages/mail
 const sendgrid = require('@sendgrid/mail');
@@ -28,4 +29,4 @@ async function sendgridExample() {
   });
 }
 sendgridExample();
-// [END send]
+// [END compute_send]

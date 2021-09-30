ac73bfa746b8e57d507f923062792c29144034d2
Jonathan Simon <jsimonweb@gmail.com>
2021-09-30 10:54:11 -0700

    Update Cloud SQL sample apps with SSL example

--- cloud-sql/mysql/mysql/server.js
+++ cloud-sql/mysql/mysql/server.js
@@ -16,6 +16,7 @@
 
 const express = require('express');
 const mysql = require('promise-mysql');
+const fs = require('fs');
 
 const app = express();
 app.set('view engine', 'pug');
@@ -41,13 +42,37 @@ const logger = winston.createLogger({
   transports: [new winston.transports.Console(), loggingWinston],
 });
 
+// [START cloud_sql_mysql_mysql_create_tcp_sslcerts]
+const createTcpPoolSslCerts = async config => {
+  // Extract host and port from socket address
+  const dbSocketAddr = process.env.DB_HOST.split(':');
+
+  // Establish a connection to the database
+  return mysql.createPool({
+    user: process.env.DB_USER, // e.g. 'my-db-user'
+    password: process.env.DB_PASS, // e.g. 'my-db-password'
+    database: process.env.DB_NAME, // e.g. 'my-database'
+    host: dbSocketAddr[0], // e.g. '127.0.0.1'
+    port: dbSocketAddr[1], // e.g. '3306'
+    ssl: {
+      rejectUnauthorized: false,
+      ca: fs.readFileSync(process.env.DB_ROOT_CERT), // e.g., '/path/to/my/server-ca.pem'
+      key: fs.readFileSync(process.env.DB_KEY), // e.g. '/path/to/my/client-key.pem'
+      cert: fs.readFileSync(process.env.DB_CERT), // e.g. '/path/to/my/client-cert.pem'
+    },
+    // ... Specify additional properties here.
+    ...config,
+  });
+};
+// [END cloud_sql_mysql_mysql_create_tcp_sslcerts]
+
 // [START cloud_sql_mysql_mysql_create_tcp]
 const createTcpPool = async config => {
   // Extract host and port from socket address
   const dbSocketAddr = process.env.DB_HOST.split(':');
 
   // Establish a connection to the database
-  return await mysql.createPool({
+  return mysql.createPool({
     user: process.env.DB_USER, // e.g. 'my-db-user'
     password: process.env.DB_PASS, // e.g. 'my-db-password'
     database: process.env.DB_NAME, // e.g. 'my-database'
@@ -64,7 +89,7 @@ const createUnixSocketPool = async config => {
   const dbSocketPath = process.env.DB_SOCKET_PATH || '/cloudsql';
 
   // Establish a connection to the database
-  return await mysql.createPool({
+  return mysql.createPool({
     user: process.env.DB_USER, // e.g. 'my-db-user'
     password: process.env.DB_PASS, // e.g. 'my-db-password'
     database: process.env.DB_NAME, // e.g. 'my-database'
@@ -106,9 +131,13 @@ const createPool = async () => {
     // [END cloud_sql_mysql_mysql_backoff]
   };
   if (process.env.DB_HOST) {
-    return await createTcpPool(config);
+    if  (process.env.DB_ROOT_CERT) { 
+      return createTcpPoolSslCerts(config);
+    } else {
+      return createTcpPool(config);
+    }
   } else {
-    return await createUnixSocketPool(config);
+    return createUnixSocketPool(config);
   }
 };
 

305e836461ef5c0f97227c273424b8ec771b6af5
Oskar Damkjaer <OskarDamkjaer@users.noreply.github.com>
2021-11-17 21:09:57 +0000

    samples: update getSecret sample to include version (#311)
    Co-authored-by: Benjamin E. Coe <bencoe@google.com>

--- secret-manager/getSecret.js
+++ secret-manager/getSecret.js
@@ -19,7 +19,7 @@ async function main(name = 'projects/my-project/secrets/my-secret') {
   /**
    * TODO(developer): Uncomment these variables before running the sample.
    */
-  // const name = 'projects/my-project/secrets/my-secret';
+  // const name = 'projects/my-project/secrets/my-secret/versions/my-version';
 
   // Imports the Secret Manager library
   const {SecretManagerServiceClient} = require('@google-cloud/secret-manager');

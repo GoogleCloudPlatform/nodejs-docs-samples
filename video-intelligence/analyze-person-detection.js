3bf273d4b85926464a203dfa8f31b49efa18b6ec
nicain <nicain.seattle@gmail.com>
2021-12-07 08:38:23 -0800

    chore: Cleanup timestamp setting blocks in samples (#631)

--- video-intelligence/analyze-person-detection.js
+++ video-intelligence/analyze-person-detection.js
@@ -63,18 +63,6 @@ function main(path = 'YOUR_LOCAL_FILE') {
       console.log('Person detected:');
 
       for (const {segment, timestampedObjects} of tracks) {
-        if (segment.startTimeOffset.seconds === undefined) {
-          segment.startTimeOffset.seconds = 0;
-        }
-        if (segment.startTimeOffset.nanos === undefined) {
-          segment.startTimeOffset.nanos = 0;
-        }
-        if (segment.endTimeOffset.seconds === undefined) {
-          segment.endTimeOffset.seconds = 0;
-        }
-        if (segment.endTimeOffset.nanos === undefined) {
-          segment.endTimeOffset.nanos = 0;
-        }
         console.log(
           `\tStart: ${segment.startTimeOffset.seconds}` +
             `.${(segment.startTimeOffset.nanos / 1e6).toFixed(0)}s`

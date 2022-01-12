da0579d8086a8afd5f2805f16fb7a35c8bff776a
Justin Beckwith <justin.beckwith@gmail.com>
2022-01-12 14:19:41 -0800

    build: migrate tag enforcer to github actions

--- .github/workflows/region-tags-tests.sh
+++ .github/workflows/region-tags-tests.sh
@@ -13,11 +13,9 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-      
+
 export GOOGLE_CLOUD_PROJECT=nodejs-docs-samples-tests
 export NODE_ENV=development
-
-cd github/nodejs-docs-samples/
 
 # Display link to docs
 echo '================================================================='

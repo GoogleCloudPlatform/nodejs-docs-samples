1f53b38bc86abc46bc4e2ce3be29f65e1c966e17
Ace Nassri <anassri@google.com>
2022-01-18 13:06:49 -0800

    chore(CI): print out Kokoro test logs (#2478)

--- .kokoro/build.sh
+++ .kokoro/build.sh
@@ -127,6 +127,13 @@ if [[ $SQL_CLIENT ]]; then
 	fi
 fi
 
+# Print out log files (for discoverability)
+print_logfile() {
+	echo '----- Printing: ${MOCHA_REPORTER_OUTPUT} -----'
+	cat $MOCHA_REPORTER_OUTPUT
+	echo '----- End ${MOCHA_REPORTER_OUTPUT} -----'
+}
+
 # If tests are running against main, configure FlakyBot
 # to open issues on failures:
 if [[ $KOKORO_BUILD_ARTIFACTS_SUBDIR = *"release"* ]]; then
@@ -134,8 +141,13 @@ if [[ $KOKORO_BUILD_ARTIFACTS_SUBDIR = *"release"* ]]; then
 	cleanup() {
 	chmod +x $KOKORO_GFILE_DIR/linux_amd64/flakybot
 	$KOKORO_GFILE_DIR/linux_amd64/flakybot
+
+	# We can only set one trap per signal, so run `print_logfile` here
+	print_logfile
 	}
 	trap cleanup EXIT HUP
+else
+	trap print_logfile EXIT HUP
 fi
 
 npm test

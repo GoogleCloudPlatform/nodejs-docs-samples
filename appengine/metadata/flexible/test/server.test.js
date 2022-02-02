0c3e811a7e900f8bc6898aed0f63733c505f60cf
Sofia Leon <sofialeon@google.com>
2022-02-02 10:28:52 -0800

    fix order

--- appengine/metadata/flexible/test/server.test.js
+++ appengine/metadata/flexible/test/server.test.js
@@ -1,6 +1,6 @@
+const path = require('path');
 const supertest = require('supertest');
 const app = require(path.join(__dirname, '../', 'server.js'));
-const path = require('path');
 
 describe('gae_flex_metadata', () => {
   it('should be listening', async () => {

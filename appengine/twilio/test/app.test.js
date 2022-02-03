c6da110503dbadf3d49f51865a974fcb40df5ff8
Sofia Leon <sofialeon@google.com>
2022-02-03 14:23:21 -0800

    add test env variables

--- appengine/twilio/test/app.test.js
+++ appengine/twilio/test/app.test.js
@@ -18,6 +18,10 @@ const assert = require('assert');
 const sinon = require('sinon');
 const proxyquire = require('proxyquire').noCallThru();
 
+process.env.TWILIO_NUMBER = '+15005550006';
+process.env.TWILIO_ACCOUNT_SID = 'test';
+process.env.TWILIO_AUTH_TOKEN = 'test';
+
 const getSample = () => {
   const twilioMock = {
     messages: {

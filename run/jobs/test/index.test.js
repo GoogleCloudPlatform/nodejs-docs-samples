f9a243f4f8b26a9730f33a2a8c8782763bc3bdea
Sofia Leon <sofialeon@google.com>
2022-02-03 16:47:47 -0800

    build: move run/jobs to GH Actions

--- run/jobs/test/index.test.js
+++ run/jobs/test/index.test.js
@@ -12,8 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-const assert = require('assert');
 const {execSync} = require('child_process');
+const assert = require('assert');
 const exec = cmd => execSync(cmd, {encoding: 'utf-8'});
 
 describe('Unit Tests', () => {

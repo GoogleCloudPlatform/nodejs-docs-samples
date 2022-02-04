9218b4b68f31456065a3b3b6127117fbb02745f4
sofisl <55454395+sofisl@users.noreply.github.com>
2022-02-04 14:58:30 -0800

    build: move appengine/metadata flexible and standard to GH Actions (#2502)
    * build: move appengine/metadata flexible and standard to GH Actions

--- appengine/metadata/standard/test/server.test.js
+++ appengine/metadata/standard/test/server.test.js
@@ -1,5 +1,5 @@
-const supertest = require('supertest');
 const path = require('path');
+const supertest = require('supertest');
 const app = require(path.join(__dirname, '../', 'server.js'));
 
 it('should be listening', async () => {

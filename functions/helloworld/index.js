26f77f0f7b3077913e1ca0c485550f014f816679
Ace Nassri <anassri@google.com>
2022-02-28 10:44:26 -0800

    fix(functions/helloworld): include FF import
    Fixes b/221561399

--- functions/helloworld/index.js
+++ functions/helloworld/index.js
@@ -19,10 +19,9 @@
 // [START functions_helloworld_http]
 const escapeHtml = require('escape-html');
 
-// [END functions_helloworld_http]
-
 // [START functions_helloworld_get]
 const functions = require('@google-cloud/functions-framework');
+// [END functions_helloworld_http]
 
 // Register an HTTP function with the Functions Framework that will be executed
 // when you make an HTTP request to the deployed function's endpoint.
@@ -32,6 +31,7 @@ functions.http('helloGET', (req, res) => {
 // [END functions_helloworld_get]
 
 // [START functions_helloworld_http]
+
 // HTTP Cloud Function.
 functions.http('helloHttp', (req, res) => {
   res.send(`Hello ${escapeHtml(req.query.name || req.body.name || 'World')}!`);

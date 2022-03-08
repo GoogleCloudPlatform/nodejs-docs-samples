eca0809d0cd6cdfb7cf14b71f7a2185129e8ac57
nmykhailets <94851929+nmykhailets@users.noreply.github.com>
2022-03-08 05:01:32 +0200

    chore(samples): interactive tutorials code samples for write/rejoin/purge user events (#150)
    Co-authored-by: tetiana-karasova <tetiana.karasova@gmail.com>
    Co-authored-by: Karl Weinmeister <11586922+kweinmeister@users.noreply.github.com>
    Co-authored-by: Benjamin E. Coe <bencoe@google.com>

---
+++ retail/interactive-tutorials/events/write-user-event.js
@@ -0,0 +1,71 @@
+// Copyright 2022 Google Inc. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+'use strict';
+
+async function main() {
+  // [START retail_write_user_event]
+
+  // Imports the Google Cloud client library.
+  const {UserEventServiceClient} = require('@google-cloud/retail').v2;
+  const utils = require('../setup/setup-cleanup');
+
+  const projectNumber = process.env['GCLOUD_PROJECT'];
+  const visitorId = 'test_visitor_id';
+
+  // Placement
+  const parent = `projects/${projectNumber}/locations/global/catalogs/default_catalog`; // TO CHECK ERROR HANDLING PASTE THE INVALID CATALOG NAME HERE
+
+  // User event to write
+  const userEvent = {
+    eventType: 'home-page-view',
+    visitorId,
+    eventTime: {
+      seconds: Math.round(Date.now() / 1000),
+    },
+  };
+
+  // Instantiates a client.
+  const retailClient = new UserEventServiceClient();
+
+  const callWriteUserEvent = async () => {
+    // Construct request
+    const request = {
+      userEvent,
+      parent,
+    };
+
+    console.log('Write request: ', request);
+
+    // Run request
+    const response = await retailClient.writeUserEvent(request);
+    console.log(`Operation result: ${JSON.stringify(response[0])}`);
+  };
+
+  // Create new user event
+  console.log('Start to write user event');
+  await callWriteUserEvent();
+  console.log('Write operation finished');
+
+  // Purge user events by visitor id
+  await utils.purgeUserEvents(parent, visitorId);
+  // [END retail_write_user_event]
+}
+
+process.on('unhandledRejection', err => {
+  console.error(err.message);
+  process.exitCode = 1;
+});
+
+main();
---
+++ retail/interactive-tutorials/test/purge-user-events.test.js
@@ -0,0 +1,50 @@
+// Copyright 2022 Google Inc. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+'use strict';
+
+const path = require('path');
+const cp = require('child_process');
+const {before, describe, it} = require('mocha');
+const {assert} = require('chai');
+
+const execSync = cmd => cp.execSync(cmd, {encoding: 'utf-8'});
+
+const cwd = path.join(__dirname, '..');
+
+describe('Purge user events', () => {
+  const eventType = 'detail-page-view';
+  const visitorId = 'test_visitor_id';
+  let stdout;
+
+  before(async () => {
+    stdout = execSync(
+      'node interactive-tutorials/events/purge-user-events.js',
+      {cwd}
+    );
+  });
+
+  it('should check that new event already created', () => {
+    const regex = new RegExp(
+      `Created event ${eventType} with visitor id ${visitorId}`,
+      'g'
+    );
+    assert.match(stdout, regex);
+  });
+
+  it('should check that purge operation started', () => {
+    const regex = new RegExp('Purge operation in progress', 'g');
+    assert.match(stdout, regex);
+  });
+});

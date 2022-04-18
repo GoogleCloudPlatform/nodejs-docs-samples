1fd39b51da78ba1881a383ecfaa10f86be420739
Ace Nassri <anassri@google.com>
2022-04-18 15:49:27 -0700

    Fix lint

--- functions/v2/autoLabelInstance/test/index.test.js
+++ functions/v2/autoLabelInstance/test/index.test.js
@@ -12,7 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-const assert = require('assert');
 const sinon = require('sinon');
 const supertest = require('supertest');
 

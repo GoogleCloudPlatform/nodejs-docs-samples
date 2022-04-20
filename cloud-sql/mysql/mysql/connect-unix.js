605fd01d7ff6288234ac44360b585b04d9c23668
Jack Wotherspoon <jackwoth@google.com>
2022-04-20 11:56:13 -0400

    chore: update comments with proper dialects and method names (#2602)

--- cloud-sql/mysql/mysql/connect-unix.js
+++ cloud-sql/mysql/mysql/connect-unix.js
@@ -17,7 +17,7 @@
 const mysql = require('promise-mysql');
 
 // createUnixSocketPool initializes a Unix socket connection pool for
-// a Cloud SQL instance of SQL Server.
+// a Cloud SQL instance of MySQL.
 const createUnixSocketPool = async config => {
   // Note: Saving credentials in environment variables is convenient, but not
   // secure - consider a more secure solution such as

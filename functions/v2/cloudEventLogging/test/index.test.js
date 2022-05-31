a638e26593eabb5a2547c540f4b1cb88e88e0a76
ace-n <anassri@google.com>
2022-05-31 14:00:55 -0700

    chore(functions/v2): fix failing test

--- functions/v2/cloudEventLogging/test/index.test.js
+++ functions/v2/cloudEventLogging/test/index.test.js
@@ -26,40 +26,41 @@ require('../index.js');
 describe('functions_structured_logging_event', () => {
   let projectId;
 
+  const writeStub = sinon.stub();
+  const writeOriginal = process.stdout.write;
+
   before(async () => {
     const logging = new Logging();
     projectId = await logging.auth.getProjectId();
-    // Adding a spy to the write function in stdout
-    sinon.spy(process.stdout, 'write');
-  });
-
-  afterEach(() => {
-    process.stdout.write.restore();
   });
 
   it('structuredLoggingEvent: should correctly print logs', async () => {
     const structuredLoggingEvent = getFunction('structuredLoggingEvent');
 
     const expected = {
-      severity: 'NOTICE',
+      severity: 'ERROR',
       component: 'arbitrary-property',
       logName: `projects/${projectId}/logs/${pkg.name}`,
       message: 'Hello, world!',
     };
 
-    // Passing in a mocked CloudEvent object
+    // Only stub process.stdout.write for this function
+    // (This stub would otherwise swallow *all* console output)
+    process.stdout.write = writeStub;
     await structuredLoggingEvent(
+      // Passing in a mocked CloudEvent object
       new CloudEvent({
         type: 'google.cloud.pubsub.topic.v2.messagePublished',
         source: '//pubsub.googleapis.com/test-topic',
       })
     );
+    process.stdout.write = writeOriginal;
 
-    assert.ok(process.stdout.write.calledOnce);
-    assert.ok(process.stdout.write.getCall(0).args.length);
+    assert.ok(writeStub.callCount);
+    assert.ok(writeStub.getCall(0).args.length);
 
     // Capture the string output & parse
-    const result = JSON.parse(process.stdout.write.getCall(0).args[0]);
+    const result = JSON.parse(writeStub.getCall(0).args[0]);
 
     assert.equal(result.severity, expected.severity);
     assert.equal(result.logName, expected.logName);

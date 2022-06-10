49d6b2294eeb15aa9ef3a3ca7938cffa750fd107
Nicholas Cook <irataxy@users.noreply.github.com>
2022-06-10 15:20:10 -0700

    docs(samples): add Video Stitcher slate and CDN key code samples and tests (#6)
    Thank you for opening a Pull Request! Before submitting your PR, there are a few things you can do to make sure it goes smoothly:
    - [X] Make sure to open an issue as a [bug/issue](https://github.com/googleapis/nodejs-video-stitcher/issues/new/choose) before writing your code!  That way we can discuss the change, evaluate designs, and agree on the general idea
    - [X] Ensure the tests and linter pass
    - [ ] Code coverage does not decrease (if any source code was changed)
    - [ ] Appropriate docs were updated (if necessary)
    
    Fixes b:202972969

---
+++ media/video-stitcher/createSlate.js
@@ -0,0 +1,56 @@
+/**
+ * Copyright 2022, Google, Inc.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+'use strict';
+
+function main(projectId, location, slateId, slateUri) {
+  // [START video_stitcher_create_slate]
+  /**
+   * TODO(developer): Uncomment these variables before running the sample.
+   */
+  // projectId = 'my-project-id';
+  // location = 'us-central1';
+  // slateId = 'my-slate';
+  // slateUri = 'https://my-slate-uri/test.mp4';
+
+  // Imports the Video Stitcher library
+  const {VideoStitcherServiceClient} =
+    require('@google-cloud/video-stitcher').v1;
+  // Instantiates a client
+  const stitcherClient = new VideoStitcherServiceClient();
+
+  async function createSlate() {
+    // Construct request
+    const request = {
+      parent: stitcherClient.locationPath(projectId, location),
+      slate: {
+        uri: slateUri,
+      },
+      slateId: slateId,
+    };
+    const [slate] = await stitcherClient.createSlate(request);
+    console.log(`Slate: ${slate.name}`);
+  }
+
+  createSlate();
+  // [END video_stitcher_create_slate]
+}
+
+// node createSlate.js <projectId> <location> <slateId> <slateUri>
+process.on('unhandledRejection', err => {
+  console.error(err.message);
+  process.exitCode = 1;
+});
+main(...process.argv.slice(2));

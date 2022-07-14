b5422883d398e536524cd7df679c32d6abe38fc1
Fedor Isakov <isakovf@google.com>
2022-07-14 22:57:07 +0200

    chore(samples): add moving instances between zones samples (#747)

--- compute/test/samples.test.js
+++ compute/test/samples.test.js
@@ -79,6 +79,14 @@ describe('samples', () => {
     assert.match(output, /Instance created./);
   });
 
+  it('should print instance data', async () => {
+    const projectId = await instancesClient.getProjectId();
+    const output = execSync(
+      `node getInstance ${projectId} ${zone} ${instanceName}`
+    );
+    assert.include(output, `"name": "${instanceName}"`);
+  });
+
   it('should print instances list', async () => {
     const projectId = await instancesClient.getProjectId();
     const output = execSync(`node listInstances ${projectId} ${zone}`);

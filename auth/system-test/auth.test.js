4899d3a35430287e01ec56e2ed6193e0d455881b
Fedor Isakov <isakovf@google.com>
2022-09-16 12:20:03 +0200

    feat(auth): samples for auth

--- auth/system-test/auth.test.js
+++ auth/system-test/auth.test.js
@@ -16,27 +16,83 @@
 
 const assert = require('assert');
 const path = require('path');
-const {execSync} = require('child_process');
+const cp = require('child_process');
+const {auth} = require('google-auth-library');
 
 const cwd = path.join(__dirname, '..');
 const cmd = 'node auth.js';
 
 const BUCKET_NAME = 'long-door-651';
 const GOOGLE_CLOUD_PROJECT = 'long-door-651';
+const TARGET_AUDIENCE = 'iap.googleapis.com';
+const ZONE = 'us-central1-a';
 process.env.GOOGLE_CLOUD_PROJECT = 'long-door-651';
 
-it('should load credentials implicitly', () => {
-  const output = execSync(`${cmd} auth-cloud-implicit`, {cwd, shell: true});
-  assert.strictEqual(output.includes(BUCKET_NAME), true);
-});
+const keyFile = process.env.GOOGLE_APPLICATION_CREDENTIALS;
 
-it('should load credentials explicitly', () => {
-  const project = GOOGLE_CLOUD_PROJECT;
-  const keyfile = process.env.GOOGLE_APPLICATION_CREDENTIALS;
-  console.log(`${cmd} auth-cloud-explicit -p ${project} -k ${keyfile}`);
-  const output = execSync(
-    `${cmd} auth-cloud-explicit -p ${project} -k ${keyfile}`,
-    {cwd, shell: true}
-  );
-  assert.strictEqual(output.includes(BUCKET_NAME), true);
+const execSync = (command, opts) => {
+  return cp.execFile(command, Object.assign({encoding: 'utf-8'}, opts));
+};
+
+describe('auth samples', () => {
+  it('should load credentials implicitly', async () => {
+    const output = execSync(`${cmd} auth-cloud-implicit`, {cwd, shell: true});
+    assert.strictEqual(output.includes(BUCKET_NAME), true);
+  });
+
+  it('should load credentials explicitly', async () => {
+    const project = GOOGLE_CLOUD_PROJECT;
+    const keyfile = process.env.GOOGLE_APPLICATION_CREDENTIALS;
+    console.log(`${cmd} auth-cloud-explicit -p ${project} -k ${keyfile}`);
+    const output = execSync(
+      `${cmd} auth-cloud-explicit -p ${project} -k ${keyfile}`,
+      {cwd, shell: true}
+    );
+    assert.strictEqual(output.includes(BUCKET_NAME), true);
+  });
+
+  it('should authenticate explicitly', async () => {
+    const output = execSync('node authenticateExplicit');
+
+    assert.match(output, /Listed all storage buckets./);
+  });
+
+  it('should authenticate implicitly with adc', async () => {
+    const projectId = await auth.getProjectId();
+
+    const output = execSync(
+      `node authenticateImplicitWithAdc ${projectId} ${ZONE}`
+    );
+
+    assert.match(output, /Listed all storage buckets./);
+  });
+
+  it('should get id token from metadata server', async () => {
+    const output = execSync(
+      'node idTokenFromMetadataServer https://www.google.com'
+    );
+
+    assert.match(output, /Generated ID token./);
+  });
+
+  it('should get id token from service account', async () => {
+    const output = execSync(
+      `node idTokenFromServiceAccount ${TARGET_AUDIENCE} ${keyFile}`
+    );
+
+    assert.match(output, /Generated ID token./);
+  });
+
+  it('should verify google id token', async () => {
+    const jsonConfig = require(keyFile);
+    const client = auth.fromJSON(jsonConfig);
+
+    const idToken = await client.fetchIdToken(TARGET_AUDIENCE);
+
+    const output = execSync(
+      `node verifyGoogleIdToken ${idToken} ${TARGET_AUDIENCE} https://www.googleapis.com/oauth2/v3/certs`
+    );
+
+    assert.match(output, /ID token verified./);
+  });
 });

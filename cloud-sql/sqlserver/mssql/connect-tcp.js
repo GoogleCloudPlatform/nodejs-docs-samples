5c5071bb9f04dd5c5af622c13e7ba00df0f53c29
Jonathan Simon <jsimonweb@gmail.com>
2022-10-18 11:53:11 -0700

    Update Cloud SQL for SQL Server sample encrypted connection

--- cloud-sql/sqlserver/mssql/connect-tcp.js
+++ cloud-sql/sqlserver/mssql/connect-tcp.js
@@ -29,13 +29,24 @@ const createTcpPool = async config => {
     user: process.env.DB_USER, // e.g. 'my-db-user'
     password: process.env.DB_PASS, // e.g. 'my-db-password'
     database: process.env.DB_NAME, // e.g. 'my-database'
-    options: {
-      trustServerCertificate: true,
-    },
     // ... Specify additional properties here.
     ...config,
   };
-  return await mssql.connect(dbConfig);
+  // [END cloud_sql_sqlserver_mssql_connect_tcp]
+
+  // (OPTIONAL) Configure encrypted connection
+  // For deployments that connect directly to a Cloud SQL instance without
+  // using the Cloud SQL Proxy, configure encrypted connection.
+  if (process.env.PRIVATE_IP) {
+    dbConfig.options = {
+      trustServerCertificate: true,
+      encrypt: true,
+    };
+  }
+
+  // [START cloud_sql_mysql_mysql_connect_tcp]
+  // Establish a connection to the database.
+  return mssql.connect(dbConfig);
 };
 // [END cloud_sql_sqlserver_mssql_connect_tcp]
 module.exports = createTcpPool;

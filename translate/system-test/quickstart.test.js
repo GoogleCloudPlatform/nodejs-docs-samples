googleapis/nodejs-logging

Vision snippets. (#294)

samples/system-test/quickstart.test.js
@@ -23,14 +23,14 @@ const uuid = require(`uuid`);
 
 const logName = `nodejs-docs-samples-test-${uuid.v4()}`;
 
-test.after(async () => {
+test.after.always(async () => {
   try {
     await logging.log(logName).delete();
   } catch (err) {} // ignore error
 });
 
 test.beforeEach(stubConsole);
-test.afterEach(restoreConsole);
+test.afterEach.always(restoreConsole);
 
 test.cb(`should log an entry`, (t) => {
   const expectedlogName = `my-log`;

GoogleCloudPlatform/nodejs-docs-samples

translate/system-test/quickstart.test.js
@@ -21,7 +21,7 @@ const proxyquire = require(`proxyquire`).noPreserveCache();
 const translate = proxyquire(`@google-cloud/translate`, {})();
 
 test.before(stubConsole);
-test.after(restoreConsole);
+test.after.always(restoreConsole);
 
 test.cb(`should translate a string`, (t) => {
   const string = `Hello, world!`;
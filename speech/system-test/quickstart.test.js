googleapis/nodejs-logging

Vision snippets. (#294)

samples/system-test/quickstart.test.js
@@ -23,14 +23,14 @@ const uuid = require(`uuid`);
 
 const logName = `nodejs-docs-samples-test-${uuid.v4()}`;
 
-test.after(async () => {
+test.after.always(async () => {
   try {
     await logging.log(logName).delete();
   } catch (err) {} // ignore error
 });
 
 test.beforeEach(stubConsole);
-test.afterEach(restoreConsole);
+test.afterEach.always(restoreConsole);
 
 test.cb(`should log an entry`, (t) => {
   const expectedlogName = `my-log`;

GoogleCloudPlatform/nodejs-docs-samples

speech/system-test/quickstart.test.js
@@ -28,7 +28,7 @@ const config = {
 };
 
 test.before(stubConsole);
-test.after(restoreConsole);
+test.after.always(restoreConsole);
 
 test.cb(`should detect speech`, (t) => {
   const expectedFileName = `./resources/audio.raw`;
googleapis/nodejs-logging

Cleanup App Engine samples and re-work tests. (#354)

samples/system-test/quickstart.test.js
@@ -1,5 +1,5 @@
 /**
- * Copyright 2016, Google, Inc.
+ * Copyright 2017, Google, Inc.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -15,11 +15,13 @@
 
 'use strict';
 
-require(`../../system-test/_setup`);
-
 const proxyquire = require(`proxyquire`).noPreserveCache();
-const logging = proxyquire(`@google-cloud/logging`, {})();
+const sinon = require(`sinon`);
+const test = require(`ava`);
+const tools = require(`@google-cloud/nodejs-repo-tools`);
 const uuid = require(`uuid`);
+
+const logging = proxyquire(`@google-cloud/logging`, {})();
 
 const logName = `nodejs-docs-samples-test-${uuid.v4()}`;
 
@@ -29,8 +31,8 @@ test.after.always(async () => {
   } catch (err) {} // ignore error
 });
 
-test.beforeEach(stubConsole);
-test.afterEach.always(restoreConsole);
+test.beforeEach(tools.stubConsole);
+test.afterEach.always(tools.restoreConsole);
 
 test.cb(`should log an entry`, (t) => {
   const expectedlogName = `my-log`;

GoogleCloudPlatform/nodejs-docs-samples

translate/system-test/quickstart.test.js
@@ -1,5 +1,5 @@
 /**
- * Copyright 2016, Google, Inc.
+ * Copyright 2017, Google, Inc.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -15,13 +15,15 @@
 
 'use strict';
 
-require(`../../system-test/_setup`);
-
 const proxyquire = require(`proxyquire`).noPreserveCache();
+const sinon = require(`sinon`);
+const test = require(`ava`);
+const tools = require(`@google-cloud/nodejs-repo-tools`);
 const translate = proxyquire(`@google-cloud/translate`, {})();
 
-test.before(stubConsole);
-test.after.always(restoreConsole);
+test.before(tools.checkCredentials);
+test.before(tools.stubConsole);
+test.after.always(tools.restoreConsole);
 
 test.cb(`should translate a string`, (t) => {
   const string = `Hello, world!`;
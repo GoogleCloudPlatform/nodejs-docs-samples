7f13e56b6eb8f55202dc837628b21355ec33050f
Adam Ross <adamross@google.com>
2022-11-17 15:40:38 -0800

    fix: uncomment an important part of the file

--- .kokoro/build-with-run.sh
+++ .kokoro/build-with-run.sh
@@ -23,14 +23,14 @@ _run_error_log() {
 trap '_run_error_log' ERR
 
 # Activate mocha config
-# export MOCHA_REPORTER_OUTPUT=${PROJECT}_sponge_log.xml
-# export MOCHA_REPORTER=xunit
-# pushd github/nodejs-docs-samples
-# mv .kokoro/.mocharc.js .
-# popd
+export MOCHA_REPORTER_OUTPUT=${PROJECT}_sponge_log.xml
+export MOCHA_REPORTER=xunit
+pushd github/nodejs-docs-samples
+mv .kokoro/.mocharc.js .
+popd
 
-# export GOOGLE_CLOUD_PROJECT=nodejs-docs-samples-tests
-# pushd github/nodejs-docs-samples/${PROJECT}
+export GOOGLE_CLOUD_PROJECT=nodejs-docs-samples-tests
+pushd github/nodejs-docs-samples/${PROJECT}
 
 # Verify changes worth testing.
 SIGNIFICANT_CHANGES="$(git --no-pager diff --name-only main..HEAD | grep -Ev '(\.md$|^\.github)' || true)"

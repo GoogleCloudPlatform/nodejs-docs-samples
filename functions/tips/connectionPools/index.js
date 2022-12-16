2aae731bb2b8aaec27d444403fb03e160dc487cd
Kenneth Rosario Acevedo <kennethrosario@google.com>
2022-12-16 21:48:45 +0000

    chore(functions/connectionPooling): Update connectionPool sample to use gen 2 signature

--- functions/tips/connectionPools/index.js
+++ functions/tips/connectionPools/index.js
@@ -20,6 +20,8 @@ const fetch = require('node-fetch');
 const http = require('http');
 const https = require('https');
 
+const functions = require('@google-cloud/functions-framework');
+
 const httpAgent = new http.Agent({keepAlive: true});
 const httpsAgent = new https.Agent({keepAlive: true});
 
@@ -29,7 +31,7 @@ const httpsAgent = new https.Agent({keepAlive: true});
  * @param {Object} req Cloud Function request context.
  * @param {Object} res Cloud Function response context.
  */
-exports.connectionPooling = async (req, res) => {
+functions.http('connectionPooling', async (req, res) => {
   try {
     // TODO(optional): replace this with your own URL.
     const url = 'https://www.example.com/';
@@ -37,12 +39,12 @@ exports.connectionPooling = async (req, res) => {
     // Select the appropriate agent to use based on the URL.
     const agent = url.includes('https') ? httpsAgent : httpAgent;
 
-    const {data} = await fetch(url, {agent});
-    const text = await data.text();
+    const fetchResponse = await fetch(url, {agent});
+    const text = await fetchResponse.text();
 
     res.status(200).send(`Data: ${text}`);
   } catch (err) {
     res.status(500).send(`Error: ${err.message}`);
   }
-};
+});
 // [END functions_tips_connection_pooling]

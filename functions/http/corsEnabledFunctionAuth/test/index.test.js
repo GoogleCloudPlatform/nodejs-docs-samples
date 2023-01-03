094375a57b6800250aca2f28254cd3283fce5e4c
Gareth <garethgeorge@google.com>
2023-01-03 12:08:18 -0800

    chore(functions/v2): migrate HTTP Auth with CORS sample to gen2 (#2941)

--- functions/http/corsEnabledFunctionAuth/test/index.test.js
+++ functions/http/corsEnabledFunctionAuth/test/index.test.js
@@ -17,21 +17,7 @@
 const sinon = require('sinon');
 const proxyquire = require('proxyquire').noCallThru();
 const assert = require('assert');
-
-const getSample = () => {
-  const requestPromise = sinon
-    .stub()
-    .returns(new Promise(resolve => resolve('test')));
-
-  return {
-    sample: proxyquire('../', {
-      'request-promise': requestPromise,
-    }),
-    mocks: {
-      requestPromise: requestPromise,
-    },
-  };
-};
+const {getFunction} = require('@google-cloud/functions-framework/testing');
 
 const getMocks = () => {
   const req = {
@@ -75,15 +61,22 @@ const restoreConsole = function () {
 beforeEach(stubConsole);
 afterEach(restoreConsole);
 
+beforeEach(() => {
+  const requestPromise = sinon
+    .stub()
+    .returns(new Promise(resolve => resolve('test')));
+
+  proxyquire('../', {
+    'request-promise': requestPromise,
+  });
+});
+
 describe('functions_http_cors_auth', () => {
   it('http:cors: should respond to preflight request (auth)', () => {
     const mocks = getMocks();
-    const httpSample = getSample();
+    const corsEnabledFunctionAuth = getFunction('corsEnabledFunctionAuth');
 
-    httpSample.sample.corsEnabledFunctionAuth(
-      mocks.corsPreflightReq,
-      mocks.res
-    );
+    corsEnabledFunctionAuth(mocks.corsPreflightReq, mocks.res);
 
     assert.strictEqual(mocks.res.status.calledOnceWith(204), true);
     assert.strictEqual(mocks.res.send.calledOnce, true);
@@ -91,9 +84,9 @@ describe('functions_http_cors_auth', () => {
 
   it('http:cors: should respond to main request (auth)', () => {
     const mocks = getMocks();
-    const httpSample = getSample();
+    const corsEnabledFunctionAuth = getFunction('corsEnabledFunctionAuth');
 
-    httpSample.sample.corsEnabledFunctionAuth(mocks.corsMainReq, mocks.res);
+    corsEnabledFunctionAuth(mocks.corsMainReq, mocks.res);
 
     assert.strictEqual(mocks.res.send.calledOnceWith('Hello World!'), true);
   });

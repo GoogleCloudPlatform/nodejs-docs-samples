e06d1b329f32fffa10272d2cbcdc40581316d0d2
Karl Weinmeister <kweinmeister@google.com>
2023-03-11 09:02:28 -0600

    Use .mocharc.js from .kokoro/

--- .mocharc.js
+++ .mocharc.js
@@ -1,4 +1,4 @@
-// Copyright 2023 Google LLC
+// Copyright 2020 Google LLC
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -11,22 +11,22 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-const config = {
-    "enable-source-maps": true,
-    "throw-deprecation": true,
-    "timeout": 10000,
-    "recursive": true
-  }
-  if (process.env.MOCHA_THROW_DEPRECATION === 'false') {
+const config = {};
+
+if (process.env.MOCHA_THROW_DEPRECATION === 'false') {
     delete config['throw-deprecation'];
-  }
-  if (process.env.MOCHA_REPORTER) {
+}
+if (process.env.MOCHA_REPORTER) {
     config.reporter = process.env.MOCHA_REPORTER;
-  }
-  if (process.env.MOCHA_REPORTER_OUTPUT) {
-    config['reporter-option'] = `output=${process.env.MOCHA_REPORTER_OUTPUT}`;
-  }
-  if (process.env.MOCHA_REPORTER_SUITENAME) {
-    config['reporter-option'] = `suiteName=${process.env.MOCHA_REPORTER_SUITENAME}`;
-  }
-  module.exports = config
+}
+
+const reporterOptions = [];
+if (process.env.MOCHA_REPORTER_OUTPUT) {
+    reporterOptions.push(`output=${process.env.MOCHA_REPORTER_OUTPUT}`);
+}
+if (process.env.MOCHA_REPORTER_SUITENAME) {
+    reporterOptions.push(`suiteName=${process.env.MOCHA_REPORTER_SUITENAME}`);
+}
+config['reporter-option'] = reporterOptions;
+
+module.exports = config

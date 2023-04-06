e70bb9131e0cc51de257735219b8e76c71be4336
pattishin@google.com <pattishin@users.noreply.github.com>
2023-04-06 12:18:22 -0700

    fix: matching test names with region tags per enforcer requirements

--- appengine/hello-world/flexible_nodejs16_and_earlier/test/app.test.js
+++ appengine/hello-world/flexible_nodejs16_and_earlier/test/app.test.js
@@ -1,7 +1,7 @@
 const app = require('../app');
 const request = require('supertest');
 
-describe('gae_flex_quickstart', () => {
+describe('gae_flex_quickstart_v1', () => {
   describe('GET /', () => {
     it('should get 200', done => {
       request(app).get('/').expect(200, done);
--- appengine/metadata/flexible_nodejs16_and_earlier/test/server.test.js
+++ appengine/metadata/flexible_nodejs16_and_earlier/test/server.test.js
@@ -2,7 +2,7 @@ const path = require('path');
 const app = require(path.join(__dirname, '../', 'server.js'));
 const supertest = require('supertest');
 
-describe('gae_flex_metadata', () => {
+describe('gae_flex_metadata_v1', () => {
   it('should be listening', async () => {
     await supertest(app).get('/').expect(200);
   });

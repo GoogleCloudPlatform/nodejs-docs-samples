60fd65c12e88c0de9189fd734856026560d9f498
dinesh-crest <dinesh.choudhary@crestdatasys.com>
2023-05-08 16:52:28 +0530

    DLP: Added sample for inspect string with exclusion dict Added test cases for same

--- dlp/system-test/inspect.test.js
+++ dlp/system-test/inspect.test.js
@@ -482,17 +482,17 @@ describe('inspect', () => {
   // dlp_inspect_string_with_exclusion_dict
   it('should inspect using exclusion word list', () => {
     const output = execSync(
-      `node inspectStringWithExclusionDict.js ${projectId} "Some email addresses: gary@example.com, example@example.com" EMAIL_ADDRESS "example@example.com"`
+      `node inspectStringWithExclusionDict.js ${projectId} "Some email addresses: gary@example.com, example@example.com" EMAIL_ADDRESS "gary@example.com"`
     );
-    assert.match(output, /Quote: gary@example.com/);
-    assert.notMatch(output, /Quote: example@example.com/);
+    assert.notMatch(output, /Quote: gary@example.com/);
+    assert.match(output, /Quote: example@example.com/);
   });
 
   it('should report any errors while inspecting a string', () => {
     let output;
     try {
       output = execSync(
-        `node inspectStringWithExclusionDict.js ${projectId} "Some email addresses: gary@example.com, example@example.com" BAD_TYPE "example@example.com"`
+        `node inspectStringWithExclusionDict.js ${projectId} "Some email addresses: gary@example.com, example@example.com" BAD_TYPE "gary@example.com"`
       );
     } catch (err) {
       output = err.message;

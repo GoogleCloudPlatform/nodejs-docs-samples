9d630b34266ef04731cfadf069a14c9bc5b6e8f9
dinesh-crest <dinesh.choudhary@crestdatasys.com>
2023-05-26 17:23:54 +0530

    DLP: Added sample for create stored infotype

--- dlp/system-test/metadata.test.js
+++ dlp/system-test/metadata.test.js
@@ -17,17 +17,40 @@
 const {assert} = require('chai');
 const {describe, it, before} = require('mocha');
 const cp = require('child_process');
+const uuid = require('uuid');
 const DLP = require('@google-cloud/dlp');
+
+const dataProject = 'DATA_PROJECT';
+const dataSetId = 'DATASET_ID';
+const tableId = 'TABLE_ID';
+const fieldId = 'FIELD_ID';
 
 const execSync = cmd => cp.execSync(cmd, {encoding: 'utf-8'});
 
 const client = new DLP.DlpServiceClient();
 describe('metadata', () => {
-  let projectId;
+  let projectId, storedInfoTypeId;
 
   before(async () => {
     projectId = await client.getProjectId();
   });
+
+  // Delete stored infotypes created in the snippets.
+  afterEach(async () => {
+    if (!storedInfoTypeId) {
+      return;
+    }
+    const request = {
+      name: storedInfoTypeId,
+    };
+    try {
+      await client.deleteStoredInfoType(request);
+      storedInfoTypeId = '';
+    } catch (err) {
+      throw `Error in deleting store infoType: ${err.message || err}`;
+    }
+  });
+
   it('should list info types', () => {
     const output = execSync(`node metadata.js ${projectId} infoTypes`);
     assert.match(output, /US_DRIVERS_LICENSE_NUMBER/);
@@ -38,5 +61,30 @@ describe('metadata', () => {
       `node metadata.js ${projectId} infoTypes "supported_by=RISK_ANALYSIS"`
     );
     assert.notMatch(output, /US_DRIVERS_LICENSE_NUMBER/);
+  });
+
+  // dlp_create_stored_infotype
+  it.skip('should create a stored infotype', () => {
+    const infoTypeId = `stored-infoType-${uuid.v4()}`;
+    const infoTypeOutputPath = 'INFOTYPE_OUTPUT_PATH';
+    const output = execSync(
+      `node createStoredInfoType.js ${projectId} ${infoTypeId} ${infoTypeOutputPath} ${dataProject} ${dataSetId} ${tableId} ${fieldId}`
+    );
+    assert.match(output, /InfoType stored successfully:/);
+    storedInfoTypeId = output.split(':')[1].trim();
+  });
+
+  it.skip('should handle stored infotype creation errors', () => {
+    let output;
+    const infoTypeId = `stored-infoType-${uuid.v4()}`;
+    const infoTypeOutputPath = 'INFOTYPE_OUTPUT_PATH';
+    try {
+      output = execSync(
+        `node createStoredInfoType.js BAD_PROJECT_ID ${infoTypeId} ${infoTypeOutputPath} ${dataProject} ${dataSetId} ${tableId} ${fieldId}`
+      );
+    } catch (err) {
+      output = err.message;
+    }
+    assert.include(output, 'INVALID_ARGUMENT');
   });
 });

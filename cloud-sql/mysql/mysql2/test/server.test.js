7b8f34ab597585b9d55c2b17631c05b5e2a749c9
foo@bar.ca <ruyadorno@google.com>
2023-06-08 14:59:17 -0400

    feat(cloud-sql): add mysql2 connector sample
    This new sample uses the Cloud SQL Node.js Connector to connect to
    Cloud SQL instances instead of relying on the standalone Cloud SQL
    Proxy.
    
    The original example from `cloud-sql/mysql/mysql` has been used as a
    starting point but uses the `mysql2` driver instead thus residing in a
    new location: `cloud-sql/mysql/mysql2`.
    
    Ref: https://github.com/GoogleCloudPlatform/cloud-sql-nodejs-connector

---
+++ cloud-sql/mysql/mysql2/test/server.test.js
@@ -0,0 +1,78 @@
+// Copyright 2023 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+'use strict';
+
+const path = require('path');
+const request = require('supertest');
+const assert = require('assert');
+
+const SAMPLE_PATH = path.join(__dirname, '../server/server.js');
+
+const serverConnector = require(SAMPLE_PATH);
+
+const _instance_connect_backup = process.env.INSTANCE_CONNECTION_NAME;
+delete process.env.INSTANCE_CONNECTION_NAME;
+
+const serverTcp = require(SAMPLE_PATH);
+
+const _instance_host_backup = process.env.INSTANCE_HOST;
+delete process.env.INSTANCE_HOST;
+
+const serverUnix = require(SAMPLE_PATH);
+
+process.env.INSTANCE_CONNECTION_NAME = _instance_connect_backup;
+process.env.INSTANCE_HOST = _instance_host_backup;
+
+after(() => {
+  serverTcp.close();
+});
+
+it('should display the default page with connector', async () => {
+  await request(serverConnector)
+    .get('/')
+    .expect(response => {
+      assert.ok(response.text.includes('Tabs VS Spaces'));
+    })
+    .expect(200);
+});
+
+it('should display the default page over tcp', async () => {
+  await request(serverTcp)
+    .get('/')
+    .expect(response => {
+      assert.ok(response.text.includes('Tabs VS Spaces'));
+    })
+    .expect(200);
+});
+
+it('should display the default page over unix', async () => {
+  await request(serverUnix)
+    .get('/')
+    .expect(response => {
+      assert.ok(response.text.includes('Tabs VS Spaces'));
+    })
+    .expect(200);
+});
+
+it('should handle insert error', async () => {
+  const expectedResult = 'Invalid team specified';
+
+  await request(serverTcp)
+    .post('/')
+    .expect(400)
+    .expect(response => {
+      assert.ok(response.text.includes(expectedResult));
+    });
+});

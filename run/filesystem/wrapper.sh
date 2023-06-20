c943c48036c5a19b74dcccf7d9e57b715975e064
rogerthatdev <roger.that.dev@gmail.com>
2023-06-20 16:37:13 -0700

    add Dockerfile and wrapper script

---
+++ run/filesystem/wrapper.sh
@@ -0,0 +1,27 @@
+# Copyright 2023 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# [START cloudrun_fs_script]
+#!/usr/bin/env bash
+set -eo pipefail
+
+# Create mount directory for service.
+mkdir -p $MNT_DIR
+
+echo "Mounting Cloud Filestore."
+mount -o nolock $FILESTORE_IP_ADDRESS:/$FILE_SHARE_NAME $MNT_DIR
+echo "Mounting completed."
+
+npm start
+# [END cloudrun_fs_script]
+

133a3008c800e5153d522d5c0f0c13ab66bf5e86
rogerthatdev <roger.that.dev@gmail.com>
2023-06-20 14:49:07 -0700

    test for new file write

--- run/filesystem/test/index.test.js
+++ run/filesystem/test/index.test.js
@@ -1,7 +1,9 @@
 const path = require('path');
 const supertest = require('supertest');
 const assert = require('chai').expect;
-
+const mock = require('mock-fs');
+const fs = require('fs');
+const mntDir = process.env.MNT_DIR || '/mnt/nfs/filestore'
 let request;
 describe('Unit tests', () => {
   const defaultLogFunction = console.log;
@@ -11,9 +13,16 @@ describe('Unit tests', () => {
     console.log = (msg) => {
       consoleOutput += msg + '\n';
     };
+    mock({
+      [mntDir]: mock.directory({
+        mode: 0755,
+        items: {}
+      })
+    });
     request = supertest(app);
   });
   after(() => {
+    mock.restore();
     console.log = defaultLogFunction;
     console.log('\nconsole.log output:\n---------');
     console.log(consoleOutput);
@@ -22,15 +31,23 @@ describe('Unit tests', () => {
   describe('GET /', () => {
     it('responds with 302 Found and redirects to mount directory', async () => {
       response = await request.get('/');
-      assert(response.header.location).to.eql('/mnt/nfs/filestore');
+      assert(response.header.location).to.eql(mntDir);
       assert(response.status).to.eql(302);
     });
   });
   describe('GET nonexistant path', () => {
     it('responds with 302 Found and redirects to mount directory', async () => {
       response = await request.get('/nonexistant');
-      assert(response.header.location).to.eql('/mnt/nfs/filestore');
+      assert(response.header.location).to.eql(mntDir);
       assert(response.status).to.eql(302);
+    });
+  });
+  describe('GET mount path', () => {
+    it('writes a file', async () => {
+      response = await request.get(mntDir);
+      fs.readdir(mntDir, (err, files) => {
+        assert(files.length).to.eql(1)
+      });
     });
   });
 });

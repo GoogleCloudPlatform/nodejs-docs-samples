8363f5d575a1655725317e5dd98ac5a8c231cf1b
rogerthatdev <roger.that.dev@gmail.com>
2023-07-14 12:33:22 -0700

    add before and after to e2e test

--- run/filesystem/test/system.test.js
+++ run/filesystem/test/system.test.js
@@ -14,26 +14,50 @@
 
 const assert = require('chai').expect;
 const {GoogleAuth} = require('google-auth-library');
+const {execSync} = require('child_process');
 
-const authenticate = async () => {
-    const auth = new GoogleAuth({
-        scopes: 'https://www.googleapis.com/auth/cloud-platform'
-    });
-    const client = await auth.getClient();
-    const projectId = await auth.getProjectId();
-    const url = `https://dns.googleapis.com/dns/v1/projects/${projectId}`;
+const auth = new GoogleAuth();
+
+const getEnvironmentVariable = (variable, defaultValue) => {
+  if (variable in process.env) {
+    return process.env[variable];
+  }
+  if (defaultValue) {
+    return defaultValue;
+  }
+  throw Error(
+    `Environment variable ${variable} not set and no default value provided.`
+  );
 };
 
-authenticate().catch(console.error)
+const gcloudCmdExec = command => {
+  // Returns standard output of gcloud command
+  try {
+    return execSync(command, {timeout: 240000});
+  } catch (err) {
+    throw Error(err);
+  }
+};
+
+auth.getClient();
+const serviceName = getEnvironmentVariable('SERVICE_NAME', 'filesystem-app');
+const projectId = getEnvironmentVariable('GOOGLE_CLOUD_PROJECT', 'werwerew');
 
 describe('End-to-end test', () => {
   before(() => {
-    const projectID = getEnvironmentVariable('GOOGLE_CLOUD_PROJECT');
+    const buildCmd = `gcloud builds submit --config=test/e2e_test_setup.yaml --project ${projectId} --substitutions=_FILESTORE_IP_ADDRESS=10.42.154.2,_RUN_SERVICE=${serviceName}`;
+    console.log('Deploying required Google Cloud resources...');
+    gcloudCmdExec(buildCmd);
   });
   after(() => {
-    console.log('Run e2e_test_cleanup');
+    const cleanUpCmd = `gcloud builds submit --config=test/e2e_test_cleanup.yaml --project ${projectId} --substitutions=_RUN_SERVICE=${serviceName};`;
+    console.log('Cleaning up Google Cloud Resources...');
+    gcloudCmdExec(cleanUpCmd);
   });
-  it('completes Cloud Build e2e', () => {
+  it('has Cloud Run end point', () => {
+    assert(true).to.eql(true); // temp assert
+  });
+  it('GET endpoint responds with 200', () => {
     assert(true).to.eql(true); // temp assert
   });
 });

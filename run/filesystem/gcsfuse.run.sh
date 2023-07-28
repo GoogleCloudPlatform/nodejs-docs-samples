3dce00ed616c4fa8ca8cbd01919f1bb6898b2814
Roger Martinez <31829545+rogerthatdev@users.noreply.github.com>
2023-07-27 17:00:36 -0700

    Update run/filesystem/gcsfuse.run.sh
    Co-authored-by: Averi Kitsch <akitsch@google.com>

--- run/filesystem/gcsfuse.run.sh
+++ run/filesystem/gcsfuse.run.sh
@@ -23,11 +23,7 @@ echo "Mounting GCS Fuse."
 gcsfuse --debug_gcs --debug_fuse $BUCKET $MNT_DIR 
 echo "Mounting completed."
 
-# Run the web service on container startup. Here we use the gunicorn
-# webserver, with one worker process and 8 threads.
-# For environments with multiple CPU cores, increase the number of workers
-# to be equal to the cores available.
-# Timeout is set to 0 to disable the timeouts of the workers to allow Cloud Run to handle instance scaling.
+# Start the application
 node index.js &
 
 # Exit immediately when one of the background processes terminate.

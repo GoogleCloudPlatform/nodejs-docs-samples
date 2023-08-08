05b0e2214bf50576f410ab8105222585c4fce51c
pattishin@google.com <pattishin@users.noreply.github.com>
2023-08-08 13:41:26 -0700

    refactor: shifting region tags to show imports tps could miss

--- functions/helloworld/helloworldHttp/test/sample.unit.http.test.js
+++ functions/helloworld/helloworldHttp/test/sample.unit.http.test.js
@@ -13,10 +13,10 @@
 // limitations under the License.
 
 // FF testing layer for declarative signatures
+// [START functions_http_unit_test]
 const {getFunction} = require('@google-cloud/functions-framework/testing');
 
 describe('functions_helloworld_http', () => {
-  // [START functions_http_unit_test]
   const sinon = require('sinon');
   const assert = require('assert');
   require('../');
@@ -40,7 +40,7 @@ describe('functions_helloworld_http', () => {
 
     assert.strictEqual(mocks.res.send.calledOnceWith('Hello World!'), true);
   });
-  // [END functions_http_unit_test]
+  
   it('helloHttp: should print a name with query', () => {
     const mocks = getMocks();
     mocks.req.query = {name: 'John'};
@@ -61,3 +61,4 @@ describe('functions_helloworld_http', () => {
     assert.strictEqual(mocks.res.send.calledOnceWith('Hello John!'), true);
   });
 });
+// [END functions_http_unit_test]

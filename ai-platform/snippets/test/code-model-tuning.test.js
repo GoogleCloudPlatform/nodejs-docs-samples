06f4564be6041e30716892c80925b861439c793e
gericdong <itseric@google.com>
2023-08-17 14:15:51 -0400

    fix(aiplatform): try to get the test to run after IAM role changed

--- ai-platform/snippets/test/code-model-tuning.test.js
+++ ai-platform/snippets/test/code-model-tuning.test.js
@@ -37,8 +37,7 @@ const modelDisplayName = `my-tuned-model-${timestampId}`
 const bucketName = `ucaip-samples-europe-west4/training_pipeline_output`;
 const bucketUri = `gs://${bucketName}/tune-model-nodejs`
 
-// TODO (b/295913150): Service account permission issue with pipeline tests
-describe.skip('Tune a code model', () => {
+describe('Tune a code model', () => {
   const stubConsole = function () {
     sinon.stub(console, 'error');
     sinon.stub(console, 'log');

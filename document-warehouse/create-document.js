754faf8abd9cb6fb78610d7d8a60870c23307569
Paulina Nguyen <paulinanguyen@google.com>
2023-09-18 09:33:45 -0700

    add test for create document

---
+++ document-warehouse/create-document.js
@@ -0,0 +1,102 @@
+/** Copyright 2023 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+'use strict';
+async function main(
+  projectNumber = 'YOUR_PROJECT_NUMBER',
+  location = 'YOUR_PROJECT_LOCATION',
+  userId = 'user:xxx@example.com',
+) {
+  // [START contentwarehouse_quickstart]
+
+  /**
+   * TODO(developer): Uncomment these variables before running the sample.
+   * const projectNumber = 'YOUR_PROJECT_NUMBER';
+   * const location = 'YOUR_PROJECT_LOCATION'; // Format is 'us' or 'eu'
+   * const userId = 'user:xxx@example.com'; // Format is "user:xxx@example.com"
+   */
+
+  // Import from google cloud
+  const {DocumentSchemaServiceClient, DocumentServiceClient} =
+    require('@google-cloud/contentwarehouse').v1;
+
+  // Create service client
+  const schemaClient = new DocumentSchemaServiceClient();
+  const serviceClient = new DocumentServiceClient();
+
+  // Get Document Schema
+  async function quickstart() {
+    // Initialize request argument(s)
+    const schemaRequest = {};
+    const documentRequest = {};
+
+    // The full resource name of the location, e.g.:
+    // projects/{project_number}/locations/{location}
+    const parent = `projects/${projectNumber}/locations/${location}`;
+    schemaRequest.parent = parent;
+    documentRequest.parent = parent;
+
+    // Property Definition
+    const propertyDefinition = {};
+    propertyDefinition.name = 'schema property 1'; // Must be unique within a document schema (case insensitive)
+    propertyDefinition.displayName = 'searchable text';
+    propertyDefinition.isSearchable = true;
+    propertyDefinition.textTypeOptions = {};
+
+    // Document Schema
+    const documentSchemaRequest = {};
+    documentSchemaRequest.displayName = 'My Test Schema';
+    documentSchemaRequest.propertyDefinitions = [];
+
+    schemaRequest.documentSchema = documentSchemaRequest;
+
+    // Create Document Schema
+    const documentSchema = await schemaClient.createDocumentSchema(schemaRequest);
+
+    // Property Value Definition
+    const documentProperty = {};
+    documentProperty.name = propertyDefinition.name;
+    documentProperty.values = {textValues: ["GOOG"]}; //TODO: how to get the right object type for this?
+
+    // Document Definition
+    const document = {};
+    document.displayName = 'My Test Document';
+    document.documentSchemaName = documentSchema[0].name;
+    document.plainText = "This is a sample of a document's text.";
+    document.properties = [];
+
+    documentRequest.document = document;
+
+    // Metadata Definition
+    documentRequest.requestMetadata = {userInfo: {id: userId}};
+
+    // Make Request
+    const response = serviceClient.createDocument(documentRequest);
+
+    // Print out response
+    response.then(
+      result => console.log(`Document Created: ${JSON.stringify(result)}`),
+      error => console.log(`${error}`)
+    );
+  }
+
+  // [END contentwarehouse_quickstart]
+  await quickstart();
+}
+
+main(...process.argv.slice(2)).catch(err => {
+  console.error(err);
+  process.exitCode = 1;
+});
--- document-warehouse/test/document.test.js
+++ document-warehouse/test/document.test.js
@@ -23,16 +23,14 @@ const {ProjectsClient} = require('@google-cloud/resource-manager').v3;
 const iamClient = new PoliciesClient();
 const projectClient = new ProjectsClient();
 
-const confirmationDeleted = 'Document Schema Deleted';
-const confirmationGet = 'Schema Found';
+const confirmationCreate = 'Document Created';
 
 const execSync = cmd => cp.execSync(cmd, {encoding: 'utf-8'});
 
-describe('Document schema tests', () => {
+describe('Document tests', () => {
   let projectNumber;
-  let documentSchema;
-  let documentSchemaId;
   const location = 'us';
+  const userId = 'serviceAccount:kokoro-system-test@long-door-651.iam.gserviceaccount.com';
 
   async function getProjectNumber() {
     const projectId = await iamClient.getProjectId();
@@ -43,39 +41,16 @@ describe('Document schema tests', () => {
     return projectNumber;
   }
 
-  function getDocumentSchemaId() {
-    const name = documentSchema.name;
-    const ids = name.split('/');
-    documentSchemaId = ids[ids.length - 1];
-  }
 
   before(async () => {
     projectNumber = await getProjectNumber();
   });
 
-  it('should create a document schema', async () => {
+  it('should create a document', async () => {
     const output = execSync(
-      `node create-document-schema.js ${projectNumber} ${location}`
-    );
-    documentSchema = JSON.parse(output)[0];
-    getDocumentSchemaId();
-
-    assert.notEqual(documentSchema, null);
-  });
-
-  it('should get created document schema', async () => {
-    const output = execSync(
-      `node get-document-schema.js ${projectNumber} ${location} ${documentSchemaId}`
+      `node create-document.js ${projectNumber} ${location} ${userId}`
     );
 
-    assert(output.startsWith(confirmationGet));
-  });
-
-  it('should delete a document schema', async () => {
-    const output = execSync(
-      `node delete-document-schema.js ${projectNumber} ${location} ${documentSchemaId}`
-    );
-
-    assert(output.startsWith(confirmationDeleted));
+    assert(output.startsWith(confirmationCreate));
   });
 });

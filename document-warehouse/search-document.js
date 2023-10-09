6c305fb84eb85afb6ca75aa435c47684db4e5810
paulinawins <paulinanguyen@google.com>
2023-10-08 22:16:18 -0400

    add search document sample code

---
+++ document-warehouse/search-document.js
@@ -0,0 +1,82 @@
+/** Copyright 2023 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+'use strict';
+async function main(
+  projectNumber = 'YOUR_PROJECT_NUMBER',
+  location = 'YOUR_PROJECT_LOCATION',
+  userId = 'user:xxx@example.com',
+  documentQueryText = 'YOUR_DOCUMENT_QUERY'
+) {
+  // [START contentwarehouse_quickstart]
+  /**
+   * TODO(developer): Uncomment these variables before running the sample.
+   * const projectNumber = 'YOUR_PROJECT_NUMBER';
+   * const location = 'YOUR_PROJECT_LOCATION'; // Format is 'us' or 'eu'
+   * const userId = 'user:xxx@example.com'; // Format is "user:xxx@example.com"
+   * const documentQueryText = 'YOUR_DOCUMENT_QUERY'
+   */
+
+  // Import from google cloud
+  const {DocumentServiceClient} = require('@google-cloud/contentwarehouse').v1;
+
+  // Create service client
+  const serviceClient = new DocumentServiceClient();
+
+  // Get Document Schema
+  async function quickstart() {
+    // Initialize request argument(s)
+    const searchRequest = {};
+
+    // The full resource name of the location, e.g.:
+    // projects/{project_number}/locations/{location}
+    searchRequest.parent = `projects/${projectNumber}/locations/${location}`;
+
+    // File Type Filter
+    const fileTypeFilter = {};
+    fileTypeFilter.fileType = 'DOCUMENT';
+
+    // Document Text Query
+    const documentQuery = {};
+    documentQuery.query = documentQueryText;
+    documentQuery.fileTypeFilter = fileTypeFilter;
+    searchRequest.documentQuery = documentQuery;
+
+    // Histogram Query
+    const histogramQuery = {};
+    histogramQuery.histogramQuery = 'count("DocumentSchemaId")';
+    searchRequest.histogramQueries = [histogramQuery];
+
+    // Metadata Definition
+    searchRequest.requestMetadata = {userInfo: {id: userId}};
+
+    // Make Request
+    const response = serviceClient.searchDocuments(searchRequest);
+
+    // Print out response
+    response.then(
+      result => console.log(`Document Found: ${JSON.stringify(result)}`),
+      error => console.log(`${error}`)
+    );
+  }
+
+  // [END contentwarehouse_quickstart]
+  await quickstart();
+}
+
+main(...process.argv.slice(2)).catch(err => {
+  console.error(err);
+  process.exitCode = 1;
+});

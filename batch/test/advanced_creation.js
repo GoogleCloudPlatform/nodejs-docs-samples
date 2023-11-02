286bae399ea76de41aaea3ec597578200fe102cb
Maciej Strzelczyk <strzelczyk@google.com>
2023-11-02 10:44:17 +0000

    Fixing problem with service account ID

--- batch/test/advanced_creation.js
+++ batch/test/advanced_creation.js
@@ -193,7 +193,7 @@ async function projectIdToNumber(projectId) {
  * @param {string} templateName - Name of the new template to create.
  */
 async function createTemplate(projectId, templateName) {
-  const projectNumber = projectIdToNumber(projectId);
+  const [projectNumber] = await projectIdToNumber(projectId);
   const serviceAccountAddress = `${projectNumber}-compute@developer.gserviceaccount.com`;
 
   const [response] = await instanceTemplatesClient.insert({

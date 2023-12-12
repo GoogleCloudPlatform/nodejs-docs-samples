3fbdf511810f4d5fc9415bc14731c9bdd46306c9
Holt Skinner <holtskinner@google.com>
2023-12-12 13:29:16 -0600

    Fix formatting error

--- document-warehouse/quickstart.js
+++ document-warehouse/quickstart.js
@@ -65,9 +65,8 @@ async function main(
     };
 
     // Create Document Schema
-    const documentSchema = await schemaClient.createDocumentSchema(
-      schemaRequest
-    );
+    const documentSchema =
+      await schemaClient.createDocumentSchema(schemaRequest);
 
     const documentRequest = {
       parent: parent,

53737aef3d703ca78060158a4d7fe8662b943c23
Cloud Shell <>
2024-02-15 17:25:06 +0000

    chore: lint functionCallingStreamContent

--- generative-ai/snippets/functionCallingStreamContent.js
+++ generative-ai/snippets/functionCallingStreamContent.js
@@ -13,7 +13,10 @@
 // limitations under the License.
 
 // [START aiplatform_gemini_function_calling_content]
-const { VertexAI, FunctionDeclarationSchemaType } = require('@google-cloud/vertexai');
+const {
+  VertexAI,
+  FunctionDeclarationSchemaType,
+} = require('@google-cloud/vertexai');
 
 const functionDeclarations = [
   {
@@ -24,7 +27,7 @@ const functionDeclarations = [
         parameters: {
           type: FunctionDeclarationSchemaType.OBJECT,
           properties: {
-            location: { type: FunctionDeclarationSchemaType.STRING },
+            location: {type: FunctionDeclarationSchemaType.STRING},
             unit: {
               type: FunctionDeclarationSchemaType.STRING,
               enum: ['celsius', 'fahrenheit'],
@@ -41,8 +44,7 @@ const functionResponseParts = [
   {
     functionResponse: {
       name: 'get_current_weather',
-      response:
-        { name: 'get_current_weather', content: { weather: 'super nice' } },
+      response: {name: 'get_current_weather', content: { weather: 'super nice' }},
     },
   },
 ];
@@ -56,7 +58,7 @@ async function functionCallingStreamChat(
   model = 'gemini-pro'
 ) {
   // Initialize Vertex with your Cloud project and location
-  const vertexAI = new VertexAI({ project: projectId, location: location });
+  const vertexAI = new VertexAI({project: projectId, location: location});
 
   // Instantiate the model
   const generativeModel = vertexAI.preview.getGenerativeModel({
@@ -65,9 +67,9 @@ async function functionCallingStreamChat(
 
   const request = {
     contents: [
-      { role: 'user', parts: [{ text: 'What is the weather in Boston?' }] },
-      { role: 'model', parts: [{ functionCall: { name: 'get_current_weather', args: { 'location': 'Boston' } } }] },
-      { role: 'function', parts: functionResponseParts }
+      {role: 'user', parts: [{ text: 'What is the weather in Boston?' }]},
+      {role: 'model', parts: [{ functionCall: { name: 'get_current_weather', args: { 'location': 'Boston' } } }]},
+      {role: 'function', parts: functionResponseParts },
     ],
     tools: functionDeclarations,
   };

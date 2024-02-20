6b8365505e7d27ccfbcf41d29e8c0b95197e03af
Luke Schlangen <lukeschlangen@gmail.com>
2024-02-20 16:55:43 -0600

    chore: add react import

--- run/nextjs-helloworld/app/layout.jsx
+++ run/nextjs-helloworld/app/layout.jsx
@@ -12,6 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+import React from 'react'
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
--- run/nextjs-helloworld/app/page.jsx
+++ run/nextjs-helloworld/app/page.jsx
@@ -12,6 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+import React from 'react'
+
 export default function Page() {
   return <h1>Hello, Next.js!</h1>;
 }

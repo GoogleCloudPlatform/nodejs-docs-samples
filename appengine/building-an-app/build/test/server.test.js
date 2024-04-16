44e70b3a50d5a629b65be0276e99f725e9abbbda
Katie McLaughlin <katie@glasnt.com>
2024-04-17 07:26:05 +1000

    fix: add new region tags to tests
    should resolve region-tag-tests issues

--- appengine/building-an-app/build/test/server.test.js
+++ appengine/building-an-app/build/test/server.test.js
@@ -3,6 +3,8 @@ const path = require('path');
 const app = require(path.join(__dirname, '../', 'server.js'));
 
 
-it('should be listening', async () => {
-  await supertest(app).get('/').expect(200);
-});
+describe('gae_app', () => {
+  it('should be listening', async () => {
+    await supertest(app).get('/').expect(200);
+  });
+})

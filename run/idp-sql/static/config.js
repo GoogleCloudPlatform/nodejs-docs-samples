74f476ef840613cce960696eb65d4ee0c5add925
Katie McLaughlin <katie@glasnt.com>
2024-05-08 12:26:01 +1000

    fix: remove redundant run_ tags (fixit)

--- run/idp-sql/static/config.js
+++ run/idp-sql/static/config.js
@@ -1,8 +1,6 @@
 // [START cloudrun_end_user_firebase_config]
-// [START run_end_user_firebase_config]
 const config = {
   apiKey: 'API_KEY',
   authDomain: 'PROJECT_ID.firebaseapp.com',
 };
-// [END run_end_user_firebase_config]
 // [END cloudrun_end_user_firebase_config]

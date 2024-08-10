ff8bcdbb59f19d320cf987bf4d921fefbbd6583b
Tony Pujals <tonypujals@google.com>
2024-08-09 21:56:51 -0700

    test: only test basics.test.js (all but 'lists jobs')

--- batch/test/basics.test.js
+++ batch/test/basics.test.js
@@ -59,14 +59,14 @@ describe('Creates, lists and deletes jobs', () => {
     assert(output !== null);
   });
 
-  it('lists jobs', async () => {
+  it.skip('lists jobs', async () => {
     const output = execSync(`node list/list_jobs.js ${projectId} us-central1`, {
       cwd,
     });
     assert(output !== null);
   });
 
-  it.skip('gets a task', async () => {
+  it('gets a task', async () => {
     // the server needs a bit of time to create the objects. 10 seconds is way more than enough.
     await new Promise(resolve => setTimeout(resolve, 10000));
     const output = execSync(
@@ -76,7 +76,7 @@ describe('Creates, lists and deletes jobs', () => {
     assert(output !== null);
   });
 
-  it.skip('lists tasks', async () => {
+  it('lists tasks', async () => {
     const output = execSync(
       `node list/list_tasks.js ${projectId} us-central1 test-job-js-script-${testRunId} group0`,
       {cwd}
@@ -84,7 +84,7 @@ describe('Creates, lists and deletes jobs', () => {
     assert(output !== null);
   });
 
-  it.skip('deletes the test job', async () => {
+  it('deletes the test job', async () => {
     const output = execSync(
       `node delete/delete_job.js ${projectId} us-central1 test-job-js-script-${testRunId}`,
       {cwd}

71cbf4f6302f8bedcd8ff20d5c88ac8826cbd0c5
Joanna Grycz <jgrycz@softserveinc.com>
2024-08-21 15:12:58 +0200

    feat: compute_hyperdisk_create_from_pool

---
+++ compute/disks/createComputeHyperdiskFromPool.js
@@ -0,0 +1,105 @@
+/*
+ * Copyright 2024 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+'use strict';
+
+async function main() {
+  // [START compute_hyperdisk_create_from_pool]
+  // Import the Compute library
+  const computeLib = require('@google-cloud/compute');
+  const compute = computeLib.protos.google.cloud.compute.v1;
+
+  // Instantiate a diskClient
+  const disksClient = new computeLib.DisksClient();
+  // Instantiate a zoneOperationsClient
+  const zoneOperationsClient = new computeLib.ZoneOperationsClient();
+
+  /**
+   * TODO(developer): Update these variables before running the sample.
+   */
+  // Project ID or project number of the Google Cloud project you want to use.
+  const projectId = await disksClient.getProjectId();
+  // The zone where your VM and new disk are located.
+  const zone = 'europe-central2-b';
+  // The name of the new disk
+  const diskName = 'disk-name';
+  // The name of the storage pool
+  const storagePoolName = 'storage-pool-name';
+  // Link to the storagePool you want to use. Use format:
+  // https://www.googleapis.com/compute/v1/projects/{projectId}/zones/{zone}/storagePools/{storagePoolName}
+  const storagePool = `https://www.googleapis.com/compute/v1/projects/${projectId}/zones/${zone}/storagePools/${storagePoolName}`;
+  // The type of disk. This value uses the following format:
+  // "zones/{zone}/diskTypes/(hyperdisk-balanced|hyperdisk-extreme|hyperdisk-ml|hyperdisk-throughput)".
+  // For example: "zones/us-west3-b/diskTypes/hyperdisk-balanced"
+  const diskType = `zones/${zone}/diskTypes/hyperdisk-balanced`;
+  // Size of the new disk in gigabytes.
+  const diskSizeGb = 10;
+  // Optional: For Hyperdisk Balanced or Hyperdisk Extreme disks,
+  // this is the number of I/O operations per second (IOPS) that the disk can handle.
+  const provisionedIops = 3000;
+  // Optional: For Hyperdisk Balanced or Hyperdisk Throughput volumes,
+  // this is an integer that represents the throughput,
+  // measured in MiB per second, that the disk can handle.
+  const provisionedThroughput = 140;
+
+  async function callCreateComputeHyperdiskFromPool() {
+    // Create a disk
+    const disk = new compute.Disk({
+      sizeGb: diskSizeGb,
+      name: diskName,
+      type: diskType,
+      zone,
+      storagePool,
+      provisionedIops,
+      provisionedThroughput,
+    });
+
+    const [response] = await disksClient.insert({
+      project: projectId,
+      zone,
+      diskResource: disk,
+    });
+
+    let operation = response.latestResponse;
+
+    // Wait for the create disk operation to complete.
+    while (operation.status !== 'DONE') {
+      [operation] = await zoneOperationsClient.wait({
+        operation: operation.name,
+        project: projectId,
+        zone: operation.zone.split('/').pop(),
+      });
+    }
+
+    const hyperdisk = (
+      await disksClient.get({
+        project: projectId,
+        zone,
+        disk: diskName,
+      })
+    )[0];
+
+    console.log(JSON.stringify(hyperdisk));
+  }
+
+  await callCreateComputeHyperdiskFromPool();
+  // [END compute_hyperdisk_create_from_pool]
+}
+
+main().catch(err => {
+  console.error(err);
+  process.exitCode = 1;
+});
---
+++ compute/test/createComputeHyperdiskFromPool.test.js
@@ -0,0 +1,71 @@
+/*
+ * Copyright 2024 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+'use strict';
+
+const path = require('path');
+const {assert} = require('chai');
+const {describe, it} = require('mocha');
+const cp = require('child_process');
+const {StoragePoolsClient} = require('@google-cloud/compute');
+const {DisksClient} = require('@google-cloud/compute').v1;
+const {createStoragePool, deleteStoragePool, deleteDisk} = require('./util');
+
+const execSync = cmd => cp.execSync(cmd, {encoding: 'utf-8'});
+const cwd = path.join(__dirname, '..');
+
+describe('Create compute hyperdisk from pool', async () => {
+  const diskName = 'disk-name';
+  const zone = 'europe-central2-b';
+  const storagePoolName = 'storage-pool-name';
+  const disksClient = new DisksClient();
+  const storagePoolsClient = new StoragePoolsClient();
+  let projectId;
+
+  before(async () => {
+    projectId = await disksClient.getProjectId();
+    await createStoragePool(
+      storagePoolsClient,
+      projectId,
+      zone,
+      storagePoolName
+    );
+  });
+
+  after(async () => {
+    await deleteDisk(disksClient, projectId, zone, diskName);
+    await deleteStoragePool(
+      storagePoolsClient,
+      projectId,
+      zone,
+      storagePoolName
+    );
+  });
+
+  it('should create a new hyperdisk from pool', () => {
+    const response = JSON.parse(
+      execSync('node ./disks/createComputeHyperdiskFromPool.js', {
+        cwd,
+      })
+    );
+
+    assert.equal(response.name, diskName);
+    assert.equal(
+      response.storagePool,
+      `https://www.googleapis.com/compute/v1/projects/${projectId}/zones/${zone}/storagePools/${storagePoolName}`
+    );
+  });
+});

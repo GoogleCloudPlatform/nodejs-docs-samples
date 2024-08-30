2d1231310dc3c213b0cb516d027a87386bd891e4
Joanna Grycz <jgrycz@softserveinc.com>
2024-08-30 19:05:54 +0200

    feat: compute_reservation_list

---
+++ compute/test/getReservations.test.js
@@ -0,0 +1,73 @@
+/*
+
+ * Copyright 2024 Google LLC
+
+ *
+
+ * Licensed under the Apache License, Version 2.0 (the "License");
+
+ * you may not use this file except in compliance with the License.
+
+ * You may obtain a copy of the License at
+
+ *
+
+ *     https://www.apache.org/licenses/LICENSE-2.0
+
+ *
+
+ * Unless required by applicable law or agreed to in writing, software
+
+ * distributed under the License is distributed on an "AS IS" BASIS,
+
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+
+ * See the License for the specific language governing permissions and
+
+ * limitations under the License.
+
+ */
+
+'use strict';
+
+const path = require('path');
+
+const assert = require('node:assert/strict');
+
+const {after, before, describe, it} = require('mocha');
+
+const cp = require('child_process');
+
+const execSync = cmd => cp.execSync(cmd, {encoding: 'utf-8'});
+
+const cwd = path.join(__dirname, '..');
+
+describe('Get reservations', async () => {
+  let insertedReservation;
+
+  before(async () => {
+    // Add reservation
+    insertedReservation = JSON.parse(
+      execSync('node ./reservations/createReservationFromProperties.js', {
+        cwd,
+      })
+    );
+  });
+
+  after(async () => {
+    // Delete reservation
+    execSync('node ./reservations/deleteReservation.js', {
+      cwd,
+    });
+  });
+
+  it('should return list of reservations', () => {
+    const response = JSON.parse(
+      execSync('node ./reservations/getReservations.js', {
+        cwd,
+      })
+    );
+
+    assert.deepEqual(response, [insertedReservation]);
+  });
+});

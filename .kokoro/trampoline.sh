ffe3c53bbf7e09a7b374748393072e30db57c9e7
Tony Pujals <tonypujals@google.com>
2024-09-16 20:18:10 -0700

    wip

--- .kokoro/trampoline.sh
+++ .kokoro/trampoline.sh
@@ -12,4 +12,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+echo "EXPERIMENT (SKIP)"
+exit
 python3 "${KOKORO_GFILE_DIR}/trampoline_v1.py"

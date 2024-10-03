8cb15e0630960bb365c6ac89f5ef5f8e85d63415
David Cavazos <dcavazos@google.com>
2024-10-02 19:18:19 -0700

    disable parallelism

--- .github/workflows/samples-tools/pkg/utils/map.go
+++ .github/workflows/samples-tools/pkg/utils/map.go
@@ -1,19 +1,18 @@
 package utils
 
-import (
-	"sync"
-)
-
 func Map[a, b any](items []a, fn func(a) b) []b {
-	var wg sync.WaitGroup
+	// var wg sync.WaitGroup
 	result := make([]b, len(items))
+	// for i, item := range items {
+	// 	wg.Add(1)
+	// 	go func(i int, item a) {
+	// 		defer wg.Done()
+	// 		result[i] = fn(item)
+	// 	}(i, item)
+	// }
+	// wg.Wait()
 	for i, item := range items {
-		wg.Add(1)
-		go func(i int, item a) {
-			defer wg.Done()
-			result[i] = fn(item)
-		}(i, item)
+		result[i] = fn(item)
 	}
-	wg.Wait()
 	return result
 }

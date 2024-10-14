18cbdf3267079332663271569c4a970df016eb22
Joanna Grycz <jgrycz@softserveinc.com>
2024-10-14 20:38:09 +0200

    feat: add tpu_vm_create_topology

---
+++ tpu/test/vmTopology.test.js
@@ -0,0 +1,54 @@
+/*
+ * Copyright 2024 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+'use strict';
+
+const path = require('path');
+const assert = require('node:assert/strict');
+const {before, after, describe, it} = require('mocha');
+const cp = require('child_process');
+const {getStaleNodes, deleteNode} = require('./util');
+
+const execSync = cmd => cp.execSync(cmd, {encoding: 'utf-8'});
+const cwd = path.join(__dirname, '..');
+
+describe('Compute tpu with topology', async () => {
+  const nodePrefix = 'topology-node-name-2a2b3c';
+  const nodeName = `${nodePrefix}${Math.floor(Math.random() * 1000 + 1)}`;
+  const zone = 'europe-west4-a';
+  const tpuSoftwareVersion = 'tpu-vm-tf-2.17.0-pod-pjrt';
+
+  before(async () => {
+    // Cleanup resources
+    const nodes = await getStaleNodes(nodePrefix, zone);
+    await Promise.all(nodes.map(node => deleteNode(zone, node.nodeName)));
+  });
+
+  after(async () => {
+    // Delete node
+    await deleteNode(zone, nodeName);
+  });
+
+  it('should create a new tpu', () => {
+    const response = execSync(
+      `node ./vmCreateTopology.js ${nodeName} ${zone} ${tpuSoftwareVersion}`,
+      {
+        cwd,
+      }
+    );
+    assert(response.includes(`TPU VM: ${nodeName} created.`));
+  });
+});

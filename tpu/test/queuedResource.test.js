1b48263d23789ff1cf5dab5fac1f28ff1d1e0ade
Joanna Grycz <jgrycz@softserveinc.com>
2024-10-21 16:40:17 +0200

    Fix samples and tests

--- tpu/test/queuedResource.test.js
+++ tpu/test/queuedResource.test.js
@@ -20,26 +20,54 @@ const path = require('path');
 const assert = require('node:assert/strict');
 const {describe, it} = require('mocha');
 const cp = require('child_process');
+const {TpuClient} = require('@google-cloud/tpu').v2alpha1;
 
 const execSync = cmd => cp.execSync(cmd, {encoding: 'utf-8'});
 const cwd = path.join(__dirname, '..');
 
-describe('TPU queued resource', async () => {
+async function waitForTPUCreation(nodeName, zone) {
+  const tpuClient = new TpuClient();
+  const projectId = await tpuClient.getProjectId();
+
+  // Give a time to start process of creating TPU Node
+  await new Promise(resolve => setTimeout(resolve, 60000));
+
+  const getNodeRequest = {
+    name: `projects/${projectId}/locations/${zone}/nodes/${nodeName}`,
+  };
+
+  console.log(`Waiting for TPU node ${nodeName} to become ready...`);
+
+  let state;
+
+  while (state !== 'READY') {
+    try {
+      state = (await tpuClient.getNode(getNodeRequest))[0].state;
+      // Wait another minute to try
+      await new Promise(resolve => setTimeout(resolve, 60000));
+    } catch (err) {
+      console.log('TPU node not ready');
+      // Wait another minute to try
+      await new Promise(resolve => setTimeout(resolve, 60000));
+    }
+  }
+}
+
+describe('TPU queued resource', () => {
   const queuedResourceName = `queued-resource-name-1a2sdf${Math.floor(Math.random() * 1000 + 1)}`;
   const nodePrefix = 'node-name-2a2b3c';
   const nodeName = `${nodePrefix}${Math.floor(Math.random() * 1000 + 1)}`;
-  const zone = 'europe-west4-a';
+  const zone = 'us-central1-f';
   const tpuType = 'v2-8';
   const tpuSoftwareVersion = 'tpu-vm-tf-2.14.1';
 
-  it('should create a new queued resource', () => {
+  it('should create a new queued resource', async () => {
     const response = execSync(
       `node ./queuedResource/createQueuedResource.js ${nodeName} ${queuedResourceName} ${zone} ${tpuType} ${tpuSoftwareVersion}`,
       {
         cwd,
       }
     );
-
     assert(response.includes(`Queued resource ${queuedResourceName} created.`));
   });
 
@@ -50,20 +78,20 @@ describe('TPU queued resource', async () => {
         cwd,
       }
     );
-
     assert(
       response.includes(`Queued resource ${queuedResourceName} retrived.`)
     );
   });
 
-  it('should delete queued resource', () => {
+  it('should delete queued resource', async () => {
+    // Wait until queued resource is ready to delete.
+    await waitForTPUCreation(nodeName, zone);
     const response = execSync(
       `node ./queuedResource/deleteQueuedResource.js ${queuedResourceName} ${zone}`,
       {
         cwd,
       }
     );
-
     assert(response.includes(`Queued resource ${queuedResourceName} deleted.`));
   });
 });

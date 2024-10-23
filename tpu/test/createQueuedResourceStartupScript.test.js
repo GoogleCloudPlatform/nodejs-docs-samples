3ef4622b10856f4e7e6f03d54218364c98ebcfa9
Joanna Grycz <jgrycz@softserveinc.com>
2024-10-23 13:57:24 +0200

    feat: tpu_queued_resources_startup_script

---
+++ tpu/test/createQueuedResourceStartupScript.test.js
@@ -0,0 +1,58 @@
+/*
+ * Copyright 2024 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+'use strict';
+
+const path = require('path');
+const assert = require('node:assert/strict');
+const {after, describe, it} = require('mocha');
+const cp = require('child_process');
+
+const execSync = cmd => cp.execSync(cmd, {encoding: 'utf-8'});
+const cwd = path.join(__dirname, '..');
+
+describe('TPU queued resource with start-up script', async () => {
+  const queuedResourceName = `queued-resource-startup-script-${Math.floor(Math.random() * 1000 + 1)}`;
+  const nodeName = `node-startup-script-2a2b3c${Math.floor(Math.random() * 1000 + 1)}`;
+  const zone = 'us-east1-d';
+  const tpuType = 'v3-32';
+  const tpuSoftwareVersion = 'tpu-vm-tf-2.14.1';
+
+  after(() => {
+    // Delete queued resource
+    execSync(
+      `node ./queuedResources/forceDeleteQueuedResource.js ${queuedResourceName} ${zone}`,
+      {
+        cwd,
+      }
+    );
+  });
+
+  it('should create queued resource with start-up script', () => {
+    const response = execSync(
+      `node ./queuedResources/createQueuedResourceStartupScript.js ${nodeName} ${queuedResourceName} ${zone} ${tpuType} ${tpuSoftwareVersion}`,
+      {
+        cwd,
+      }
+    );
+
+    assert(
+      response.includes(
+        `Queued resource ${queuedResourceName} with start-up script created.`
+      )
+    );
+  });
+});

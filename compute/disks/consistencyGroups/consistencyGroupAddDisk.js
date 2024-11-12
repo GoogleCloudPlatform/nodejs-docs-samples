9f2c418f64efb332186f981dbe4c953c51af864d
Joanna Grycz <jgrycz@softserveinc.com>
2024-11-12 09:38:00 +0100

    feat: compute_consistency_group_add_disk

---
+++ compute/disks/consistencyGroups/consistencyGroupAddDisk.js
@@ -0,0 +1,90 @@
+/*
+ * Copyright 2024 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+'use strict';
+
+async function main(
+  consistencyGroupName,
+  consistencyGroupLocation,
+  diskName,
+  diskLocation
+) {
+  // [START compute_consistency_group_add_disk]
+  // Import the Compute library
+  const computeLib = require('@google-cloud/compute');
+  const compute = computeLib.protos.google.cloud.compute.v1;
+
+  // Instantiate a disksClient
+  const disksClient = new computeLib.DisksClient();
+  // Instantiate a zone
+  const zoneOperationsClient = new computeLib.ZoneOperationsClient();
+
+  /**
+   * TODO(developer): Update/uncomment these variables before running the sample.
+   */
+  // The project that contains the disk.
+  // const projectId = await disksClient.getProjectId();
+  const projectId = 'jgrycz-softserve-project';
+
+  // The name of the disk.
+  // diskName = 'disk-name';
+
+  // The zone of the disk.
+  // diskLocation = 'europe-central2-a';
+
+  // The name of the consistency group.
+  // consistencyGroupName = 'consistency-group-name';
+
+  // The region of the consistency group.
+  // consistencyGroupLocation = 'europe-central2';
+
+  async function callAddDiskToConsistencyGroup() {
+    const [response] = await disksClient.addResourcePolicies({
+      disk: diskName,
+      project: projectId,
+      zone: diskLocation,
+      disksAddResourcePoliciesRequestResource:
+        new compute.DisksAddResourcePoliciesRequest({
+          resourcePolicies: [
+            `https://www.googleapis.com/compute/v1/projects/${projectId}/regions/${consistencyGroupLocation}/resourcePolicies/${consistencyGroupName}`,
+          ],
+        }),
+    });
+
+    let operation = response.latestResponse;
+
+    // Wait for the add disk operation to complete.
+    while (operation.status !== 'DONE') {
+      [operation] = await zoneOperationsClient.wait({
+        operation: operation.name,
+        project: projectId,
+        zone: operation.zone.split('/').pop(),
+      });
+    }
+
+    console.log(
+      `Disk: ${diskName} added to consistency group: ${consistencyGroupName}.`
+    );
+  }
+
+  await callAddDiskToConsistencyGroup();
+  // [END compute_consistency_group_add_disk]
+}
+
+main(...process.argv.slice(2)).catch(err => {
+  console.error(err);
+  process.exitCode = 1;
+});

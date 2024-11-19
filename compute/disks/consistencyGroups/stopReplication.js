ffa36df45263a364a560daa7e6573307e38a8421
Joanna Grycz <jgrycz@softserveinc.com>
2024-11-19 16:07:11 +0100

    feat: compute_consistency_group_stop_replication

--- compute/disks/consistencyGroups/stopReplication.js
+++ compute/disks/consistencyGroups/stopReplication.js
@@ -17,52 +17,50 @@
 'use strict';
 
 async function main(disksClient, zoneOperationsClient) {
-  // [START compute_consistency_group_clone]
+  // [START compute_consistency_group_stop_replication]
   // Import the Compute library
   const computeLib = require('@google-cloud/compute');
   const compute = computeLib.protos.google.cloud.compute.v1;
 
-  // If you want to clone regional disks,
-  // you should use: RegionDisksClient and RegionOperationsClient.
+  // If disks are regional- use RegionDisksClient and RegionOperationsClient.
   // TODO(developer): Uncomment disksClient and zoneOperationsClient before running the sample.
   // Instantiate a disksClient
   // disksClient = new computeLib.DisksClient();
-  // Instantiate a zone
+  // Instantiate a zoneOperationsClient
   // zoneOperationsClient = new computeLib.ZoneOperationsClient();
 
   /**
    * TODO(developer): Update/uncomment these variables before running the sample.
    */
-  // The project that contains the disks.
+  // The project that contains the consistency group.
   const projectId = await disksClient.getProjectId();
 
   // If you use RegionDisksClient- define region, if DisksClient- define zone.
-  // The zone or region that the disks in the consistency group are located in. The clones are created in this location.
-  // diskLocation = 'europe-central2-a';
-  const disksLocation = 'europe-north1-a';
+  // The zone or region of the disks.
+  const disksLocation = 'europe-central2-a';
 
-  // The name of the consistency group, that contains secondary disks to clone.
-  // consistencyGroupName = 'consistency-group-name';
+  // The name of the consistency group.
   const consistencyGroupName = 'consistency-group-1';
 
   // The region of the consistency group.
-  const consistencyGroupLocation = 'europe-north1';
+  const consistencyGroupLocation = 'europe-central2';
 
-  async function callConsistencyGroupClone() {
-    const [response] = await disksClient.bulkInsert({
+  async function callStopReplication() {
+    const [response] = await disksClient.stopGroupAsyncReplication({
       project: projectId,
       // If you use RegionDisksClient, pass region as an argument instead of zone.
       zone: disksLocation,
-      bulkInsertDiskResourceResource: new compute.BulkInsertDiskResource({
-        sourceConsistencyGroupPolicy: [
-          `https://www.googleapis.com/compute/v1/projects/${projectId}/regions/${consistencyGroupLocation}/resourcePolicies/${consistencyGroupName}`,
-        ],
-      }),
+      disksStopGroupAsyncReplicationResourceResource:
+        new compute.DisksStopGroupAsyncReplicationResource({
+          resourcePolicy: [
+            `https://www.googleapis.com/compute/v1/projects/${projectId}/regions/${consistencyGroupLocation}/resourcePolicies/${consistencyGroupName}`,
+          ],
+        }),
     });
 
     let operation = response.latestResponse;
 
-    // Wait for the clone operation to complete.
+    // Wait for the operation to complete.
     while (operation.status !== 'DONE') {
       [operation] = await zoneOperationsClient.wait({
         operation: operation.name,
@@ -72,13 +70,13 @@ async function main(disksClient, zoneOperationsClient) {
       });
     }
 
-    const message = `Disks cloned from consistency group: ${consistencyGroupName}.`;
+    const message = `Replication stopped for consistency group: ${consistencyGroupName}.`;
     console.log(message);
     return message;
   }
 
-  return await callConsistencyGroupClone();
-  // [END compute_consistency_group_clone]
+  return await callStopReplication();
+  // [END compute_consistency_group_stop_replication]
 }
 
 module.exports = main;

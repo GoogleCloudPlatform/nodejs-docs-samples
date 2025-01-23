65ba40c394d3a36e068fbb7c595104d2a50cb991
Ivan Hernandez <hivanalejandro@google.com>
2025-01-23 17:59:57 +0000

    added the gae_build_web_server_app test

--- appengine/building-an-app/build/test/server.test.js
+++ appengine/building-an-app/build/test/server.test.js
@@ -21,3 +21,9 @@ describe('gae_app', () => {
     await supertest(app).get('/').expect(200);
   });
 })
+
+describe('gae_build_web_server_app', () => {
+  it('should be listening', async () => {
+    await supertest(app).get('/').expect(200);
+  });
+})

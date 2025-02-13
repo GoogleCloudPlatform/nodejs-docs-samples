baddd1b00bbd37f8a0ea29e8d044cb7353d8213e
David Cavazos <dcavazos@google.com>
2025-02-13 14:55:00 -0800

    have local testing package

--- .github/scripts/setup-vars.js
+++ .github/scripts/setup-vars.js
@@ -1,9 +1,28 @@
-module.exports = ({core, process}) => {
-  const setup = JSON.parse(process.env.CI_SETUP);
+/*
+ Copyright 2025 Google LLC
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+      https://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ */
+
+import fs from 'node:fs';
+
+const PROJECT_ID = 'long-door-651';
+
+export default function setupVars({core, setup}) {
   const env = {
-    GOOGLE_SAMPLES_PROJECT: setup['project-id'],
-    PROJECT_ID: setup['project-id'],
-    ...setup.env,
+    GOOGLE_SAMPLES_PROJECT: PROJECT_ID,
+    PROJECT_ID: PROJECT_ID,
+    ...(setup.env || {}),
   };
   for (const key in env) {
     console.log(`${key}: ${env[key]}`);
@@ -11,8 +30,20 @@ module.exports = ({core, process}) => {
   }
   return {
     env: env,
-    secrets: Object.keys(setup.secrets)
+    secrets: Object.keys(setup.secrets || {})
       .map(key => `${key}:${setup.secrets[key]}`)
       .join('\n'),
   };
+}
+
+const core = {
+  exportVariable: (key, value) => null,
 };
+const setupFile = process.argv[2];
+if (!setupFile) {
+  console.error('Please provide the path to a setup file.');
+  exit(1);
+}
+const data = fs.readFileSync(setupFile, 'utf8');
+const setup = JSON.parse(data);
+setupVars({core, setup});

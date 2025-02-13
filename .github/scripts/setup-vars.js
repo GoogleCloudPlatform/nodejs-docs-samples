f8a48c941c7224272d15b53b348d8b01c5b3b322
David Cavazos <dcavazos@google.com>
2025-02-13 13:53:54 -0800

    use script file

---
+++ .github/scripts/setup-vars.js
@@ -0,0 +1,17 @@
+module.exports = ({core, process}) => {
+  const setup = JSON.parse(process.env.CI_SETUP);
+  const env = {
+    GOOGLE_SAMPLES_PROJECT: setup['project-id'],
+    ...setup.env,
+  };
+  for (const key in env) {
+    console.log(`${key}: ${env[key]}`);
+    core.exportVariable(key, env[key]);
+  }
+  return {
+    env: env,
+    secrets: Object.keys(setup.secrets)
+      .map(key => `${key}:${setup.secrets[key]}`)
+      .join('\n'),
+  };
+};

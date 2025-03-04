bb6bc1f5a5be5cf4d6801f605d2ca2b5a0d8a9c6
Vatsal Vora <vatsal.vora@crestdata.ai>
2025-03-04 15:52:53 +0530

    feat(parametermanager): Added samples for create list and render params in global

---
+++ parameter-manager/getParam.js
@@ -0,0 +1,60 @@
+// Copyright 2025 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+'use strict';
+
+/**
+ * Retrieves a parameter from the global location of the specified
+ * project using the Google Cloud Parameter Manager SDK.
+ *
+ * @param {string} projectId - The Google Cloud project ID where the parameter is located.
+ * @param {string} parameterId - The ID of the parameter to retrieve.
+ */
+async function main(projectId = 'my-project', parameterId = 'my-parameter') {
+  // [START parametermanager_get_param]
+  /**
+   * TODO(developer): Uncomment these variables before running the sample.
+   */
+  // const projectId = 'my-project';
+  // const parameterId = 'my-parameter';
+
+  // Imports the Parameter Manager library
+  const {ParameterManagerClient} = require('@google-cloud/parametermanager');
+
+  // Instantiates a client
+  const client = new ParameterManagerClient();
+
+  async function getParam() {
+    // Construct the fully qualified parameter name
+    const name = client.parameterPath(projectId, 'global', parameterId);
+
+    // Get the parameter
+    const [parameter] = await client.getParameter({
+      name: name,
+    });
+
+    // Find more details for the Parameter object here:
+    // https://cloud.google.com/secret-manager/parameter-manager/docs/reference/rest/v1/projects.locations.parameters#Parameter
+    console.log(
+      `Found parameter ${parameter.name} with format ${parameter.format}`
+    );
+  }
+
+  await getParam();
+  // [END parametermanager_get_param]
+}
+
+// The command-line arguments are passed as an array to main()
+const args = process.argv.slice(2);
+main(...args).catch(console.error);

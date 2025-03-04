43535694273e21d3a7903e3bbfca691a50e114ce
Katie McLaughlin <katie@glasnt.com>
2025-03-05 08:18:06 +1100

    ci(scheduler): migrate to new CI (#4006)
    * ci(scheduler): migrate to new CI
    
    * 'npm run build' output
    
    * restore generated note
    
    * mark as module
    
    * exports
    
    * update config, regenerate
    
    * debug: extend timeout

--- scheduler/createJob.js
+++ scheduler/createJob.js
@@ -14,60 +14,50 @@
 
 // This is a generated sample, using the typeless sample bot. Please
 // look for the source TypeScript sample (.ts) for modifications.
-'use strict';
 
 // sample-metadata:
 //   title: Create Job
 //   description: Create a job that posts to /log_payload on an App Engine service.
 //   usage: node createJob.js [project-id] [location-id] [app-engine-service-id]
-
 const args = process.argv.slice(2);
 const [projectId, locationId, serviceId] = args;
-
 // [START cloudscheduler_create_job]
-const {CloudSchedulerClient} = require('@google-cloud/scheduler');
-
+import { CloudSchedulerClient } from '@google-cloud/scheduler';
 // TODO(developer): Uncomment and set the following variables
 // const projectId = "PROJECT_ID"
 // const locationId = "LOCATION_ID"
 // const serviceId = "my-serivce"
-
 // Create a client.
 const client = new CloudSchedulerClient();
-
 /**
  * Create a job with an App Engine target via the Cloud Scheduler API
  */
 async function createJob(projectId, locationId, serviceId) {
-  // Construct the fully qualified location path.
-  const parent = client.locationPath(projectId, locationId);
-
-  // Construct the request body.
-  const job = {
-    appEngineHttpTarget: {
-      appEngineRouting: {
-        service: serviceId,
-      },
-      relativeUri: '/log_payload',
-      httpMethod: 'POST',
-      body: Buffer.from('Hello World'),
-    },
-    schedule: '* * * * *',
-    timeZone: 'America/Los_Angeles',
-  };
-
-  const request = {
-    parent: parent,
-    job: job,
-  };
-
-  // Use the client to send the job creation request.
-  const [response] = await client.createJob(request);
-  console.log(`Created job: ${response.name}`);
+    // Construct the fully qualified location path.
+    const parent = client.locationPath(projectId, locationId);
+    // Construct the request body.
+    const job = {
+        appEngineHttpTarget: {
+            appEngineRouting: {
+                service: serviceId,
+            },
+            relativeUri: '/log_payload',
+            httpMethod: 'POST',
+            body: Buffer.from('Hello World'),
+        },
+        schedule: '* * * * *',
+        timeZone: 'America/Los_Angeles',
+    };
+    const request = {
+        parent: parent,
+        job: job,
+    };
+    // Use the client to send the job creation request.
+    const [response] = await client.createJob(request);
+    console.log(`Created job: ${response.name}`);
 }
-
 createJob(projectId, locationId, serviceId).catch(err => {
-  console.error(err.message);
-  process.exitCode = 1;
+    console.error(err.message);
+    process.exitCode = 1;
 });
 // [END cloudscheduler_create_job]

5013dfc6431f786dcdbf4c904ff05fa2260fea6d
Ivan Hernandez <hivanalejandro@google.com>
2025-03-08 13:40:58 +0000

    feat(bigquery): Add samples for control access 3/3

---
+++ bigquery/cloud-client/viewDatasetAccessPolicy.js
@@ -0,0 +1,59 @@
+// Copyright 2025 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+'use strict';
+
+/**
+ * View access policies for a BigQuery dataset.
+ * @param {string} datasetId Dataset ID to view access policies for.
+ * @returns {Array} Array of access entries.
+ */
+function viewDatasetAccessPolicy(datasetId) {
+  // [START bigquery_view_dataset_access_policy]
+  const {BigQuery} = require('@google-cloud/bigquery');
+
+  // Instantiate a client.
+  const bigquery = new BigQuery();
+
+  // TODO (developer): Update and un-comment below lines.
+
+  // Dataset from which to get the access policy.
+  // datasetId = "my_dataset_id";
+
+  // Get a reference to the dataset.
+  const dataset = bigquery.dataset(datasetId);
+
+  return dataset.getMetadata().then(([metadata]) => {
+    const accessEntries = metadata.access || [];
+
+    // Show the array of AccessEntry objects.
+    // More details about the AccessEntry object in the BigQuery documentation:
+    // https://cloud.google.com/nodejs/docs/reference/bigquery/latest
+    console.log(
+      `${accessEntries.length} Access entries in dataset '${datasetId}':`
+    );
+    for (const accessEntry of accessEntries) {
+      console.log(`Role: ${accessEntry.role || 'null'}`);
+      console.log(`Special group: ${accessEntry.specialGroup || 'null'}`);
+      console.log(`User by Email: ${accessEntry.userByEmail || 'null'}`);
+    }
+
+    return accessEntries;
+  });
+  // [END bigquery_view_dataset_access_policy]
+}
+
+module.exports = {
+  viewDatasetAccessPolicy,
+};

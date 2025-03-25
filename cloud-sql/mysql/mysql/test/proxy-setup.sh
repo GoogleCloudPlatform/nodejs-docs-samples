5c98171c4b16028280048f577df3877173929524
Katie McLaughlin <katie@glasnt.com>
2025-03-25 14:27:02 +1100

    v2 format

--- cloud-sql/mysql/mysql/test/proxy-setup.sh
+++ cloud-sql/mysql/mysql/test/proxy-setup.sh
@@ -16,4 +16,4 @@
 
 curl -o cloud-sql-proxy https://storage.googleapis.com/cloud-sql-connectors/cloud-sql-proxy/v2.15.1/cloud-sql-proxy.linux.amd64
 chmod +x cloud-sql-proxy
-./cloud-sql-proxy  -instances=$INSTANCE_CONNECTION_NAME &
+./cloud-sql-proxy $INSTANCE_CONNECTION_NAME &

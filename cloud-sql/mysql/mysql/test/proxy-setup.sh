2720f4204e7884ee24fdc5f4c79ad75774af1c3c
Katie McLaughlin <katie@glasnt.com>
2025-03-25 13:56:02 +1100

    debug

--- cloud-sql/mysql/mysql/test/proxy-setup.sh
+++ cloud-sql/mysql/mysql/test/proxy-setup.sh
@@ -14,6 +14,8 @@
 
 # Proof of concept: setting up proxy
 
+pwd
 curl -o cloud-sql-proxy https://storage.googleapis.com/cloud-sql-connectors/cloud-sql-proxy/v2.15.1/cloud-sql-proxy.linux.amd64
 chmod +x cloud-sql-proxy
+ls
 cloud-sql-proxy -dir=/cloudsql -instances=$INSTANCE_CONNECTION_NAME &

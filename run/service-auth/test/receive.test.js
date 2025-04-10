6e4451d8fd9d4431b4c2f12a82c6ecc61059cfb8
Ivan Hernandez <hivanalejandro@google.com>
2025-04-10 15:07:33 +0000

    test(cloudrun): add unit test for token parsing and validation sample

---
+++ run/service-auth/test/receive.test.js
@@ -0,0 +1,82 @@
+// Copyright 2025 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+'use strict';
+
+const {expect} = require('chai');
+const sinon = require('sinon');
+const {OAuth2Client} = require('google-auth-library');
+const {main} = require('../receive');
+
+describe('receiveRequestAndParseAuthHeader sample', () => {
+  let verifyStub, consoleStub;
+
+  beforeEach(() => {
+    verifyStub = sinon.stub(OAuth2Client.prototype, 'verifyIdToken');
+    consoleStub = sinon.stub(console, 'log');
+  });
+
+  afterEach(() => {
+    sinon.restore();
+  });
+
+  it('should log greeting if token is valid', async () => {
+    const mockReq = {
+      headers: {
+        authorization: 'Bearer valid-token',
+      },
+    };
+
+    verifyStub.resolves({
+      getPayload: () => ({email: 'test@example.com'}),
+    });
+
+    await main(mockReq);
+    expect(consoleStub.calledWith('Hello, test@example.com!\n')).to.be.true;
+  });
+
+  it('should log error message if token is invalid', async () => {
+    const mockReq = {
+      headers: {
+        authorization: 'Bearer invalid-token',
+      },
+    };
+
+    verifyStub.rejects(new Error('invalid'));
+
+    await main(mockReq);
+    expect(consoleStub.calledWithMatch(/Invalid token: invalid/)).to.be.true;
+  });
+
+  it('should log anonymous message if no auth header', async () => {
+    const mockReq = {
+      headers: {},
+    };
+
+    await main(mockReq);
+    expect(consoleStub.calledWith('Hello, anonymous user.\n')).to.be.true;
+  });
+
+  it('should log unhandled format message if auth is not bearer', async () => {
+    const mockReq = {
+      headers: {
+        authorization: 'Basic something',
+      },
+    };
+
+    await main(mockReq);
+    expect(consoleStub.calledWith('Unhandled header format (Basic).\n')).to.be
+      .true;
+  });
+});

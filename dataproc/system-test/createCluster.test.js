b555671232faedbe1a5d5adb40c3c83b1b60a84a
Katie McLaughlin <katie@glasnt.com>
2025-04-29 14:41:19 +1000

    move to less used region (resource quota)

--- dataproc/system-test/createCluster.test.js
+++ dataproc/system-test/createCluster.test.js
@@ -19,7 +19,7 @@ const {describe, it, after} = require('mocha');
 const cp = require('child_process');
 const {v4} = require('uuid');
 
-const region = 'us-central1';
+const region = 'us-west1';
 const clusterName = `node-cc-test-${v4()}`;
 
 const dataproc = require('@google-cloud/dataproc');

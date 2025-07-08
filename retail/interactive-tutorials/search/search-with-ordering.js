208ac81e3a32c4f3448a2f62a2eec7b0160a514e
rebeccaellis <rebeccaellis@google.com>
2025-07-08 16:25:34 -0600

    Update search-with-ordering.js

--- retail/interactive-tutorials/search/search-with-ordering.js
+++ retail/interactive-tutorials/search/search-with-ordering.js
@@ -11,6 +11,7 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+// [START search_for_products_with_ordering]
 
 'use strict';
 
@@ -81,3 +82,5 @@ process.on('unhandledRejection', err => {
 });
 
 main();
+
+// [END search_for_products_with_ordering]

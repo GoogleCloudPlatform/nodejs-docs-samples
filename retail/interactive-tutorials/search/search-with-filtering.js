2da2f4cebe7baddd27ab2026a9f55a0ff3df228b
rebeccaellis <rebeccaellis@google.com>
2025-07-08 16:33:48 -0600

    Update search-with-filtering.js

--- retail/interactive-tutorials/search/search-with-filtering.js
+++ retail/interactive-tutorials/search/search-with-filtering.js
@@ -11,6 +11,7 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+// [START retail_search_for_products_with_filtering]
 
 'use strict';
 
@@ -82,3 +83,5 @@ process.on('unhandledRejection', err => {
 });
 
 main();
+
+// [END retail_search_for_products_with_filtering]

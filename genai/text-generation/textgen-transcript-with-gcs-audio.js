db683bd777fe985c037b30d1d842d475cf931104
Guiners <rkoza@softserveinc.com>
2025-08-08 12:11:54 +0200

    adding samples, test, lints

---
+++ genai/text-generation/textgen-transcript-with-gcs-audio.js
@@ -0,0 +1,66 @@
+// Copyright 2025 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+'use strict';
+
+// [START googlegenaisdk_textgen_with_mute_video]
+const {GoogleGenAI} = require('@google/genai');
+
+const GOOGLE_CLOUD_PROJECT = process.env.GOOGLE_CLOUD_PROJECT;
+const GOOGLE_CLOUD_LOCATION = process.env.GOOGLE_CLOUD_LOCATION || 'global';
+// todo
+async function generateContent(
+  projectId = GOOGLE_CLOUD_PROJECT,
+  location = GOOGLE_CLOUD_LOCATION
+) {
+  const ai = new GoogleGenAI({
+    vertexai: true,
+    project: projectId,
+    location: location,
+  });
+
+  const prompt = `Transcribe the interview, in the format of timecode, speaker, caption.
+    Use speaker A, speaker B, etc. to identify speakers.`;
+
+
+  const response = await ai.models.generateContent({
+    model: 'gemini-2.5-flash',
+    contents: [
+      {
+        role: 'user',
+        parts: [
+          {
+            fileData: {
+              mimeType: 'video/mp4',
+              fileUri:
+                'gs://cloud-samples-data/generative-ai/video/ad_copy_from_video.mp4',
+            },
+          },
+          {
+            text: 'What is in the video?',
+          },
+        ],
+      },
+    ],
+  });
+
+  console.log(response.text);
+
+  return response.text;
+}
+// [END googlegenaisdk_textgen_with_mute_video]
+
+module.exports = {
+  generateContent,
+};

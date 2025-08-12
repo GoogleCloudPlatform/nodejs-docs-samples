7b40ebe91e8ab6d1d52fb8455d6963d452212391
Mihir Vala <179564180+mihirvala-crestdata@users.noreply.github.com>
2025-08-12 12:24:38 +0530

    chore(modelarmor): enabled floor settings tests (#4123)
    * chore(tests): enabled floor settings tests
    
    * chore(tests): added floor setting enable flag exists on get tests
    
    * chore(tests): enabled floor settings clean-up
    
    * removed empty template related tests
    
    As Katie mentioned earlier in C# repo that service level tests already exists, so anyways these tests won't help. Hence, removing them
    
    * chore: update dependencies and improve floor settings tests
    
    * ci: move custard config to same level as package.json
    
    * ci: may not support ints here, use strings
    
    ---------
    
    Co-authored-by: Harsh Nasit <131268456+harshnasitcrest@users.noreply.github.com>
    Co-authored-by: Katie McLaughlin <katie@glasnt.com>
    Co-authored-by: Harsh Nasit <harsh.nasit@crestdata.ai>

--- model-armor/snippets/updateFolderFloorSettings.js
+++ model-armor/snippets/updateFolderFloorSettings.js
@@ -57,7 +57,8 @@ async function main(folderId) {
                 protos.google.cloud.modelarmor.v1.RaiFilterType
                   .SEXUALLY_EXPLICIT,
               confidenceLevel:
-                protos.google.cloud.modelarmor.v1.DetectionConfidenceLevel.HIGH,
+                protos.google.cloud.modelarmor.v1.DetectionConfidenceLevel
+                  .LOW_AND_ABOVE,
             },
           ],
         },

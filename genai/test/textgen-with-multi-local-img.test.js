9810e172d8f88107e1177e5373b524ca1a4ef9e6
Guiners <rkoza@softserveinc.com>
2025-08-19 14:47:47 +0200

    adding samples, test, lints

---
+++ genai/test/textgen-with-multi-local-img.test.js
@@ -0,0 +1,38 @@
+// Copyright 2025 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+'use strict';
+
+const {assert} = require('chai');
+const {describe, it} = require('mocha');
+
+const projectId = process.env.CAIP_PROJECT_ID;
+const location = process.env.GOOGLE_CLOUD_LOCATION || 'global';
+
+const sample = require('../text-generation/textgen-with-multi-local-img.js');
+
+describe('textgen-with-multi-local-img', async () => {
+  it('should generate text content from a pdf', async function () {
+    this.timeout(100000);
+    const imagePath1 = './test/test-data/latte.jpg';
+    const imagePath2 = './test/test-data/scones.jpg';
+    const output = await sample.generateContent(
+      projectId,
+      location,
+      imagePath1,
+      imagePath2
+    );
+    assert(output.length > 0);
+  });
+});
--- genai/text-generation/textgen-with-multi-local-img.js
+++ genai/text-generation/textgen-with-multi-local-img.js
@@ -14,15 +14,23 @@
 
 'use strict';
 
-// [START googlegenaisdk_textgen_code_with_pdf]
+// [START googlegenaisdk_textgen_with_multi_local_img]
 const {GoogleGenAI} = require('@google/genai');
+const fs = require('fs');
 
 const GOOGLE_CLOUD_PROJECT = process.env.GOOGLE_CLOUD_PROJECT;
 const GOOGLE_CLOUD_LOCATION = process.env.GOOGLE_CLOUD_LOCATION || 'global';
 
+function loadImageAsBase64(path) {
+  const bytes = fs.readFileSync(path);
+  return bytes.toString('base64');
+}
+
 async function generateContent(
   projectId = GOOGLE_CLOUD_PROJECT,
-  location = GOOGLE_CLOUD_LOCATION
+  location = GOOGLE_CLOUD_LOCATION,
+  imagePath1,
+  imagePath2
 ) {
   const ai = new GoogleGenAI({
     vertexai: true,
@@ -30,32 +38,41 @@ async function generateContent(
     location: location,
   });
 
-  const contents = [
-    {
-      role: 'user',
-      parts: [
-        {text: 'Convert this python code to use Google Python Style Guide.'},
-        {
-          fileData: {
-            fileUri:
-              'https://storage.googleapis.com/cloud-samples-data/generative-ai/text/inefficient_fibonacci_series_python_code.pdf',
-            mimeType: 'application/pdf',
-          },
-        },
-      ],
-    },
-  ];
+  // TODO(Developer): Update the below file paths to your images
+  const image1 = loadImageAsBase64(imagePath1);
+  const image2 = loadImageAsBase64(imagePath2);
 
   const response = await ai.models.generateContent({
     model: 'gemini-2.5-flash',
-    contents: contents,
+    contents: [
+      {
+        role: 'user',
+        parts: [
+          {
+            text: 'Generate a list of all the objects contained in both images.',
+          },
+          {
+            inlineData: {
+              data: image1,
+              mimeType: 'image/jpeg',
+            },
+          },
+          {
+            inlineData: {
+              data: image2,
+              mimeType: 'image/jpeg',
+            },
+          },
+        ],
+      },
+    ],
   });
 
   console.log(response.text);
 
   return response.text;
 }
-// [END googlegenaisdk_textgen_code_with_pdf]
+// [END googlegenaisdk_textgen_with_multi_local_img]
 
 module.exports = {
   generateContent,

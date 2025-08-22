83ca1ff3fc56e20ca2bf054ebea154a0cad3b80f
Guiners <rkoza@softserveinc.com>
2025-08-22 10:05:16 +0200

    adding samples, test, lints

---
+++ genai/test/tools-vais-with-txt.test.js
@@ -0,0 +1,32 @@
+// Copyright 2025 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+'use strict';
+
+const {assert} = require('chai');
+const {describe, it} = require('mocha');
+
+const projectId = process.env.CAIP_PROJECT_ID;
+const sample = require('../tools/tools-vais-with-txt.js');
+const location = process.env.GOOGLE_CLOUD_LOCATION || 'global';
+const datastore = `projects/${projectId}/locations/global/collections/default_collection/dataStores/grounding-test-datastore`;
+
+describe('tools-vais-with-txt', () => {
+  it('should generate a function call', async function () {
+    this.timeout(60000);
+    const output = await sample.generateContent(datastore, projectId, location);
+    assert(output.length > 0);
+  });
+});
+6;

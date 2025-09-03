4aa07301bb3e40e481a4cc541dab298ad736471e
Thiyagu K <thiyaguk@google.com>
2025-09-03 07:40:50 +0000

    feat(storagecontrol): Add samples for Storage Anywhere Cache

---
+++ storage-control/updateAnywhereCache.js
@@ -0,0 +1,71 @@
+// Copyright 2025 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+'use strict';
+
+function main(bucketName, cacheName, admissionPolicy) {
+  // [START storage_control_update_anywhere_cache]
+  /**
+   * TODO(developer): Uncomment these variables before running the sample.
+   */
+
+  // The name of your GCS bucket
+  // const bucketName = 'bucketName';
+
+  // The name of the cache to be update
+  // const cacheName = 'cacheName';
+
+  // The admission policy of the cache to be updated
+  // const admissionPolicy = 'admit-on-first-miss';
+
+  // Imports the Control library
+  const {StorageControlClient} = require('@google-cloud/storage-control').v2;
+
+  // Instantiates a client
+  const controlClient = new StorageControlClient();
+
+  async function callUpdateAnywhereCache() {
+    const anywhereCachePath = controlClient.anywhereCachePath(
+      '_',
+      bucketName,
+      cacheName
+    );
+
+    // Create the request
+    const request = {
+      anywhereCache: {
+        name: anywhereCachePath,
+        admissionPolicy: admissionPolicy,
+      },
+      updateMask: {
+        paths: ['admission_policy'],
+      },
+    };
+
+    // Run request
+    const [operation] = await controlClient.updateAnywhereCache(request);
+    const [response] = await operation.promise();
+
+    console.log(`Updated anywhere cache: ${response.name}.`);
+  }
+
+  callUpdateAnywhereCache();
+  // [END storage_control_update_anywhere_cache]
+}
+
+process.on('unhandledRejection', err => {
+  console.error(err.message);
+  process.exitCode = 1;
+});
+main(...process.argv.slice(2));

f4139e687426f4c55ebcaa15f68518fc31e259ea
Guiners <rkoza@softserveinc.com>
2025-09-29 12:20:54 +0200

    updating sdk version
    /q
    
    /q
    q/
    \q
    \q
    q\
    
    

--- genai/count-tokens/counttoken-with-txt-vid.js
+++ genai/count-tokens/counttoken-with-txt-vid.js
@@ -24,7 +24,7 @@ async function countTokens(
   projectId = GOOGLE_CLOUD_PROJECT,
   location = GOOGLE_CLOUD_LOCATION
 ) {
-  const ai = new GoogleGenAI({
+  const client = new GoogleGenAI({
     vertexai: true,
     project: projectId,
     location: location,
@@ -37,7 +37,7 @@ async function countTokens(
     },
   };
 
-  const response = await ai.models.countTokens({
+  const response = await client.models.countTokens({
     model: 'gemini-2.5-flash',
     contents: [video, 'Provide a description of the video.'],
   });
--- genai/text-generation/textgen-with-txt-routing.js
+++ genai/text-generation/textgen-with-txt-routing.js
@@ -25,7 +25,7 @@ async function generateContent(
   projectId = GOOGLE_CLOUD_PROJECT,
   location = GOOGLE_CLOUD_LOCATION
 ) {
-  const ai = new GoogleGenAI({
+  const client = new GoogleGenAI({
     vertexai: true,
     project: projectId,
     location: location,
@@ -37,7 +37,7 @@ async function generateContent(
     },
   };
 
-  const response = await ai.models.generateContent({
+  const response = await client.models.generateContent({
     model: 'model-optimizer-exp-04-09',
     contents: 'How does AI work?',
     config: generateContentConfig,

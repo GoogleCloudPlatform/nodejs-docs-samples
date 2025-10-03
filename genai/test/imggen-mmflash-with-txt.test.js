aebb45a9e47887406bb6b2056d141a20ddf7b8ef
Guiners <rkoza@softserveinc.com>
2025-10-03 14:08:58 +0200

    test fix

--- genai/test/imggen-mmflash-with-txt.test.js
+++ genai/test/imggen-mmflash-with-txt.test.js
@@ -28,7 +28,7 @@ describe('imggen-mmflash-with-txt', async () => {
     this.timeout(180000);
     this.retries(5);
     await delay(this.test);
-    const generatedFileNames = await sample.generateContent(
+    const generatedFileNames = await sample.generateImage(
       projectId,
       location
     );

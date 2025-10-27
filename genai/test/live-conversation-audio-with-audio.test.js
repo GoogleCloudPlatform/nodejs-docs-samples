1be8530dcd482e12aedec25eb92cfac8e0a6bf16
Guiners <rkoza@softserveinc.com>
2025-10-27 12:08:29 +0100

    adding mock to live-conversation-audio-with-audio

--- genai/test/live-conversation-audio-with-audio.test.js
+++ genai/test/live-conversation-audio-with-audio.test.js
@@ -19,10 +19,13 @@ const {describe, it} = require('mocha');
 
 const projectId = process.env.CAIP_PROJECT_ID;
 const sample = require('../live/live-conversation-audio-with-audio');
+const {delay} = require('./util');
 
 describe('live-conversation-audio-with-audio', () => {
   it('should generate content in a live session conversation from a text prompt', async function () {
     this.timeout(180000);
+    this.retries(4);
+    await delay(this.test);
     const output = await sample.generateLiveConversation(projectId);
     console.log('Generated output:', output);
     assert(output.length > 0);

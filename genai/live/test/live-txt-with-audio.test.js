8b29558c870baed0e3601f222d4b406fcc19dfaf
Holt Skinner <13262395+holtskinner@users.noreply.github.com>
2025-12-18 11:22:48 -0600

    Move tests into dedicated directories for each sub-folder

---
+++ genai/live/test/live-txt-with-audio.test.js
@@ -0,0 +1,86 @@
+// Copyright 2025 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+'use strict';
+
+const {assert} = require('chai');
+const {describe, it} = require('mocha');
+
+const projectId = process.env.CAIP_PROJECT_ID;
+const {delay} = require('../../test/util');
+
+const proxyquire = require('proxyquire');
+
+describe('live-txt-with-audio', () => {
+  it('should generate txt content in a live session from an audio', async function () {
+    const fakeFetch = async () => ({
+      ok: true,
+      arrayBuffer: async () => Buffer.from('fake audio'),
+    });
+
+    const fakeClient = {
+      live: {
+        connect: async (opts = {}) => {
+          console.log('Mock is called');
+
+          if (
+            opts &&
+            opts.callbacks &&
+            typeof opts.callbacks.onmessage === 'function'
+          ) {
+            setImmediate(() =>
+              opts.callbacks.onmessage({
+                text: 'Yes, I can hear you.',
+                serverContent: {
+                  turnComplete: false,
+                },
+              })
+            );
+
+            setImmediate(() =>
+              opts.callbacks.onmessage({
+                text: 'Here is the final response.',
+                serverContent: {
+                  turnComplete: true,
+                },
+              })
+            );
+          }
+
+          return {
+            sendRealtimeInput: async () => {},
+            close: async () => {},
+          };
+        },
+      },
+    };
+
+    const sample = proxyquire('../live-txt-with-audio', {
+      'node-fetch': fakeFetch,
+      '@google/genai': {
+        GoogleGenAI: function () {
+          return fakeClient;
+        },
+        Modality: {TEXT: 'TEXT'},
+      },
+    });
+
+    this.timeout(180000);
+    this.retries(4);
+    await delay(this.test);
+    const output = await sample.generateLiveConversation(projectId);
+    console.log('Generated output:', output);
+    assert(output.length > 0);
+  });
+});

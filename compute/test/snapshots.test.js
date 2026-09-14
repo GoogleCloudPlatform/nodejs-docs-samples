a5754aa5b76fcb29c92e540f884400523570920c
Maciej Strzelczyk <strzelczyk@google.com>
2026-09-14 16:24:01 +0200

    chore: Updating Debian 11 to 13

--- compute/test/snapshots.test.js
+++ compute/test/snapshots.test.js
@@ -132,7 +132,7 @@ describe('snapshots tests', () => {
 
     const [newestDebian] = await imagesClient.getFromFamily({
       project: 'debian-cloud',
-      family: 'debian-11',
+      family: 'debian-13',
     });
 
     await createDisk(projectId, zone, diskName, newestDebian.selfLink);
